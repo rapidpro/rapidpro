a91423405b5b588202969cc2110e955b0a07c0eb
Nic Pottier <nicpottier@gmail.com>
2014-12-22 13:59:08 -0700

    fix empty case, yay for unit tests

--- temba/values/models.py
+++ temba/values/models.py
@@ -207,7 +207,15 @@ class Value(models.Model):
 
             # contacts that have gotten to this step
             step_contacts = FlowStep.objects.filter(step_uuid=ruleset.uuid)
-            step_contacts = step_contacts.extra(where=["run_id = ANY(VALUES " + ", ".join(["(%d)" % r for r in runs]) + ")"])
+
+            # if we have runs to filter by, do so using ANY
+            if runs:
+                step_contacts = step_contacts.extra(where=["run_id = ANY(VALUES " + ", ".join(["(%d)" % r for r in runs]) + ")"])
+
+            # otherwise, exclude all
+            else:
+                step_contacts = step_contacts.extra(where=["1=0"])
+
             step_contacts = set([s['run__contact'] for s in step_contacts.values('run__contact')])
 
             # restrict to our filter uuids if we are self filtering
