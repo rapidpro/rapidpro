be8c6d41f6c9e3e1f65134b1ae0884ac49d93feb
Simon de Haan <simon@praekeltfoundation.org>
2015-02-25 20:44:47 +0200

    install less without bower

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -2,16 +2,12 @@ pip="${VENV}/bin/pip"
 
 cd "${INSTALLDIR}/${REPO}/"
 
-if [ ! -f `which coffee` ]; then
+if [ ! -f /usr/local/bin/coffee ]; then
     npm install -g coffee-script
 fi
 
-if [ ! -f `which bower` ]; then
-    npm install -g bower
-fi
-
-if [ ! -f `which lessc` ]; then
-    bower --allow-root install lessc
+if [ ! -f /usr/bin/lessc ]; then
+    npm install -g less
 fi
 
 $pip install -r pip-freeze.txt
