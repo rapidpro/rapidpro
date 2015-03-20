2e8b6eb15a538ce3b279e948171329e1e4ff94e6
Rowan Seymour <rowanseymour@gmail.com>
2015-03-20 13:45:33 +0200

    Small fix for fetching messages by id in the API

--- temba/api/tests.py
+++ temba/api/tests.py
@@ -1098,6 +1098,11 @@ class APITest(TembaTest):
         self.assertEquals(self.admin.get_org(), sms.org)
         self.assertEquals(self.channel, sms.channel)
         self.assertEquals(broadcast, sms.broadcast)
+        
+        # fetch by message id
+        response = self.fetchJSON(url, "id=%d" % sms.pk)
+        self.assertResultCount(response, 1)
+        self.assertContains(response, "test1")
 
         response = self.fetchJSON(url, "status=Q&before=2030-01-01T00:00:00.000&after=2010-01-01T00:00:00.000&phone=%%2B250788123123&channel=%d" % self.channel.pk)
         self.assertEquals(200, response.status_code)
