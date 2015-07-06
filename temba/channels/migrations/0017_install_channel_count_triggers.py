a418b3980698d06c00965e4c3dea7e5a11916de0
Nic Pottier <nicpottier@gmail.com>
2015-07-06 17:35:38 -0400

    Trigger fixes

--- temba/channels/migrations/0017_install_channel_count_triggers.py
+++ temba/channels/migrations/0017_install_channel_count_triggers.py
@@ -69,19 +69,19 @@ class Migration(migrations.Migration):
         """
         #language=SQL
         install_trigger = """
-            CREATE OR REPLACE FUNCTION decrement_daily_channel_count(channel_id, count_type, count_day) RETURNS TRIGGER AS $$
+            CREATE OR REPLACE FUNCTION decrement_daily_channel_count(_channel_id INTEGER, _msg_type VARCHAR(2), _count_day DATE) RETURNS VOID AS $$
               BEGIN
                 UPDATE channels_dailychannelcount SET "count"="count"-1
-                  WHERE channel_id=channel_id AND count_type=count_type AND "day" = count_day;
+                  WHERE "channel_id"=_channel_id AND "count_type"=_count_type AND "day" = _count_day;
               END;
             $$ LANGUAGE plpgsql;
 
-            CREATE OR REPLACE FUNCTION increment_daily_channel_count(channel_id, count_type, count_day) RETURNS TRIGGER AS $$
+            CREATE OR REPLACE FUNCTION increment_daily_channel_count(_channel_id INTEGER, _count_type VARCHAR(2), _count_day DATE) RETURNS VOID AS $$
               BEGIN
                 LOOP
                   -- first try incrementing
-                  updated = UPDATE channels_dailychannelcount SET "count"="count"+1
-                              WHERE channel_id=channel_id AND count_type=count_type AND "day" = count_day;
+                  UPDATE channels_dailychannelcount SET "count"="count"+1
+                    WHERE "channel_id"=_channel_id AND "count_type"=_count_type AND "day" = _count_day;
                   IF found THEN
                     RETURN;
                   END IF;
@@ -90,8 +90,8 @@ class Migration(migrations.Migration):
                   -- if someone else inserts the same key concurrently,
                   -- we will get a unique-key failure
                   BEGIN
-                    INSERT INTO channels_dailychannelcount(channel_id, count_type, "day", "count")
-                      VALUES(channel_id, count_type, count_day, 1);
+                    INSERT INTO channels_dailychannelcount("channel_id", "count_type", "day", "count")
+                      VALUES(_channel_id, _count_type, _count_day, 1);
                     RETURN;
                   EXCEPTION WHEN unique_violation THEN
                     -- Do nothing, and loop to try the UPDATE again.
@@ -122,48 +122,48 @@ class Migration(migrations.Migration):
                 -- If this is an incoming message, without message type, then increment that count
                 IF NEW.direction = 'I' THEN
                   -- This is a voice message, increment that count
-                  IF NEW.msg_type IS 'V' THEN
-                    SELECT increment_daily_channel_count(NEW.channel_id, "IV", DAY(NEW.created_on))
+                  IF NEW.msg_type = 'V' THEN
+                    PERFORM increment_daily_channel_count(NEW.channel_id, 'IV', NEW.created_on::date);
                   -- Otherwise, this is a normal message
                   ELSE
-                    SELECT increment_daily_channel_count(NEW.channel_id, "IM", DAY(NEW.created_on))
+                    PERFORM increment_daily_channel_count(NEW.channel_id, 'IM', NEW.created_on::date);
                   END IF;
 
                 -- This is an outgoing message
                 ELSIF NEW.direction = 'O' THEN
                   -- This is a voice message, increment that count
-                  IF NEW.msg_type IS 'V' THEN
-                    SELECT increment_daily_channel_count(NEW.channel_id, "OV", DAY(NEW.created_on))
+                  IF NEW.msg_type = 'V' THEN
+                    PERFORM increment_daily_channel_count(NEW.channel_id, 'OV', NEW.created_on::date);
                   -- Otherwise, this is a normal message
                   ELSE
-                    SELECT increment_daily_channel_count(NEW.channel_id, "OM", DAY(NEW.created_on))
+                    PERFORM increment_daily_channel_count(NEW.channel_id, 'OM', NEW.created_on::date);
                   END IF;
 
                 END IF;
-              END IF;
-
-              RETURN NULL;
 
               -- Assert that updates aren't happening that we don't approve of
               ELSIF TG_OP = 'UPDATE' THEN
                 -- If the direction is changing, blow up
-                IF NEW.direction != OLD.direction
-                  RAISE ERROR 'Cannot change direction on messages'
+                IF NEW.direction <> OLD.direction THEN
+                  RAISE EXCEPTION 'Cannot change direction on messages';
+                END IF;
 
                 -- Cannot move from IVR to Text
-                IF NEW.msg_type = 'V' and OLD.msg_type != 'V' THEN
-                  RAISE ERROR 'Cannot change a message from voice to something else'
+                IF NEW.msg_type = 'V' and OLD.msg_type <> 'V' THEN
+                  RAISE EXCEPTION 'Cannot change a message from voice to something else';
+                END IF;
 
-                -- Cannot change created_on
-                IF NEW.created_on != OLD.created_on
-                  RAISE ERROR 'Cannot change created_on on messages'
+               -- Cannot change created_on
+                IF NEW.created_on <> OLD.created_on THEN
+                  RAISE EXCEPTION 'Cannot change created_on on messages';
+                END IF;
 
               -- Message is being deleted, we need to decrement our count
               ELSIF TG_OP = 'DELETE' THEN
                 -- Find out if this is a test contact
                 SELECT contacts_contact.is_test INTO STRICT is_test FROM contacts_contact WHERE id=OLD.contact_id;
 
-                -- Escape out if we are
+                -- Escape out if this is a test contact
                 IF is_test THEN
                   RETURN NULL;
                 END IF;
@@ -171,29 +171,30 @@ class Migration(migrations.Migration):
                 -- This is an incoming message
                 IF NEW.direction = 'I' THEN
                   -- And it is voice
-                  IF NEW.msg_type IS 'V' THEN
-                    SELECT decrement_daily_channel_count(NEW.channel_id, "IV", DAY(NEW.created_on))
+                  IF NEW.msg_type = 'V' THEN
+                    PERFORM decrement_daily_channel_count(NEW.channel_id, 'IV', DAY(NEW.created_on));
                   -- Otherwise, this is a normal message
                   ELSE
-                    SELECT decrement_daily_channel_count(NEW.channel_id, "IM", DAY(NEW.created_on))
+                    PERFORM decrement_daily_channel_count(NEW.channel_id, 'IM', DAY(NEW.created_on));
                   END IF;
 
                 -- This is an outgoing message
                 ELSIF NEW.direction = 'O' THEN
                   -- And it is voice
-                  IF NEW.msg_type IS 'V' THEN
-                    SELECT decrement_daily_channel_count(NEW.channel_id, "OV", DAY(NEW.created_on))
+                  IF NEW.msg_type = 'V' THEN
+                    PERFORM decrement_daily_channel_count(NEW.channel_id, 'OV', DAY(NEW.created_on));
                   -- Otherwise, this is a normal message
                   ELSE
-                    SELECT decrement_daily_channel_count(NEW.channel_id, "OM", DAY(NEW.created_on))
+                    PERFORM decrement_daily_channel_count(NEW.channel_id, 'OM', DAY(NEW.created_on));
                   END IF;
+                END IF;
 
               -- Table being cleared, reset all counts
               ELSIF TG_OP = 'TRUNCATE' THEN
-                UPDATE channels_dailychannelcount SET count=0;
+                TRUNCATE channels_dailychannelcount;
               END IF;
 
-              RETURN NEW;
+              RETURN NULL;
             END;
             $$ LANGUAGE plpgsql;
 
