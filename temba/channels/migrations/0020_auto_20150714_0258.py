2007266f7b9170190cd362c9d4f432261b29c742
Nic Pottier <nicpottier@gmail.com>
2015-07-14 12:20:28 -0400

    Remove duplicate migration, fix old one

--- temba/channels/migrations/0020_auto_20150714_0258.py
+++ temba/channels/migrations/0020_auto_20150714_0258.py
@@ -10,63 +10,12 @@ class Migration(migrations.Migration):
         ('channels', '0019_update_channellog_triggers'),
     ]
 
-    def calculate_counts(apps, schema_editor):
-        """
-        Iterate across all our channels, calculate our message counts for each category
-        """
-        ChannelCount = apps.get_model('channels', 'ChannelCount')
-        Channel = apps.get_model('channels', 'Channel')
-        Msg = apps.get_model('msgs', 'Msg')
-
-        def add_daily_counts(count_channel, count_type, count_totals):
-            for daily_count in count_totals:
-                print "Adding %d - %s - %s" % (count_channel.id, count_type, str(daily_count))
-
-                ChannelCount.objects.create(channel=channel, count_type=count_type,
-                                            day=daily_count['created'], count=daily_count['count'])
-
-        for channel in Channel.objects.all():
-            # remove previous counts
-            ChannelCount.objects.filter(channel=channel, count_type__in=['IM', 'OM', 'IV', 'OV']).delete()
-
-            # incoming msgs
-            daily_counts = Msg.objects.filter(channel=channel, contact__is_test=False, direction='I')\
-                                      .exclude(msg_type='V')\
-                                      .extra({'created': "date(msgs_msg.created_on)"})\
-                                      .values('created')\
-                                      .annotate(count=Count('id'))\
-                                      .order_by('created')
-            add_daily_counts(channel, 'IM', daily_counts)
-
-            # outgoing msgs
-            daily_counts = Msg.objects.filter(channel=channel, contact__is_test=False, direction='O')\
-                                      .exclude(msg_type='V')\
-                                      .extra({'created': "date(msgs_msg.created_on)"})\
-                                      .values('created')\
-                                      .annotate(count=Count('id'))\
-                                      .order_by('created')
-            add_daily_counts(channel, 'OM', daily_counts)
-
-            # incoming voice
-            daily_counts = Msg.objects.filter(channel=channel, contact__is_test=False, direction='I')\
-                                      .filter(msg_type='V')\
-                                      .extra({'created': "date(msgs_msg.created_on)"})\
-                                      .values('created')\
-                                      .annotate(count=Count('id'))\
-                                      .order_by('created')
-            add_daily_counts(channel, 'IV', daily_counts)
-
-            # outgoing voice
-            daily_counts = Msg.objects.filter(channel=channel, contact__is_test=False, direction='O')\
-                                      .filter(msg_type='V')\
-                                      .extra({'created': "date(msgs_msg.created_on)"})\
-                                      .values('created')\
-                                      .annotate(count=Count('id'))\
-                                      .order_by('created')
-            add_daily_counts(channel, 'OV', daily_counts)
+    # this used to be a fix for another migration, but that has been applied earlier
+    def noop(apps, schema_editor):
+        pass
 
     operations = [
         migrations.RunPython(
-            calculate_counts,
+            noop,
         ),
     ]
