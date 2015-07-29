ab168e1c08491e8a180009cb200d1e99f6129e4f
Norbert Kwizera <norkans7@gmail.com>
2015-07-29 15:17:48 +0200

    fix migrations

--- temba/values/migrations/0004_auto_20150728_1030.py
+++ temba/values/migrations/0004_auto_20150728_1030.py
@@ -13,27 +13,28 @@ def fix_district_contact_fields_values(apps, schema_editor):
         contact = district_value.contact
 
         state_field = ContactField.objects.filter(is_active=True, org=org, value_type='S').first()
-        state_field_key = state_field.key
+        if state_field:
+            state_field_key = state_field.key
 
-        state_value = Value.objects.filter(contact=contact, contact_field__key__exact=state_field_key).first()
+            state_value = Value.objects.filter(contact=contact, contact_field__key__exact=state_field_key).first()
 
-        if district_value.location_value.parent != state_value.location_value:
-            new_district_boundary = state_value.location_value.children.filter(
-                name__iexact=district_value.location_value.name,
-                level=2).first()
+            if state_value and district_value.location_value:
+                if district_value.location_value.parent != state_value.location_value:
+                    new_district_boundary = state_value.location_value.children.filter(
+                        name__iexact=district_value.location_value.name,
+                        level=2).first()
 
-            if new_district_boundary:
-                district_value.location_value = new_district_boundary
-                district_value.save()
-            else:
-                state_value.location_value = district_value.location_value.parent
-                state_value.save()
+                    if new_district_boundary:
+                        district_value.location_value = new_district_boundary
+                        district_value.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('values', '0003_auto_20150527_1909'),
+        ('contacts', '0020_exportcontactstask_is_finished'),
+        ('locations', '0002_auto_20141126_2054'),
     ]
 
     operations = [
