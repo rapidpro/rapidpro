88d6785716f02f44c20e68fe8ff9b288a6bd0c34
Nic Pottier <nicpottier@gmail.com>
2015-08-03 20:06:33 -0400

    Better naming of removed, remove unneeded DECLARE

--- temba/channels/migrations/0021_auto_20150803_1857.py
+++ temba/channels/migrations/0021_auto_20150803_1857.py
@@ -29,22 +29,20 @@ class Migration(migrations.Migration):
             $$ LANGUAGE plpgsql;
 
             CREATE OR REPLACE FUNCTION temba_maybe_squash_channelcount(_channel_id INTEGER, _count_type VARCHAR(2), _count_day DATE) RETURNS VOID AS $$
-              DECLARE
-               current_count INT;
               BEGIN
-                IF RANDOM() < .01 THEN
+                IF RANDOM() < .005 THEN
                   IF _count_day IS NULL THEN
-                    WITH D as (DELETE FROM channels_channelcount
+                    WITH removed as (DELETE FROM channels_channelcount
                       WHERE "channel_id" = _channel_id AND "count_type" = _count_type AND "day" IS NULL
                       RETURNING "count")
                       INSERT INTO channels_channelcount("channel_id", "count_type", "count")
-                      VALUES (_channel_id, _count_type, GREATEST(0, (SELECT SUM("count") FROM D)));
+                      VALUES (_channel_id, _count_type, GREATEST(0, (SELECT SUM("count") FROM removed)));
                   ELSE
-                    WITH D as (DELETE FROM channels_channelcount
+                    WITH removed as (DELETE FROM channels_channelcount
                       WHERE "channel_id" = _channel_id AND "count_type" = _count_type AND "day" = _count_day
                       RETURNING "count")
                       INSERT INTO channels_channelcount("channel_id", "count_type", "day", "count")
-                      VALUES (_channel_id, _count_type, _count_day, GREATEST(0, (SELECT SUM("count") FROM D)));
+                      VALUES (_channel_id, _count_type, _count_day, GREATEST(0, (SELECT SUM("count") FROM removed)));
                   END IF;
                 END IF;
               END;
