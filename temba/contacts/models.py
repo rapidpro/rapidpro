14ff305cf3897f24d2744878b1d8f33fb98c3edf
Norbert Kwizera <norkans7@gmail.com>
2015-08-14 14:04:06 +0200

    update OLD_TEST_CONTACT_TEL value

--- temba/contacts/models.py
+++ temba/contacts/models.py
@@ -31,7 +31,7 @@ RESERVED_CONTACT_FIELDS = ['name', 'phone', 'created_by', 'modified_by', 'org', 
 GROUP_MEMBER_COUNT_CACHE_KEY = 'org:%d:cache:group_member_count:%d'
 
 # phone number for every org's test contact
-OLD_TEST_CONTACT_TEL = '+12065551212'
+OLD_TEST_CONTACT_TEL = '12065551212'
 START_TEST_CONTACT_PATH = 12065550100
 END_TEST_CONTACT_PATH = 12065550199
 
