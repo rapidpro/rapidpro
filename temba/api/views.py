11685c098c40e8cda7e494c81435abdf6c4cc517
Nic Pottier <nicpottier@gmail.com>
2015-10-26 13:17:23 -0500

    Pass through appropriate fields in our webhook simulator

--- temba/api/views.py
+++ temba/api/views.py
@@ -170,8 +170,9 @@ class WebHookTunnelView(View):
             incoming_data = parse_qs(data)
             outgoing_data = dict()
             for key in incoming_data.keys():
-                if key in ['relayer', 'sms', 'phone', 'text', 'time', 'call', 'duration', 'power_level', 'power_status',
-                           'power_source', 'network_type', 'pending_message_count', 'retry_message_count', 'last_seen', 'event']:
+                if key in ['relayer', 'channel', 'sms', 'phone', 'text', 'time', 'call', 'duration', 'power_level', 'power_status',
+                           'power_source', 'network_type', 'pending_message_count', 'retry_message_count', 'last_seen', 'event',
+                           'step', 'values', 'flow', 'relayer_phone']:
                     outgoing_data[key] = incoming_data[key]
 
             response = requests.post(url, data=outgoing_data, timeout=3)
@@ -249,6 +250,7 @@ class WebHookSimulatorView(SmartTemplateView):
         fields = list()
         fields.append(dict(name="relayer", help="The id of the channel which handled this flow step", default=1))
         fields.append(dict(name="relayer_phone", help="The phone number of the channel", default="+250788123123"))
+        fields.append(dict(name="phone", help="The phone number of the contact", default="+250788788123"))
         fields.append(dict(name="flow", help="The id of the flow (reference the URL on your flow page)", default=504))
         fields.append(dict(name="step", help="The uuid of the step which triggered this event (reference your flow)", default="15121251-15121241-15145152-12541241"))
         fields.append(dict(name="time", help="The time that this step was reached by the user in ECMA-162 format", default="2013-01-21T22:34:00.123"))
