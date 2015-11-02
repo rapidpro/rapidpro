8cb4327464e7835e3a22af683b1301d856fa709f
Rowan Seymour <rowanseymour@gmail.com>
2015-11-02 17:41:53 +0200

    Re-add select_related for Msg.contact in process_message_task and also include contact_urn

--- temba/msgs/tasks.py
+++ temba/msgs/tasks.py
@@ -25,7 +25,7 @@ def process_message_task(msg_id, from_mage=False, new_contact=False):
     Processes a single incoming message through our queue.
     """
     r = get_redis_connection()
-    msg = Msg.objects.filter(pk=msg_id, status=PENDING).select_related('org').first()
+    msg = Msg.objects.filter(pk=msg_id, status=PENDING).select_related('org', 'contact', 'contact_urn').first()
 
     # somebody already handled this message, move on
     if not msg:
