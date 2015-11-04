f6e8cf0c7f008d50024b22acf758c7f982623daf
Rowan Seymour <rowanseymour@gmail.com>
2015-11-04 11:53:44 +0200

    Fixed migration conflict with latest master changes

--- temba/flows/migrations/0040_auto_20151103_1014.py
+++ temba/flows/migrations/0040_auto_20151103_1014.py
@@ -7,15 +7,10 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flows', '0037_auto_20151023_1704'),
+        ('flows', '0039_auto_20151028_1648'),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='flow',
-            name='version_number',
-            field=models.IntegerField(default=8, help_text='The flow version this definition is in'),
-        ),
         migrations.AlterField(
             model_name='flowstart',
             name='contacts',
@@ -30,10 +25,5 @@ class Migration(migrations.Migration):
             model_name='flowstep',
             name='messages',
             field=models.ManyToManyField(help_text='Any messages that are associated with this step (either sent or received)', related_name='steps', to='msgs.Msg'),
-        ),
-        migrations.AlterField(
-            model_name='flowversion',
-            name='spec_version',
-            field=models.IntegerField(default=8, help_text='The flow version this definition is in'),
         ),
     ]
