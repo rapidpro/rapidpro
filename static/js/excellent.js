d3fe0f50d6d51e20d68e1e213ade833b128ab45d
Norbert Kwizera <norkans7@gmail.com>
2015-11-06 17:11:03 +0200

    Fix bug for variable completion inside functions

--- static/js/excellent.js
+++ static/js/excellent.js
@@ -54,7 +54,7 @@
         var skipChar = false;
         var neededParentheses = [];
         var inQuotes = false;
-        var prependFlag = ''
+        var prependFlag = '';
 
         for (var pos = partialExpression.length - 1; pos >= 0; pos--) {
             var ch = partialExpression[pos];
@@ -92,8 +92,11 @@
                 }
             }
 
-            if (skipChar || inQuotes || (ch === '(' && fragment == '')) {
+            if (ch === '(' && fragment == '') {
                 prependFlag = '#';
+            }
+
+            if (skipChar || inQuotes || (ch === '(' && fragment == '')) {
                 continue;
             }
 
