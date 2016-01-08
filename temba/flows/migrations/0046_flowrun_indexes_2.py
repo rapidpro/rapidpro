ed9c0e3321ba4cb0595d559cb381a3f6f549a95b
Rowan Seymour <rowanseymour@gmail.com>
2016-01-08 11:16:50 +0200

    Amend previously added migration to also update the flow specific index to include respnoded

--- temba/flows/migrations/0046_flowrun_indexes_2.py
+++ temba/flows/migrations/0046_flowrun_indexes_2.py
@@ -14,7 +14,14 @@ IF NOT EXISTS (
     CREATE INDEX flows_flowrun_org_id_modified_on_responded ON flows_flowrun (org_id, modified_on DESC, responded);
 END IF;
 
+IF NOT EXISTS (
+    SELECT 1 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace
+    WHERE c.relname = 'flows_flowrun_flow_id_modified_on_responded' AND n.nspname = 'public') THEN
+    CREATE INDEX flows_flowrun_flow_id_modified_on_responded ON flows_flowrun (flow_id, modified_on DESC, responded);
+END IF;
+
 DROP INDEX IF EXISTS flows_flowrun_org_id_modified_on;
+DROP INDEX IF EXISTS flows_flowrun_flow_id_modified_on;
 
 END$$;"""
 
