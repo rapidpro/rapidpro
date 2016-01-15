8194b7c9ffec905b76a3792cac2120b5da2055d6
Eric Newcomer <ericnewcomer@gmail.com>
2016-01-15 13:18:29 -0800

    Add optional parameters to purge migration

--- temba/msgs/migrations/0040_auto_20151205_0206.py
+++ temba/msgs/migrations/0040_auto_20151205_0206.py
@@ -3,15 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 from django.db.models import Max
+from django.utils import timezone
+from datetime import timedelta
 
-BATCH_SIZE = 5000
-MSG_BATCH = BATCH_SIZE * 5
 
+def update_msg_purge_status(Broadcast, Msg, update_broadcasts=True, batch_size=5000, msg_start=0):
 
-def update_msg_purge_status(Broadcast, Msg):
-
-    from django.utils import timezone
-    from datetime import timedelta
+    msg_batch = batch_size * 5
 
     # 90 days ago
     purge_date = timezone.now() - timedelta(days=90)
@@ -21,47 +19,48 @@ def update_msg_purge_status(Broadcast, Msg):
     else:
         msgs = Msg.current_messages
 
-    max_pk = Broadcast.objects.aggregate(Max('pk'))['pk__max']
-    if max_pk is not None:
-        print "Populating broadcasts purged field.."
-        for offset in range(0, max_pk+1, BATCH_SIZE):
-            print 'On %d of %d' % (offset, max_pk)
+    if update_broadcasts:
+        max_pk = Broadcast.objects.aggregate(Max('pk'))['pk__max']
+        if max_pk is not None:
+            print "Populating broadcasts purged field.."
+            for offset in range(0, max_pk+1, batch_size):
+                print 'Broadcast %d of %d' % (offset, max_pk)
 
-            # determine which broadcasts are old
-            broadcasts = Broadcast.objects.filter(pk__gte=offset,
-                                                  pk__lt=offset+BATCH_SIZE,
-                                                  created_on__lt=purge_date,
-                                                  purged__isnull=True)
+                # determine which broadcasts are old
+                broadcasts = Broadcast.objects.filter(pk__gte=offset,
+                                                      pk__lt=offset+batch_size,
+                                                      created_on__lt=purge_date,
+                                                      purged__isnull=True)
 
-            # set our old broadcast purge
-            broadcasts.update(purged=False)
+                # set our old broadcast purge
+                broadcasts.update(purged=False)
 
-            # store the broadcasts we purged
-            purged_broadcasts = [b.id for b in broadcasts]
+                # store the broadcasts we purged
+                purged_broadcasts = [b.id for b in broadcasts]
 
-            # all the related messages for those broadcasts
-            max_msg_pk = msgs.aggregate(Max('pk'))['pk__max']
-            for msg_offset in range(0, max_msg_pk+1, MSG_BATCH*5):
-                # msg part of a purged broadcast
-                msgs.filter(pk__gte=msg_offset,
-                            pk__lt=msg_offset+MSG_BATCH,
-                            purged__isnull=True,
-                            broadcast_id__in=purged_broadcasts).update(purged=True)
+                # all the related messages for those broadcasts
+                max_msg_pk = msgs.aggregate(Max('pk'))['pk__max']
+                for msg_offset in range(msg_start, max_msg_pk+1, msg_batch):
+                    # msg part of a purged broadcast
+                    msgs.filter(pk__gte=msg_offset,
+                                pk__lt=msg_offset+msg_batch,
+                                purged__isnull=True,
+                                broadcast_id__in=purged_broadcasts).update(purged=True)
 
-            # any other unset broadcasts are considered not purged
-            Broadcast.objects.filter(pk__gte=offset,
-                                     pk__lt=offset+BATCH_SIZE,
-                                     purged__isnull=True).update(purged=False)
+                # any other unset broadcasts are considered not purged
+                Broadcast.objects.filter(pk__gte=offset,
+                pk__lt=offset+batch_size,
+                                         purged__isnull=True).update(purged=False)
 
     max_pk = msgs.aggregate(Max('pk'))['pk__max']
     if max_pk is not None:
-        print "Populating messages purged field.."
-        for offset in range(0, max_pk+1, BATCH_SIZE):
-            print 'On %d of %d' % (offset, max_pk)
+        print "Populating messages purged field.. (start %d)" % msg_start
+        for offset in range(msg_start, max_pk+1, batch_size):
+            print 'Msg %d of %d' % (offset, max_pk)
 
             # all remaining messages
             msgs.filter(pk__gte=offset,
-                        pk__lt=offset+BATCH_SIZE,
+                        pk__lt=offset+batch_size,
                         purged__isnull=True).update(purged=False)
 
 
