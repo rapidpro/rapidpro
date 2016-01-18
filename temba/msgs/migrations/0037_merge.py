cf962c9c6c4deaaaaab964a0b74585ddab4e9d31
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-01-18 13:03:01 +0100

    merge migration conflicts

---
+++ temba/msgs/migrations/0037_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('msgs', '0030_auto_20151116_1433'),
+        ('msgs', '0036_auto_20151103_1014'),
+    ]
+
+    operations = [
+    ]
