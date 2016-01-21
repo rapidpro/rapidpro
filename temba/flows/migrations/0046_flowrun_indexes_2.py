647ec6adb9af369dc2c9c06bb54715f5acb62e3f
Rowan Seymour <rowanseymour@gmail.com>
2016-01-21 16:39:32 +0200

    Updated index migration to create new partial indexes for flow runs where responded=true

--- temba/flows/migrations/0046_flowrun_indexes_2.py
+++ temba/flows/migrations/0046_flowrun_indexes_2.py
@@ -11,17 +11,14 @@ BEGIN
 IF NOT EXISTS (
     SELECT 1 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace
     WHERE c.relname = 'flows_flowrun_org_id_modified_on_responded' AND n.nspname = 'public') THEN
-    CREATE INDEX flows_flowrun_org_id_modified_on_responded ON flows_flowrun (org_id, modified_on DESC, responded);
+    CREATE INDEX flows_flowrun_org_id_modified_on_responded ON flows_flowrun (org_id, modified_on DESC) WHERE responded = TRUE;
 END IF;
 
 IF NOT EXISTS (
     SELECT 1 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace
     WHERE c.relname = 'flows_flowrun_flow_id_modified_on_responded' AND n.nspname = 'public') THEN
-    CREATE INDEX flows_flowrun_flow_id_modified_on_responded ON flows_flowrun (flow_id, modified_on DESC, responded);
+    CREATE INDEX flows_flowrun_flow_id_modified_on_responded ON flows_flowrun (flow_id, modified_on DESC) WHERE responded = TRUE;
 END IF;
-
-DROP INDEX IF EXISTS flows_flowrun_org_id_modified_on;
-DROP INDEX IF EXISTS flows_flowrun_flow_id_modified_on;
 
 END$$;"""
 
