2f5a130e7b334df6b267a7eda42d8bae9ecd7d9e
Nic Pottier <nicpottier@gmail.com>
2016-01-28 16:01:45 -0800

    fix delete case

--- temba/flows/migrations/0049_install_flowcount_triggers.py
+++ temba/flows/migrations/0049_install_flowcount_triggers.py
@@ -95,22 +95,32 @@ class Migration(migrations.Migration):
                 RETURN NULL;
               END IF;
 
-              -- Is this a test contact, ignore
-              IF temba_contact_is_test(NEW.contact_id) THEN
-                RETURN NULL;
-              END IF;
-
               -- FlowRun being added
               IF TG_OP = 'INSERT' THEN
+                 -- Is this a test contact, ignore
+                 IF temba_contact_is_test(NEW.contact_id) THEN
+                   RETURN NULL;
+                 END IF;
+
                 -- Increment appropriate type
                 PERFORM temba_insert_flowruncount(NEW.flow_id, NEW.exit_type, 1);
 
               -- FlowRun being removed
               ELSIF TG_OP = 'DELETE' THEN
+                 -- Is this a test contact, ignore
+                 IF temba_contact_is_test(OLD.contact_id) THEN
+                   RETURN NULL;
+                 END IF;
+
                 PERFORM temba_insert_flowruncount(OLD.flow_id, OLD.exit_type, -1);
 
               -- Updating exit type
               ELSIF TG_OP = 'UPDATE' THEN
+                 -- Is this a test contact, ignore
+                 IF temba_contact_is_test(NEW.contact_id) THEN
+                   RETURN NULL;
+                 END IF;
+
                 PERFORM temba_insert_flowruncount(OLD.flow_id, OLD.exit_type, -1);
                 PERFORM temba_insert_flowruncount(NEW.flow_id, NEW.exit_type, 1);
               END IF;
