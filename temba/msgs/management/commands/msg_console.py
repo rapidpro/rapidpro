9e3bee9a3775c94a96e12f7710032d737627dee7
Rowan Seymour <rowanseymour@gmail.com>
2016-02-12 09:51:09 +0200

    Some tweaks to the message console

--- temba/msgs/management/commands/msg_console.py
+++ temba/msgs/management/commands/msg_console.py
@@ -2,28 +2,49 @@ from __future__ import unicode_literals
 
 import cmd
 
-from colorama import init, Fore, Style
-from django.core.management.base import BaseCommand
+from colorama import init as colorama_init, Fore, Style
+from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
-from optparse import make_option
-from temba.contacts.models import Contact, TEL_SCHEME
+from temba.contacts.models import Contact, ContactURN, TEL_SCHEME
 from temba.orgs.models import Org
 from temba.msgs.models import Msg, OUTGOING
+
+
+DEFAULT_ORG = '1'
+DEFAULT_URN = "tel:+250788123123"
+
+
+def get_org(id_or_name):
+    """
+    Gets an org by its id or name. If more than one org has the name, first org is returned
+    """
+    try:
+        org_id = int(id_or_name)
+        try:
+            return Org.objects.get(pk=org_id)
+        except Org.DoesNotExist:
+            raise CommandError("No such org with id %d" % org_id)
+    except ValueError:
+        org = Org.objects.filter(name=id_or_name).first()
+        if not org:
+            raise CommandError("No such org with name '%s'" % id_or_name)
+        return org
+
 
 class MessageConsole(cmd.Cmd):
     """
     Useful REPL'like utility to simulate sending messages into RapidPro. Mostly useful for testing things
     with real contacts across multiple flows and contacts where the simulator isn't enough.
     """
-
-    def __init__(self, org, *args, **kwargs):
+    def __init__(self, org, urn, *args, **kwargs):
         cmd.Cmd.__init__(self, *args, **kwargs)
 
         self.org = org
         self.user = org.get_org_users()[0]
         self.user.set_org(org)
 
-        self.contact = self.get_contact("+250788123123")
+        self.urn = urn
+        self.contact = self.get_or_create_contact(urn)
 
         self.update_prompt()
         self.echoed = []
@@ -36,18 +57,23 @@ class MessageConsole(cmd.Cmd):
         self.echoed.append(line)
 
     def update_prompt(self):
-        urn = self.contact.get_urn()
-        self.prompt = ("\n" + Fore.CYAN + "[%s] " + Fore.WHITE) % urn.path
+        self.prompt = ("\n" + Fore.CYAN + "[%s] " + Fore.WHITE) % self.contact
 
-    def get_contact(self, number):
-        return Contact.get_or_create(self.org, self.user, name=None, urns=[(TEL_SCHEME, number)])
+    def get_or_create_contact(self, urn):
+        if ':' in urn:
+            parsed = ContactURN.parse_urn(urn)
+            urn = (parsed.scheme, parsed.path)
+        else:
+            urn = (TEL_SCHEME, urn)  # assume phone number
+
+        return Contact.get_or_create(self.org, self.user, name=None, urns=[urn])
 
     def do_org(self, line):
         """
-        Changes to the org with the specified id
+        Changes the current org
         """
         if not line:
-            self.echo("Select org with org id.  ex: org 4")
+            self.echo("Select org with org id or name.  ex: org 4")
 
             # list all org options
             for org in Org.objects.all().order_by('pk'):
@@ -57,11 +83,14 @@ class MessageConsole(cmd.Cmd):
                 self.echo((Fore.YELLOW + "[%d]" + Fore.WHITE + " %s % 40s") % (org.pk, org.name, user))
         else:
             try:
-                self.org = Org.objects.get(pk=line)
+                self.org = get_org(line)
                 self.user = self.org.get_org_admins()[0]
                 self.user.set_org(self.org)
-                self.contact = self.get_contact(self.contact.get_urn().path)
-                self.echo("You are now sending messages for %s" % self.org.name)
+                self.contact = self.get_or_create_contact(self.urn)
+
+                self.update_prompt()
+
+                self.echo("You are now sending messages for %s [%d]" % (self.org.name, self.org.pk))
                 self.echo("You are now sending as %s [%d]" % (self.contact, self.contact.pk))
             except Exception as e:
                 self.echo("Error changing org: %s" % e)
@@ -71,9 +100,9 @@ class MessageConsole(cmd.Cmd):
         Sets the current contact by URN
         """
         if not line:
-            self.echo("Set contact by specifying URN.  ex: phone tel:+250788123123")
+            self.echo("Set contact by specifying URN, ex: tel:+250788123123")
         else:
-            self.contact = self.get_contact(line)
+            self.contact = self.get_or_create_contact(line)
             self.update_prompt()
             self.echo("You are now sending as %s [%d]" % (self.contact, self.contact.pk))
 
@@ -98,24 +127,34 @@ class MessageConsole(cmd.Cmd):
         """
         return True
 
+    def do_exit(self, line):
+        """
+        Exit console
+        """
+        return True
+
 
 class Command(BaseCommand):  # pragma: no cover
-    option_list = BaseCommand.option_list + (
-        make_option('--org',
-                    action='store',
-                    dest='org',
-                    default=1,
-                    help='The id of the organization to send message for'),
-    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('--org', type=str, action='store', dest='org', default=DEFAULT_ORG,
+                            help="The id or name of the organization to send messages for")
+
+        parser.add_argument('--urn', type=str, action='store', dest='urn', default=DEFAULT_URN,
+                            help="The URN of the contact to send messages for")
 
     def handle(self, *args, **options):
-        org = Org.objects.get(pk=int(options['org']))
-        init()
-        print("Sending messages for %s\n" % org.name)
+        colorama_init()
 
-        intro = Style.BRIGHT + "Welcome to the message console." + \
-                Style.NORMAL + "\n\nSend messages by typing anything." + \
-                "\nChange org with the org command. ex: org 3" + \
-                "\nChange contact with the contact command. ex: contact 250788124124"
+        org = get_org(options['org'])
+        urn = options['urn']
 
-        MessageConsole(org).cmdloop(intro=intro)
+        intro = Style.BRIGHT + "Welcome to the message console.\n\n"
+        intro += Style.NORMAL + "Send messages by typing anything\n"
+        intro += "Change org with the org command, ex: " + Fore.YELLOW + "org 3" + Fore.WHITE + "\n"
+        intro += "Change contact with the contact command, ex: " + Fore.YELLOW + "contact tel:+250788124124" + Fore.WHITE + "\n"
+        intro += "Exit with the " + Fore.YELLOW + "exit" + Fore.WHITE + " command\n\n"
+
+        intro += ("Currently sending messages for %s [%d] as " + Fore.CYAN + "%s" + Fore.WHITE) % (org.name, org.id, urn)
+
+        MessageConsole(org, urn).cmdloop(intro=intro)
