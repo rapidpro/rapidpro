9bbed1a6fb9d8a9ea3fd88cce25e4a72ecd6f0e6
Nic Pottier <nicpottier@gmail.com>
2016-02-22 14:58:55 -0800

    sow

---
+++ temba/msgs/migrations/0045_populate_modified_on.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('msgs', '0044_auto_20160222_2214'),
+    ]
+
+    operations = [
+    ]
