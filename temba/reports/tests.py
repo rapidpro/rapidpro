de35bdf60f4d9d6876d7b91ef455c536049edab7
Rowan Seymour <rowanseymour@gmail.com>
2016-03-30 18:24:40 -0400

    Fixed unit test

--- temba/reports/tests.py
+++ temba/reports/tests.py
@@ -33,17 +33,18 @@ class ReportTest(TembaTest):
         self.assertEquals(report.pk, json.loads(response.content)['report']['id'])
 
     def test_report_model(self):
+        Report.create_report(self.org, self.admin, dict(title="first", description="blah blah text",
+                                                        config=dict(fields=[1, 2, 3])))
+        self.assertEqual(Report.objects.all().count(), 1)
 
-        Report.create_report(self.org, self.admin, dict(title="first", description="blah blah text", config=dict(fields=[1, 2, 3])))
-        self.assertEquals(Report.objects.all().count(), 1)
-
-        Report.create_report(self.org, self.admin, dict(title="second", description="yeah yeah yeah", config=dict(fields=[4, 5, 6])))
-        self.assertEquals(Report.objects.all().count(), 2)
+        Report.create_report(self.org, self.admin, dict(title="second", description="yeah yeah yeah",
+                                                        config=dict(fields=[4, 5, 6])))
+        self.assertEqual(Report.objects.all().count(), 2)
 
         report_id = Report.objects.filter(title="first")[0].pk
         Report.create_report(self.org, self.admin, dict(title="updated",
                                                         description="yeah yeah yeahnew description",
                                                         config=dict(fields=[8, 4]), id=report_id))
-        self.assertEquals(Report.objects.all().count(), 2)
+        self.assertEqual(Report.objects.all().count(), 2)
         self.assertFalse(Report.objects.filter(title="first"))
-        self.assertEquals(id, Report.objects.get(title="updated").pk)
+        self.assertEqual(Report.objects.get(title="updated").pk, report_id)
