2af5f0a1ff305b8d5b10493b8ba8f14452550b39
Fekete Gyorgy <f@pulilab.com>
2016-04-17 13:56:48 +0200

    better handling of incoming events and messages for ussd type

--- temba/channels/handlers.py
+++ temba/channels/handlers.py
@@ -907,7 +907,7 @@ class VumiHandler(View):
             return HttpResponse("Invalid JSON: %s" % unicode(e), status=400)
 
         # determine if it's a USSD session message or a regular SMS
-        is_ussd = body.get('transport_type') == 'ussd'
+        is_ussd = "ussd" in body.get('transport_name') or body.get('transport_type') == 'ussd'
         channel_type = VUMIUSSD if is_ussd else VUMI
 
         # look up the channel
