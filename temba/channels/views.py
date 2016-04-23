d140f0cc61e1d60038a597642037ab8b9796213e
Nic Pottier <nicpottier@gmail.com>
2016-04-22 21:14:19 -0500

    Move minimum access token length to 100

--- temba/channels/views.py
+++ temba/channels/views.py
@@ -1711,7 +1711,7 @@ class ChannelCRUDL(SmartCRUDL):
 
     class ClaimFacebook(OrgPermsMixin, SmartFormView):
         class FacebookForm(forms.Form):
-            page_access_token = forms.CharField(min_length=190, required=True,
+            page_access_token = forms.CharField(min_length=100, required=True,
                                                 help_text=_("The Page Access Token for your Application"))
 
             def clean_page_access_token(self):
