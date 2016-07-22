b5c0ecc402fc5d2996b9be778efa9ef05162f199
Norbert Kwizera <norkans7@gmail.com>
2016-07-22 13:11:21 +0200

    show airtime transfers logs

--- temba/airtime/views.py
+++ temba/airtime/views.py
@@ -10,9 +10,10 @@ class AirtimeCRUDL(SmartCRUDL):
     actions = ('list', 'read')
 
     class List(OrgPermsMixin, SmartListView):
-        fields = ('contact', 'status', 'channel', 'amount', 'created_on')
+        fields = ('contact', 'status', 'channel', 'amount', 'message', 'created_on')
         title = _("Recent Airtime Transfers")
         default_order = ('-created_on',)
+        field_config = dict(created_on=dict(label="Time"))
 
         def get_status(self, obj):
             return obj.get_status_display()
@@ -32,7 +33,27 @@ class AirtimeCRUDL(SmartCRUDL):
             return context
 
     class Read(OrgObjPermsMixin, SmartReadView):
-        fields = ('contact', 'status', 'channel', 'amount', 'created_on')
+        title = _("Airtime Transfer Details")
+        field_config = dict(created_on=dict(label="Time"))
+
+        def get_context_data(self, **kwargs):
+            context = super(AirtimeCRUDL.Read, self).get_context_data(**kwargs)
+            context['show_logs'] = self.show_logs()
+            return context
+
+        def show_logs(self):
+            org = self.derive_org()
+            user = self.request.user
+            if not org.is_anon or user.is_superuser or user.is_staff:
+                return True
+            return False
+
+        def derive_fields(self):
+            if self.show_logs():
+                return ('contact', 'status', 'channel', 'amount', 'message',
+                        'recipient', 'denomination', 'created_on')
+
+            return ('contact', 'status', 'channel', 'amount', 'message', 'created_on')
 
         def get_status(self, obj):
             return obj.get_status_display()
