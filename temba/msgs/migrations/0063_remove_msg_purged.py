f73bbd6f75ef39ddf682dd257af6bf91d00dd357
Nic Pottier <nicpottier@gmail.com>
2016-09-01 16:36:24 -0500

    Fix migration conflict

--- temba/msgs/migrations/0063_remove_msg_purged.py
+++ temba/msgs/migrations/0063_remove_msg_purged.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('msgs', '0061_broadcast_recipients'),
+        ('msgs', '0062_remove_exportmessagestask_host'),
     ]
 
     operations = [
