7956251ae0dcae1ffb7380364c80e6673f27b312
Norbert Kwizera <norkans7@gmail.com>
2016-09-09 12:33:36 +0200

    Do not create incoming IVRcall if no flow is set up to handle them

--- temba/channels/handlers.py
+++ temba/channels/handlers.py
@@ -83,12 +83,12 @@ class TwilioHandler(View):
 
                 flow = Trigger.find_flow_for_inbound_call(contact)
 
-                call = IVRCall.create_incoming(channel, contact, urn_obj, flow, channel.created_by)
-                call.update_status(request.POST.get('CallStatus', None),
-                                   request.POST.get('CallDuration', None))
-                call.save()
-
                 if flow:
+                    call = IVRCall.create_incoming(channel, contact, urn_obj, flow, channel.created_by)
+                    call.update_status(request.POST.get('CallStatus', None),
+                                       request.POST.get('CallDuration', None))
+                    call.save()
+
                     FlowRun.create(flow, contact.pk, call=call)
                     response = Flow.handle_call(call, {})
                     return HttpResponse(unicode(response))
