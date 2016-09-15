9d0eae5cb98b00ef693d8763fe62ac7f43c37d50
Teeh Amaral <contato@teehamaral.me>
2016-09-15 14:51:35 -0300

    Change TwilioHandler to extend from TwimlAPIHandler

--- temba/channels/handlers.py
+++ temba/channels/handlers.py
@@ -30,21 +30,21 @@ from .tasks import fb_channel_subscribe
 from twilio import twiml
 
 
-class TwilioHandler(View):
+class TwimlAPIHandler(View):
 
     @disable_middleware
     def dispatch(self, *args, **kwargs):
-        return super(TwilioHandler, self).dispatch(*args, **kwargs)
+        return super(TwimlAPIHandler, self).dispatch(*args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("ILLEGAL METHOD")
+        return self.post(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         from twilio.util import RequestValidator
         from temba.msgs.models import Msg
 
         signature = request.META.get('HTTP_X_TWILIO_SIGNATURE', '')
-        url = "https://" + settings.TEMBA_HOST + "%s" % request.get_full_path()
+        url = "https://" + settings.HOSTNAME + "%s" % request.get_full_path()
 
         call_sid = request.REQUEST.get('CallSid', None)
         direction = request.REQUEST.get('Direction', None)
@@ -61,11 +61,11 @@ class TwilioHandler(View):
         if to_number and call_sid and direction == 'inbound' and status == 'ringing':
 
             # find a channel that knows how to answer twilio calls
-            channel = Channel.objects.filter(address=to_number, channel_type='T', role__contains='A', is_active=True).exclude(org=None).first()
+            channel = self.get_channel(to_number=to_number, type='ringing')
             if not channel:
                 raise Exception("No active answering channel found for number: %s" % to_number)
 
-            client = channel.org.get_twilio_client()
+            client = self.get_client(org=channel.org)
             validator = RequestValidator(client.auth[1])
             signature = request.META.get('HTTP_X_TWILIO_SIGNATURE', '')
 
@@ -109,6 +109,8 @@ class TwilioHandler(View):
                     return HttpResponse(unicode(response))
 
         action = request.GET.get('action', 'received')
+        channel_uuid = kwargs.get('uuid')
+
         # this is a callback for a message we sent
         if action == 'callback':
             smsId = request.GET.get('id', None)
@@ -117,9 +119,9 @@ class TwilioHandler(View):
             # get the SMS
             sms = Msg.all_messages.select_related('channel').get(id=smsId)
 
-            # validate this request is coming from twilio
+            # validate this request is coming from Twilio
             org = sms.org
-            client = org.get_twilio_client()
+            client = self.get_client(org=org)
             validator = RequestValidator(client.auth[1])
 
             if not validator.validate(url, request.POST, signature):
@@ -136,15 +138,12 @@ class TwilioHandler(View):
 
             return HttpResponse("", status=200)
 
-        # this is an incoming message that is being received by Twilio
         elif action == 'received':
-            channel = Channel.objects.filter(address=to_number, is_active=True).exclude(org=None).first()
+            channel = self.get_channel(channel_uuid=channel_uuid, to_number=to_number, type='received')
             if not channel:
-                raise Exception("No active channel found for number: %s" % to_number)
+                return HttpResponse("Channel with uuid: %s not found." % channel_uuid, status=404)
 
-            # validate this request is coming from twilio
-            org = channel.org
-            client = org.get_twilio_client()
+            client = self.get_client(org=channel.org)
             validator = RequestValidator(client.auth[1])
 
             if not validator.validate(url, request.POST, signature):
@@ -166,6 +165,38 @@ class TwilioHandler(View):
             return HttpResponse("", status=201)
 
         return HttpResponse("Not Handled, unknown action", status=400)
+
+    @classmethod
+    def get_channel(cls, to_number=None, channel_uuid=None, type='ringing'):
+        if type == 'ringing':
+            channel = Channel.objects.filter(address=to_number, channel_type=TWIML_API, role__contains='A', is_active=True).exclude(org=None).first()
+        elif type == 'received':
+            channel = Channel.objects.filter(uuid=channel_uuid, is_active=True, channel_type=TWIML_API).exclude(org=None).first()
+        else:
+            channel = None
+
+        return channel
+
+    @classmethod
+    def get_client(cls, org):
+        print(org.get_twiml_client())
+        return org.get_twiml_client()
+
+
+class TwilioHandler(TwimlAPIHandler):
+
+    def get_channel(cls, to_number=None, channel_uuid=None, type='ringing'):
+        if type == 'ringing':
+            channel = Channel.objects.filter(address=to_number, channel_type='T', role__contains='A', is_active=True).exclude(org=None).first()
+        elif type == 'received':
+            channel = Channel.objects.filter(address=to_number, is_active=True).exclude(org=None).first()
+        else:
+            channel = None
+
+        return channel
+
+    def get_client(cls, org):
+        return org.get_twilio_client()
 
 
 class TwilioMessagingServiceHandler(View):
@@ -201,142 +232,6 @@ class TwilioMessagingServiceHandler(View):
                 raise ValidationError("Invalid request signature")
 
             Msg.create_incoming(channel, URN.from_tel(request.POST['From']), request.POST['Body'])
-
-            return HttpResponse("", status=201)
-
-        return HttpResponse("Not Handled, unknown action", status=400)
-
-
-class TwimlAPIHandler(View):
-    @disable_middleware
-    def dispatch(self, *args, **kwargs):
-        return super(TwimlAPIHandler, self).dispatch(*args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        return self.post(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        from twilio.util import RequestValidator
-        from temba.msgs.models import Msg
-
-        signature = request.META.get('HTTP_X_TWILIO_SIGNATURE', '')
-        url = "https://" + settings.HOSTNAME + "%s" % request.get_full_path()
-
-        call_sid = request.REQUEST.get('CallSid', None)
-        direction = request.REQUEST.get('Direction', None)
-        status = request.REQUEST.get('CallStatus', None)
-        to_number = request.REQUEST.get('To', None)
-        to_country = request.REQUEST.get('ToCountry', None)
-        from_number = request.REQUEST.get('From', None)
-
-        # Twilio sometimes sends un-normalized numbers
-        if not to_number.startswith('+') and to_country:
-            to_number, valid = URN.normalize_number(to_number, to_country)
-
-        # see if it's a twilio call being initiated
-        if to_number and call_sid and direction == 'inbound' and status == 'ringing':
-
-            # find a channel that knows how to answer twilio calls
-            channel = Channel.objects.filter(address=to_number, channel_type=TWIML_API, role__contains='A', is_active=True).exclude(org=None).first()
-            if not channel:
-                raise Exception("No active answering channel found for number: %s" % to_number)
-
-            client = channel.org.get_twiml_client()
-            validator = RequestValidator(client.auth[1])
-            signature = request.META.get('HTTP_X_TWILIO_SIGNATURE', '')
-
-            base_url = settings.TEMBA_HOST
-            url = "https://%s%s" % (base_url, request.get_full_path())
-
-            if validator.validate(url, request.POST, signature):
-                from temba.ivr.models import IVRCall
-
-                # find a contact for the one initiating us
-                urn = URN.from_tel(from_number)
-                contact = Contact.get_or_create(channel.org, channel.created_by, urns=[urn], channel=channel)
-                urn_obj = contact.urn_objects[urn]
-
-                flow = Trigger.find_flow_for_inbound_call(contact)
-
-                call = IVRCall.create_incoming(channel, contact, urn_obj, flow, channel.created_by)
-                call.update_status(request.POST.get('CallStatus', None),
-                                   request.POST.get('CallDuration', None))
-                call.save()
-
-                if flow:
-                    FlowRun.create(flow, contact.pk, call=call)
-                    response = Flow.handle_call(call, {})
-                    return HttpResponse(unicode(response))
-                else:
-
-                    # we don't have an inbound trigger to deal with this call.
-                    response = twiml.Response()
-
-                    # say nothing and hangup, this is a little rude, but if we reject the call, then
-                    # they'll get a non-working number error. We send 'busy' when our server is down
-                    # so we don't want to use that here either.
-                    response.say('')
-                    response.hangup()
-
-                    # if they have a missed call trigger, fire that off
-                    Trigger.catch_triggers(contact, Trigger.TYPE_MISSED_CALL, channel)
-
-                    # either way, we need to hangup now
-                    return HttpResponse(unicode(response))
-
-        action = request.GET.get('action', 'received')
-        channel_uuid = kwargs['uuid']
-
-        # this is a callback for a message we sent
-        if action == 'callback':
-            smsId = request.GET.get('id', None)
-            status = request.POST.get('SmsStatus', None)
-
-            # get the SMS
-            sms = Msg.all_messages.select_related('channel').get(id=smsId)
-
-            # validate this request is coming from TwiML
-            org = sms.org
-            client = org.get_twiml_client()
-            validator = RequestValidator(client.auth[1])
-
-            if not validator.validate(url, request.POST, signature):
-                # raise an exception that things weren't properly signed
-                raise ValidationError("Invalid request signature")
-
-            # queued, sending, sent, failed, or received.
-            if status == 'sent':
-                sms.status_sent()
-            elif status == 'delivered':
-                sms.status_delivered()
-            elif status == 'failed':
-                sms.fail()
-
-            return HttpResponse("", status=200)
-
-        elif action == 'received':
-            channel = Channel.objects.filter(uuid=channel_uuid, is_active=True, channel_type=TWIML_API).exclude(org=None).first()
-            if not channel:
-                return HttpResponse("Channel with uuid: %s not found." % channel_uuid, status=404)
-
-            client = channel.org.get_twiml_client()
-            validator = RequestValidator(client.auth[1])
-
-            if not validator.validate(url, request.POST, signature):
-                # raise an exception that things weren't properly signed
-                raise ValidationError("Invalid request signature")
-
-            body = request.POST.get('Body')
-            urn = URN.from_tel(request.POST['From'])
-
-            # process any attached media
-            for i in range(int(request.POST.get('NumMedia', 0))):
-                media_url = client.download_media(request.POST['MediaUrl%d' % i])
-                path = media_url.partition(':')[2]
-                Msg.create_incoming(channel, urn, path, media=media_url)
-
-            if body:
-                Msg.create_incoming(channel, urn, body)
 
             return HttpResponse("", status=201)
 
