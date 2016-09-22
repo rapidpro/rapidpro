b49d606a2b95ee888e96358e901c9690f88c2ff4
Teeh Amaral <contato@teehamaral.me>
2016-09-22 13:01:02 -0300

    Add more tests

--- temba/channels/tests.py
+++ temba/channels/tests.py
@@ -1370,7 +1370,23 @@ class ChannelTest(TembaTest):
         response = self.client.post(claim_url, dict(number='5512345678', country='AA'))
         self.assertTrue(response.context['form'].errors)
 
-        response = self.client.post(claim_url, dict(country='US', number='5512345678', url='https://twilio.com', role='SR', account_sid='abcd1234', account_token='abcd1234'))
+        response = self.client.post(claim_url, dict(country='US', number='12345678', url='https://twilio.com', role='SR', account_sid='abcd1234', account_token='abcd1234'))
+        channel = self.org.channels.all().first()
+        self.assertRedirects(response, reverse('channels.channel_configuration', args=[channel.pk]))
+        self.assertEqual(channel.channel_type, "TW")
+        self.assertEqual(channel.config_json(), dict(ACCOUNT_TOKEN='abcd1234', send_url='https://twilio.com', ACCOUNT_SID='abcd1234'))
+
+        self.org.channels.update(is_active=False, org=None)
+
+        response = self.client.post(claim_url, dict(country='US', number='12345678', url='https://twilio.com', role='SR', account_sid='abcd4321', account_token='abcd4321'))
+        channel = self.org.channels.all().first()
+        self.assertRedirects(response, reverse('channels.channel_configuration', args=[channel.pk]))
+        self.assertEqual(channel.channel_type, "TW")
+        self.assertEqual(channel.config_json(), dict(ACCOUNT_TOKEN='abcd4321', send_url='https://twilio.com', ACCOUNT_SID='abcd4321'))
+
+        self.org.channels.update(is_active=False, org=None)
+
+        response = self.client.post(claim_url, dict(country='US', number='8080', url='https://twilio.com', role='SR', account_sid='abcd1234', account_token='abcd1234'))
         channel = self.org.channels.all().first()
         self.assertRedirects(response, reverse('channels.channel_configuration', args=[channel.pk]))
         self.assertEqual(channel.channel_type, "TW")
