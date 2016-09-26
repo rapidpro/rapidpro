2d744bfc26cdd8307f718e4ba9e6762169a587fc
Teeh Amaral <contato@teehamaral.me>
2016-09-26 09:52:41 -0300

    Add migrations

---
+++ temba/contacts/migrations/0043_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contacts', '0042_remove_exportcontactstask_host'),
+        ('contacts', '0042_auto_20160830_1727'),
+    ]
+
+    operations = [
+    ]
