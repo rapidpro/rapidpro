6bfa813ad88ce49e5718084ec07fcf186f06554e
Teeh Amaral <contato@teehamaral.me>
2016-09-28 01:17:32 -0300

    Change contact tests to support Line channel

--- temba/contacts/tests.py
+++ temba/contacts/tests.py
@@ -2392,7 +2392,7 @@ class ContactTest(TembaTest):
                                  dict(records=1, errors=2, creates=0, updates=1,
                                       error_messages=[dict(line=3,
                                                            error="Missing any valid URNs; at least one among phone, "
-                                                                 "twitter, telegram, email, facebook, external should be provided"),
+                                                                 "twitter, telegram, email, facebook, external, line should be provided"),
                                                       dict(line=4, error="Invalid Phone number 12345")]))
 
         # import a spreadsheet with a name and a twitter columns only
@@ -2448,7 +2448,7 @@ class ContactTest(TembaTest):
                                      dict(records=3, errors=1, creates=1, updates=2,
                                           error_messages=[dict(line=3,
                                                           error="Missing any valid URNs; at least one among phone, "
-                                                                "twitter, telegram, email, facebook, external should be provided")]))
+                                                                "twitter, telegram, email, facebook, external, line should be provided")]))
 
             # lock for creates only
             self.assertEquals(mock_lock.call_count, 1)
@@ -2515,7 +2515,7 @@ class ContactTest(TembaTest):
                                      dict(records=3, errors=1, creates=1, updates=2,
                                           error_messages=[dict(line=3,
                                                           error="Missing any valid URNs; at least one among phone, "
-                                                                "twitter, telegram, email, facebook, external should be provided")]))
+                                                                "twitter, telegram, email, facebook, external, line should be provided")]))
 
             # only lock for create
             self.assertEquals(mock_lock.call_count, 1)
@@ -2630,14 +2630,14 @@ class ContactTest(TembaTest):
         response = self.client.post(import_url, post_data)
         self.assertFormError(response, 'form', 'csv_file',
                              'The file you provided is missing a required header. At least one of "Phone", "Twitter", '
-                             '"Telegram", "Email", "Facebook", "External" should be included.')
+                             '"Telegram", "Email", "Facebook", "External", "Line" should be included.')
 
         csv_file = open('%s/test_imports/sample_contacts_missing_name_phone_headers.xls' % settings.MEDIA_ROOT, 'rb')
         post_data = dict(csv_file=csv_file)
         response = self.client.post(import_url, post_data)
         self.assertFormError(response, 'form', 'csv_file',
                              'The file you provided is missing a required header. At least one of "Phone", "Twitter", '
-                             '"Telegram", "Email", "Facebook", "External" should be included.')
+                             '"Telegram", "Email", "Facebook", "External", "Line" should be included.')
 
         csv_file = open('%s/test_imports/sample_contacts.xlsx' % settings.MEDIA_ROOT, 'rb')
         post_data = dict(csv_file=csv_file)
@@ -3410,7 +3410,7 @@ class ContactFieldTest(TembaTest):
         blocking_export.is_finished = True
         blocking_export.save()
 
-        with self.assertNumQueries(35):
+        with self.assertNumQueries(36):
             self.client.get(reverse('contacts.contact_export'), dict())
             task = ExportContactsTask.objects.all().order_by('-id').first()
 
@@ -3434,7 +3434,7 @@ class ContactFieldTest(TembaTest):
         contact4 = self.create_contact('Stephen', '+12078778899', twitter='stephen')
         ContactURN.create(self.org, contact, 'tel:+12062233445')
 
-        with self.assertNumQueries(35):
+        with self.assertNumQueries(36):
             self.client.get(reverse('contacts.contact_export'), dict())
             task = ExportContactsTask.objects.all().order_by('-id').first()
 
@@ -3607,54 +3607,58 @@ class ContactFieldTest(TembaTest):
 
         response_json = json.loads(response.content)
 
-        self.assertEquals(len(response_json), 40)
+        self.assertEquals(len(response_json), 41)
         self.assertEquals(response_json[0]['label'], 'Full name')
         self.assertEquals(response_json[0]['key'], 'name')
-        self.assertEquals(response_json[1]['label'], 'External identifier')
-        self.assertEquals(response_json[1]['key'], 'ext')
-        self.assertEquals(response_json[2]['label'], 'Facebook identifier')
-        self.assertEquals(response_json[2]['key'], 'facebook')
-        self.assertEquals(response_json[3]['label'], 'Email address')
-        self.assertEquals(response_json[3]['key'], 'mailto')
-        self.assertEquals(response_json[4]['label'], 'Telegram identifier')
-        self.assertEquals(response_json[4]['key'], 'telegram')
-        self.assertEquals(response_json[5]['label'], 'Twitter handle')
-        self.assertEquals(response_json[5]['key'], 'twitter')
-        self.assertEquals(response_json[6]['label'], 'Phone number')
-        self.assertEquals(response_json[6]['key'], 'tel_e164')
-        self.assertEquals(response_json[7]['label'], 'Groups')
-        self.assertEquals(response_json[7]['key'], 'groups')
-        self.assertEquals(response_json[8]['label'], 'First')
-        self.assertEquals(response_json[8]['key'], 'first')
-        self.assertEquals(response_json[9]['label'], 'label0')
-        self.assertEquals(response_json[9]['key'], 'key0')
+        self.assertEquals(response_json[1]['label'], 'LINE identifier')
+        self.assertEquals(response_json[1]['key'], 'line')
+        self.assertEquals(response_json[2]['label'], 'External identifier')
+        self.assertEquals(response_json[2]['key'], 'ext')
+        self.assertEquals(response_json[3]['label'], 'Facebook identifier')
+        self.assertEquals(response_json[3]['key'], 'facebook')
+        self.assertEquals(response_json[4]['label'], 'Email address')
+        self.assertEquals(response_json[4]['key'], 'mailto')
+        self.assertEquals(response_json[5]['label'], 'Telegram identifier')
+        self.assertEquals(response_json[5]['key'], 'telegram')
+        self.assertEquals(response_json[6]['label'], 'Twitter handle')
+        self.assertEquals(response_json[6]['key'], 'twitter')
+        self.assertEquals(response_json[7]['label'], 'Phone number')
+        self.assertEquals(response_json[7]['key'], 'tel_e164')
+        self.assertEquals(response_json[8]['label'], 'Groups')
+        self.assertEquals(response_json[8]['key'], 'groups')
+        self.assertEquals(response_json[9]['label'], 'First')
+        self.assertEquals(response_json[9]['key'], 'first')
+        self.assertEquals(response_json[10]['label'], 'label0')
+        self.assertEquals(response_json[10]['key'], 'key0')
 
         ContactField.objects.filter(org=self.org, key='key0').update(label='AAAA')
 
         response = self.client.get(contact_field_json_url)
         response_json = json.loads(response.content)
 
-        self.assertEquals(len(response_json), 40)
+        self.assertEquals(len(response_json), 41)
         self.assertEquals(response_json[0]['label'], 'Full name')
         self.assertEquals(response_json[0]['key'], 'name')
-        self.assertEquals(response_json[1]['label'], 'External identifier')
-        self.assertEquals(response_json[1]['key'], 'ext')
-        self.assertEquals(response_json[2]['label'], 'Facebook identifier')
-        self.assertEquals(response_json[2]['key'], 'facebook')
-        self.assertEquals(response_json[3]['label'], 'Email address')
-        self.assertEquals(response_json[3]['key'], 'mailto')
-        self.assertEquals(response_json[4]['label'], 'Telegram identifier')
-        self.assertEquals(response_json[4]['key'], 'telegram')
-        self.assertEquals(response_json[5]['label'], 'Twitter handle')
-        self.assertEquals(response_json[5]['key'], 'twitter')
-        self.assertEquals(response_json[6]['label'], 'Phone number')
-        self.assertEquals(response_json[6]['key'], 'tel_e164')
-        self.assertEquals(response_json[7]['label'], 'Groups')
-        self.assertEquals(response_json[7]['key'], 'groups')
-        self.assertEquals(response_json[8]['label'], 'AAAA')
-        self.assertEquals(response_json[8]['key'], 'key0')
-        self.assertEquals(response_json[9]['label'], 'First')
-        self.assertEquals(response_json[9]['key'], 'first')
+        self.assertEquals(response_json[1]['label'], 'LINE identifier')
+        self.assertEquals(response_json[1]['key'], 'line')
+        self.assertEquals(response_json[2]['label'], 'External identifier')
+        self.assertEquals(response_json[2]['key'], 'ext')
+        self.assertEquals(response_json[3]['label'], 'Facebook identifier')
+        self.assertEquals(response_json[3]['key'], 'facebook')
+        self.assertEquals(response_json[4]['label'], 'Email address')
+        self.assertEquals(response_json[4]['key'], 'mailto')
+        self.assertEquals(response_json[5]['label'], 'Telegram identifier')
+        self.assertEquals(response_json[5]['key'], 'telegram')
+        self.assertEquals(response_json[6]['label'], 'Twitter handle')
+        self.assertEquals(response_json[6]['key'], 'twitter')
+        self.assertEquals(response_json[7]['label'], 'Phone number')
+        self.assertEquals(response_json[7]['key'], 'tel_e164')
+        self.assertEquals(response_json[8]['label'], 'Groups')
+        self.assertEquals(response_json[8]['key'], 'groups')
+        self.assertEquals(response_json[9]['label'], 'AAAA')
+        self.assertEquals(response_json[9]['key'], 'key0')
+        self.assertEquals(response_json[10]['label'], 'First')
+        self.assertEquals(response_json[10]['key'], 'first')
 
 
 class URNTest(TembaTest):
