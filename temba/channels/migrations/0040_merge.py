9d2d28a0e98e5a18f437b520ddd11ce6d571e971
Teeh Amaral <contato@teehamaral.me>
2016-10-06 09:18:49 -0300

    Add migrations merge

---
+++ temba/channels/migrations/0040_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('channels', '0038_auto_20161004_1538'),
+        ('channels', '0039_auto_20161005_2052'),
+    ]
+
+    operations = [
+    ]
