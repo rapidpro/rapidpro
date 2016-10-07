7d33bb9526fa51578319c6eed9d83249010b6a37
Nic Pottier <nicpottier@gmail.com>
2016-10-07 14:06:33 -0500

    assert that send_msgs is only called once

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -6324,11 +6324,17 @@ class ParentChildOrderingTest(FlowFileTest):
                                       config=dict(send_url='https://google.com'), uuid='00000000-0000-0000-0000-000000001234')
 
     def test_parent_child_ordering(self):
+        from temba.channels.tasks import send_msg_task
+
         # start our parent flow
         flow = self.get_flow('parent_child_ordering')
-        flow.start([], [self.contact])
 
-        # get the msgs for our contact
-        msgs = Msg.objects.filter(contact=self.contact).order_by('sent_on')
-        self.assertEquals(msgs[0].text, "Parent 1")
-        self.assertEquals(msgs[1].text, "Child Msg")
+        with patch('temba.channels.tasks.send_msg_task', wraps=send_msg_task) as mock_send_msg:
+            flow.start([], [self.contact])
+
+            # get the msgs for our contact
+            msgs = Msg.objects.filter(contact=self.contact).order_by('sent_on')
+            self.assertEquals(msgs[0].text, "Parent 1")
+            self.assertEquals(msgs[1].text, "Child Msg")
+
+            self.assertEqual(mock_send_msg.call_count, 1)
