03319ed91c0afb7b0ff112b2b6783d4769c10ed9
Rowan Seymour <rowanseymour@gmail.com>
2016-10-10 10:38:57 +0200

    Add custom HAML teplate loader which is compatible with Django 1.9

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -105,8 +105,8 @@ SECRET_KEY = 'your own secret key'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'hamlpy.template.loaders.HamlPyFilesystemLoader',
-    'hamlpy.template.loaders.HamlPyAppDirectoriesLoader',
+    'temba.utils.haml.HamlFilesystemLoader',
+    'temba.utils.haml.HamlAppDirectoriesLoader',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
     'django.template.loaders.eggs.Loader',
