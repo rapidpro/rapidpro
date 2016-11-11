7004de1c7f90515fa02847ac406773397e6d06fb
Teeh Amaral <contato@teehamaral.me>
2016-11-11 19:39:26 -0300

    Add migrations

--- temba/contacts/migrations/0043_auto_20161111_1850.py
+++ temba/contacts/migrations/0043_auto_20161111_1850.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0041_indexes_update'),
+        ('contacts', '0042_remove_exportcontactstask_host'),
     ]
 
     operations = [
