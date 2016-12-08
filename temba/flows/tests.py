e18a2691b0f10c7ed1e9994ab83c99cfcf130d2e
Norbert Kwizera <norkans7@gmail.com>
2016-12-08 14:11:29 +0200

    change id of flow in exported json

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -4167,18 +4167,7 @@ class FlowsTest(FlowFileTest):
 
         # we are going to expire, but we want runs across two different flows
         # to make sure that our optimization for expiration is working properly
-        self.get_flow('color_gender_age')
-        print "=" * 80
-        print "=" * 80
-
-        for imp_flow in Flow.objects.all():
-            print imp_flow.__dict__
-            print "+" * 80
-
-        print "=" * 80
-        print "=" * 80
-
-        cga_flow = Flow.objects.get(name="Color Age Gender")
+        cga_flow = self.get_flow('color_gender_age')
         self.assertEquals("What is your gender?", self.send_message(cga_flow, "Red"))
         self.assertEquals(1, len(cga_flow.get_activity()[0]))
 
