1540ce10c072994c6e107c831c6a260699ce1cfa
Gyorgy Fekete <f@pulilab.com>
2016-12-14 15:12:48 +0100

    fix migrations

--- temba/msgs/migrations/0070_auto_20161201_1639.py
+++ temba/msgs/migrations/0070_auto_20161201_1639.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('msgs', '0067_auto_20161005_0731'),
+        ('msgs', '0069_populate_broadcast_base_lang'),
     ]
 
     operations = [
