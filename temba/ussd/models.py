a6045c87d4886e000ba4debf2773fcfc58782fb2
Gyorgy Fekete <f@pulilab.com>
2016-12-19 18:03:29 +0100

    remove unused import

--- temba/ussd/models.py
+++ temba/ussd/models.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.contrib.auth.models import User
 from django.db import models
 from temba.channels.models import ChannelSession
 from temba.contacts.models import Contact, URN
