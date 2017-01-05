6d1a795a8c58222a92822e3c7af7ebd6ab15d548
Nic Pottier <nicpottier@gmail.com>
2017-01-05 13:48:46 -0800

    dont propogate invalid host errors, just swallow them

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -274,6 +274,10 @@ LOGGING = {
             'handlers': ['console'],
             'propagate': False,
         },
+        'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
         'django.db.backends': {
             'level': 'ERROR',
             'handlers': ['console'],
