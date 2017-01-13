dd4c240ef741962800a44e2399fb41f666ebbfa5
Nic Pottier <nicpottier@gmail.com>
2017-01-13 13:20:13 -0800

    log all celery exceptions

--- temba/temba_celery.py
+++ temba/temba_celery.py
@@ -1,12 +1,26 @@
 from __future__ import absolute_import, unicode_literals
 
+import celery
 import os
+import raven
 import sys
-from celery import Celery
+
 from django.conf import settings
+from raven.contrib.celery import register_signal, register_logger_signal
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temba.settings')
+
+
+# Custom Celery so that we can hook in raven
+class Celery(celery.Celery):
+
+    def on_configure(self):
+        client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+
+        # register raven for error tracking
+        register_logger_signal(client)
+        register_signal(client)
 
 app = Celery('temba')
 
