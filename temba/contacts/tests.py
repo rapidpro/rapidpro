66fb16981bc4bf74ae1e8c5f65bfdd9e2c1e2c0d
Eric Newcomer <ericnewcomer@gmail.com>
2017-01-16 18:11:27 -0500

    add coverage

--- temba/contacts/tests.py
+++ temba/contacts/tests.py
@@ -1218,9 +1218,13 @@ class ContactTest(TembaTest):
         self.create_campaign()
 
         # create some messages
-        for i in range(100):
+        for i in range(99):
             self.create_msg(direction='I', contact=self.joe, text="Inbound message %d" % i,
                             created_on=timezone.now() - timedelta(days=(100 - i)))
+
+        # add one that is a video
+        self.create_msg(direction='I', contact=self.joe, media="video:http://blah/file.mp4",
+                        created_on=timezone.now())
 
         # because messages are stored with timestamps from external systems, possible to have initial message
         # which is little bit older than the contact itself
@@ -1256,7 +1260,9 @@ class ContactTest(TembaTest):
         self.assertEqual(activity[2].direction, 'O')
         self.assertIsInstance(activity[3], FlowRun)
         self.assertIsInstance(activity[4], Msg)
-        self.assertEqual(activity[4].text, "Inbound message 99")
+        self.assertEqual(activity[4].media, "video:http://blah/file.mp4")
+        self.assertIsInstance(activity[5], Msg)
+        self.assertEqual(activity[5].text, "Inbound message 98")
         self.assertIsInstance(activity[8], EventFire)
         self.assertEqual(activity[-1].text, "Inbound message 11")
 
@@ -1311,7 +1317,8 @@ class ContactTest(TembaTest):
 
         # with our recent flag on, should not see the older messages
         activity = response.context['activity']
-        self.assertEqual(len(activity), 5)
+        self.assertEqual(len(activity), 6)
+        self.assertContains(response, 'video')
 
         # can't view history of contact in another org
         self.create_secondary_org()
