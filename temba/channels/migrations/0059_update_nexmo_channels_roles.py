420066d7e8866ab743a276d066010668428fef22
Norbert Kwizera <norkans7@gmail.com>
2017-02-02 22:25:11 +0200

    run migrations only in production

--- temba/channels/migrations/0059_update_nexmo_channels_roles.py
+++ temba/channels/migrations/0059_update_nexmo_channels_roles.py
@@ -16,40 +16,41 @@ class Migration(migrations.Migration):
     def update_nexmo_channels_roles(apps, schema_editor):
         Channel = apps.get_model('channels', 'Channel')
 
-        nexmo_channels = Channel.objects.filter(channel_type='NX').exclude(org=None)
+        if settings.IS_PROD:
+            nexmo_channels = Channel.objects.filter(channel_type='NX').exclude(org=None)
 
-        updated = []
-        updated_orgs = []
-        for channel in nexmo_channels:
-            try:
-                org = Org.objects.get(pk=channel.org_id)
+            updated = []
+            updated_orgs = []
+            for channel in nexmo_channels:
+                try:
+                    org = Org.objects.get(pk=channel.org_id)
 
-                if org.pk not in updated_orgs:
-                    org_uuid = org.config_json().get(NEXMO_UUID)
-                    nexmo_api_key = org.config_json().get(NEXMO_KEY, None)
-                    nexmo_secret = org.config_json().get(NEXMO_SECRET, None)
+                    if org.pk not in updated_orgs:
+                        org_uuid = org.config_json().get(NEXMO_UUID)
+                        nexmo_api_key = org.config_json().get(NEXMO_KEY, None)
+                        nexmo_secret = org.config_json().get(NEXMO_SECRET, None)
 
-                    org.connect_nexmo(nexmo_api_key, nexmo_secret, org.created_by)
-                    org.refresh_from_db()
+                        org.connect_nexmo(nexmo_api_key, nexmo_secret, org.created_by)
+                        org.refresh_from_db()
 
-                    updated_orgs.append(org.pk)
+                        updated_orgs.append(org.pk)
 
-                    app_id = org.config_json().get(NEXMO_APP_ID, None)
+                        app_id = org.config_json().get(NEXMO_APP_ID, None)
 
-                    nexmo_client = org.get_nexmo_client()
+                        nexmo_client = org.get_nexmo_client()
 
-                    mo_path = reverse('handlers.nexmo_handler', args=['receive', org_uuid])
+                        mo_path = reverse('handlers.nexmo_handler', args=['receive', org_uuid])
 
-                    nexmo_client.update_nexmo_number(channel.country, channel.address,
-                                                     'http://%s%s' % (settings.TEMBA_HOST, mo_path),
-                                                     app_id)
+                        nexmo_client.update_nexmo_number(channel.country, channel.address,
+                                                         'http://%s%s' % (settings.TEMBA_HOST, mo_path),
+                                                         app_id)
 
-                updated.append(channel.id)
-            except Exception:
-                pass
+                    updated.append(channel.id)
+                except Exception:
+                    pass
 
-        # change role for those we successfully updated the callback URLs
-        Channel.objects.filter(id__in=updated).update(role='SRCA')
+            # change role for those we successfully updated the callback URLs
+            Channel.objects.filter(id__in=updated).update(role='SRCA')
 
     operations = [
         migrations.RunPython(update_nexmo_channels_roles)
