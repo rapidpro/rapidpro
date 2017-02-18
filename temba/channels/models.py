f594ee3d70c79c4c87023f4160953aa5fd78690a
Eric Newcomer <ericnewcomer@gmail.com>
2017-02-18 09:42:51 -0500

    add session expiration test for ussd

--- temba/channels/models.py
+++ temba/channels/models.py
@@ -3571,7 +3571,7 @@ class ChannelSession(SmartModel):
     def is_ivr(self):
         return self.session_type == self.IVR
 
-    def close(self):
+    def close(self):  # pragma: no cover
         pass
 
     def get(self):
