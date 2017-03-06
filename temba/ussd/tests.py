d7a87fe6214af7084499544015d48d57fb47752a
Gyorgy Fekete <f@pulilab.com>
2017-03-06 15:58:08 +0100

    USSD Session tests for all possible session closing cases

--- temba/ussd/tests.py
+++ temba/ussd/tests.py
@@ -146,6 +146,246 @@ class USSDSessionTest(TembaTest):
         self.assertIsInstance(session.ended_on, datetime)
         self.assertEqual(session.external_id, "21345")
 
+    def test_end_with_menu_no_destination(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with no destination connected
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="4",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be 2 outgoing messages, one with an empty message to close down the session gracefully
+        self.assertEqual(Msg.objects.count(), 3)
+        self.assertEqual(Msg.objects.filter(direction='O').count(), 2)
+
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertNotEqual(msgs[0].text, '')
+        self.assertEqual(msgs[1].text, '')
+
+        # the session should be marked as "ENDING"
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_end_with_menu_destination_with_messaging(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another wait_menu ruleset
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="1",
+                                              date=timezone.now(), external_id="21345")
+
+        # the session shouldn't be marked as ending
+        self.assertEqual(session.status, USSDSession.IN_PROGRESS)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_end_with_menu_destination_without_messaging(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another wait_menu ruleset
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="test",
+                                              date=timezone.now(), external_id="21345")
+
+        # the session shouldn't be marked as ending
+        self.assertEqual(session.status, USSDSession.IN_PROGRESS)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+        # choose option with destination connected to actions (add to groups and set language later)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="2",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with no content to close the session
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, '')
+
+        # the session should be marked as ending at this point, cos' there's no more messaging destination in the flow
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_end_with_arbitrary_rules(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to the arbitarty ussd ruleset
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="3",
+                                              date=timezone.now(), external_id="21345")
+
+        # the session shouldn't be marked as ending
+        self.assertEqual(session.status, USSDSession.IN_PROGRESS)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+        # now we are at the ussd response, choose an option here
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="3",
+                                              date=timezone.now(), external_id="21345")
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_end_with_end_action(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to the arbitarty ussd ruleset
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="3",
+                                              date=timezone.now(), external_id="21345")
+
+        # the session shouldn't be marked as ending
+        self.assertEqual(session.status, USSDSession.IN_PROGRESS)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+        # choose the Maybe option which leads to a USSD End with message action
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="Maybe",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with the message of the end action
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, 'perfect, thanks')
+
+        # same session is modified
+        self.assertEqual(USSDSession.objects.count(), 1)
+
+        session.refresh_from_db()
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_end_with_end_action_with_more_actions_in_actionset(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to a set of actions including an end session w message action
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="2",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with the message of the end action
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, 'Im sorry, we will contact you')
+
+        session.refresh_from_db()
+
+        # the session should be marked as ending
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_ruleset_through_ruleset_no_destination(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another type of ruleset (Split by Expression)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="6",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with empty msg just to close the session gracefully
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, '')
+
+        session.refresh_from_db()
+        # the session should be marked as ending since the next ruleset doesn't have any destination
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_ruleset_through_ruleset_with_third_ruleset_destination(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another type of ruleset (Split by Expression)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="5",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with the message of the end action
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, 'a')
+
+        session.refresh_from_db()
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_ruleset_through_two_rulesets_ending_with_not_messaging_action(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another type of ruleset (Split by Expression)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="8",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with empty message
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, '')
+
+        session.refresh_from_db()
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_ruleset_through_ruleset_ending_with_end_action(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another type of ruleset (Split by Expression)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="9",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with the message of the end action
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, 'c')
+
+        session.refresh_from_db()
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
+    def test_ruleset_through_rulesets_ending_with_no_action(self):
+        flow = self.get_flow('ussd_session_end')
+        contact = self.create_contact("Joe", "+250788383383")
+        flow.start([], [contact])
+
+        # choose option with destination connected to another type of ruleset (Split by Expression)
+        session = USSDSession.handle_incoming(channel=self.channel, urn="+250788383383", content="7",
+                                              date=timezone.now(), external_id="21345")
+
+        # there should be a last message with empty message
+        msgs = Msg.objects.filter(direction='O').order_by('id')
+
+        self.assertEqual(msgs.last().text, '')
+
+        session.refresh_from_db()
+
+        self.assertEqual(session.status, USSDSession.ENDING)
+        self.assertIsNone(session.ended_on)
+        self.assertEqual(session.external_id, "21345")
+
 
 class VumiUssdTest(TembaTest):
 
