2ad4ed082c8091d87b7cbc7e920becd3357d990f
Simon de Haan <simon@praekelt.org>
2017-04-05 10:14:56 +0200

    love for longer lines (thanks @rowanseymour)

--- temba/locations/management/commands/download_geojson.py
+++ temba/locations/management/commands/download_geojson.py
@@ -1,6 +1,8 @@
+from __future__ import print_function, unicode_literals
+
 import os
 import requests
-import re
+import regex
 
 from django.core.management.base import BaseCommand
 
@@ -10,23 +12,13 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('relation_ids', nargs='+')
-        parser.add_argument('--oauth-token',
-                            dest='oauth_token',
-                            default=None,
-                            help='The OAuth token to use when authenticating '
-                                 'to GitHub. Unauthenticated requests are '
-                                 'rate limited to 60 per hour. Defaults to '
-                                 'None')
-        parser.add_argument('--repo',
-                            dest='repo',
-                            default='nyaruka/posm-extracts',
-                            help="The GitHub posm-extracts repo to use. "
-                                 "Defaults to nyaruka/posm-extracts")
-        parser.add_argument('--dir',
-                            dest='dir',
-                            default='geojson',
-                            help='The directory to write the geojson files '
-                                 'to. Defaults to `geojson`')
+        parser.add_argument('--oauth-token', dest='oauth_token', default=None,
+                            help='The OAuth token to use when authenticating to GitHub. Unauthenticated requests are '
+                                 'rate limited to 60 per hour. Defaults to None')
+        parser.add_argument('--repo', dest='repo', default='nyaruka/posm-extracts',
+                            help="The GitHub posm-extracts repo to use. Defaults to nyaruka/posm-extracts")
+        parser.add_argument('--dir', dest='dir', default='geojson',
+                            help='The directory to write the geojson files to. Defaults to `geojson`')
 
     def handle(self, *args, **options):
 
@@ -43,28 +35,22 @@ class Command(BaseCommand):
             headers = {}
 
         data = requests.get(
-            "https://api.github.com/repos/%s/git/trees/master" % (
-                repo,), headers=headers).json()
+            "https://api.github.com/repos/%s/git/trees/master" % (repo,), headers=headers).json()
         [geojson] = filter(lambda obj: obj['path'] == "geojson", data['tree'])
         geojson_sha = geojson['sha']
 
-        files = requests.get(
-            'https://api.github.com/repos/%s/git/trees/%s' % (
-                repo, geojson_sha,), headers=headers).json()
+        files = requests.get('https://api.github.com/repos/%s/git/trees/%s' % (repo, geojson_sha,),
+                             headers=headers).json()
 
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir)
 
         for relation_id in relation_ids:
             relation_files = filter(
-                lambda obj: re.match(r'R%s.*_simplified.json' % (relation_id,),
-                                     obj['path']), files['tree'])
+                lambda obj: regex.match(r'R%s.*_simplified.json' % (relation_id,), obj['path']), files['tree'])
             for relation_file in relation_files:
-                destination = os.path.join(
-                    destination_dir, relation_file['path'])
+                destination = os.path.join(destination_dir, relation_file['path'])
                 with open(destination, 'w') as fp:
-                    response = requests.get(
-                        'https://raw.githubusercontent.com/%s/'
-                        'master/geojson/%s' % (
-                            repo, relation_file['path']), headers=headers)
+                    response = requests.get('https://raw.githubusercontent.com/%s/master/geojson/%s' % (
+                                            repo, relation_file['path']), headers=headers)
                     fp.write(response.content)
