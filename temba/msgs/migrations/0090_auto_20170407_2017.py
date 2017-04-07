997c35e2efa9d4213302304200ff4a913f837725
Norbert Kwizera <norkans7@gmail.com>
2017-04-07 22:26:51 +0200

    Fix migrations

--- temba/msgs/migrations/0090_auto_20170407_2017.py
+++ temba/msgs/migrations/0090_auto_20170407_2017.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('msgs', '0087_populate_broadcast_send_all'),
+        ('msgs', '0089_populate_broadcast_send_all'),
     ]
 
     operations = [
