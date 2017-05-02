3ee7c6b631e8d455b66ce2b077a93714f102eded
Rowan Seymour <rowanseymour@gmail.com>
2017-05-02 09:37:04 -0500

    Really fix migration

--- temba/msgs/migrations/0093_populate_translatables.py
+++ temba/msgs/migrations/0093_populate_translatables.py
@@ -9,30 +9,39 @@ from django.db import migrations
 from temba.utils import chunk_list
 
 
-def populate_translatables(Broadcast):
-    broadcast_ids = list(Broadcast.objects.values_list('id', flat=True))
+def populate_translatables(Org, Broadcast):
+    broadcast_ids = list(Broadcast.objects.filter(translations=None).values_list('id', flat=True))
     if not broadcast_ids:
         return
 
     print("Fetched %d broadcast ids to be updated..." % len(broadcast_ids))
-    num_updated = 0
 
+    primary_lang_by_org_id = {}
+    for org in Org.objects.select_related('primary_language'):
+        primary_lang_by_org_id[org.id] = org.primary_language.iso_code if org.primary_language else None
+
+    print("Fetched %d org primary languages..." % (len(primary_lang_by_org_id)))
+
+    num_updated = 0
     start = time.time()
 
     for id_batch in chunk_list(broadcast_ids, 5000):
-        batch = Broadcast.objects.filter(id__in=id_batch).prefetch_related('org__primary_language')
+        batch = Broadcast.objects.filter(id__in=id_batch)
         for broadcast in batch:
+            org_language = primary_lang_by_org_id[broadcast.org_id]
+
             # Figure out base language for this broadcast
             if broadcast.base_language:
                 base_language = broadcast.base_language
-            elif broadcast.org.primary_language:
-                base_language = broadcast.org.primary_language.iso_code
+            elif org_language:
+                base_language = org_language
             else:
                 base_language = 'base'
 
-            if broadcast.language_dict:
-                translations = json.loads(broadcast.language_dict)
+            translations = json.loads(broadcast.language_dict) if broadcast.language_dict else None
 
+            # We might have a valid translations dict, an empty dict or none
+            if translations:
                 # We have some broadcasts with language dicts with language keys other than anything calculated above.
                 # In this case best we can do is a pick a random key so at least base_language exists in language_dict
                 if base_language not in translations:
@@ -62,12 +71,14 @@ def populate_translatables(Broadcast):
 
 def apply_manual():
     from temba.msgs.models import Broadcast
-    populate_translatables(Broadcast)
+    from temba.orgs.models import Org
+    populate_translatables(Org, Broadcast)
 
 
 def apply_as_migration(apps, schema_editor):
     Broadcast = apps.get_model('msgs', 'Broadcast')
-    populate_translatables(Broadcast)
+    Org = apps.get_model('orgs', 'Org')
+    populate_translatables(Org, Broadcast)
 
 
 class Migration(migrations.Migration):
