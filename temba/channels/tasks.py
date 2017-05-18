570c961148e5977ec5c89cc1a68cf096705ccfa3
Rowan Seymour <rowanseymour@gmail.com>
2017-05-18 16:01:16 -0500

    Add attachments attribute to MsgStruct regardless of whether media is sert

--- temba/channels/tasks.py
+++ temba/channels/tasks.py
@@ -65,8 +65,8 @@ def send_msg_task():
                                      datetime_fields=['modified_on', 'sent_on', 'created_on', 'queued_on',
                                                       'next_attempt'])
 
-                if not hasattr(msg, 'attachments') and hasattr(msg, 'media') and msg.media:  # pragma: no cover
-                    msg.attachments = [msg.media]
+                if not hasattr(msg, 'attachments') and hasattr(msg, 'media'):  # pragma: no cover
+                    msg.attachments = [msg.media] if msg.media else []
 
                 Channel.send_message(msg)
 
