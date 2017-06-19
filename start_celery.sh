e8c0a0900ff56aa61794f5cef05140994089b4c6
mvilchis <mvilchis@ciencias.unam.mx>
2017-06-19 18:00:56 -0500

    Change if order celery start

--- start_celery.sh
+++ start_celery.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 ldconfig
 #################  Celery configuration  #################
-nohup celery  --app=temba  worker --loglevel=INFO --queues=$CELERY_QUEUE --concurrency=$CELERY_WORKERS>/cel.out 2>&1&
-celery -A temba flower --port=5555 
+if [ -z "$CELERY_BEAT" ]; then
+  echo "Celery worker"
+  celery  --app=temba  worker --loglevel=INFO --queues=$CELERY_QUEUE --concurrency=$CELERY_WORKERS
+else
+  nohup celery  --beat --app=temba  worker --loglevel=INFO --queues=celery --concurrency=$CELERY_WORKERS>/cel.out 2>&1&
+  echo "Celery beat"
+  celery -A temba flower --port=5555 
+fi
 #################       End Celery       #################
