144befb0a1e3d0665950011bc5a8eb452f3ba2bb
Teeh Amaral <contato@teehamaral.me>
2017-08-14 16:05:28 -0300

    Remove unnecessary vars

--- temba/orgs/models.py
+++ temba/orgs/models.py
@@ -114,7 +114,6 @@ CHATBASE_TYPE_AGENT = 'agent'
 CHATBASE_TYPE_USER = 'user'
 CHATBASE_FEEDBACK = 'CHATBASE_FEEDBACK'
 CHATBASE_VERSION = 'CHATBASE_VERSION'
-CHATBASE_BATCH_SIZE = 500
 
 ORG_STATUS = 'STATUS'
 SUSPENDED = 'suspended'
@@ -136,7 +135,6 @@ ORG_ACTIVE_TOPUP_KEY = 'org:%d:cache:active_topup'
 ORG_ACTIVE_TOPUP_REMAINING = 'org:%d:cache:credits_remaining:%d'
 ORG_CREDIT_EXPIRING_CACHE_KEY = 'org:%d:cache:credits_expiring_soon'
 ORG_LOW_CREDIT_THRESHOLD_CACHE_KEY = 'org:%d:cache:low_credits_threshold'
-ORG_CHATBASE_LOG_CACHE_KEY = 'org:%d:cache:chatbase_log'
 
 ORG_LOCK_TTL = 60  # 1 minute
 ORG_CREDITS_CACHE_TTL = 7 * 24 * 60 * 60  # 1 week
