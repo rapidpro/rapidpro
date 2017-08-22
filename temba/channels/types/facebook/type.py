dfcf53789b25f61d6ce0339d87c497397ea4cdfc
mvilchis <mvilchis@ciencias.unam.mx>
2017-08-22 14:34:27 -0500

    Version 2,facebook bot

--- temba/channels/types/facebook/type.py
+++ temba/channels/types/facebook/type.py
@@ -55,15 +55,49 @@ class FacebookType(ChannelType):
     def send(self, channel, msg, text):
         # build our payload
         payload = {'message': {'text': text}}
-        #if 'pruebaprueba' in text:
-        options = re.compile("(?<!^)\s+(?=[0-9]+[:\)\.])(?!.\s)").split(text)
-        if len(options) > 1:
-            list_dic = [{"content_type":"text",
-                        "title":t,
-                        "payload":(re.findall(r'^\D*(\d+)', t)[0])
-                        } for t in options[1:]]
-            payload = {'message': {'text': options[0],'quick_replies' : list_dic}}
-
+        ###################### MX ABIERTO   ######################
+        # Parse options like "1.- .. 2.- "
+        text = re.compile("\([0-9]+-[0-9]\)").split(t)[0]
+        end = [(m.end(0)) for m in re.finditer("[0-9]+[:\.\)](-)?", text)]
+        start = [(m.start(0)) for m in re.finditer("[0-9]+[:\.\)](-)?", text)]
+        start.append(len(text))
+        if end and start:
+            title = text[0:start[0]].strip()
+            options = [text[s:e].strip()
+                        for s,e in zip(end,start[1:])
+                          if text[s:e].strip()]
+            valid_items = len(options)
+            numbers = [re.sub("\D", "",text[s:e].strip())
+                         for s,e in zip(start[:valid_items], end[:valid_items])]
+            #### Maybe last element has explanation not necesary now
+            options[-1] = re.compile("([\(\.]+)").split(options[-1])[0]
+            #If all options has lenght less than 20, send like list option
+            if (len(max(options, key=len)) <= 20):
+                elements = [ {"type":"postback",  "title":o, "payload":n }
+                             for o, n in zip(options, numbers) if o]
+                payload = {"message":
+                              {"attachment":
+                                {"type":"template",
+                                  "payload":{
+                                    "template_type":"generic",
+                                    "elements": {
+                                    "title":title,
+                                    "subtitle":"",
+                                    "buttons": elements,
+                            }}}}}
+            #If is too large, only send number like options
+            else:
+                elements = [{"content_type":"text",  "title":n, "payload":n}
+                            for n,o in zip(numbers,options) if o]
+                payload = {'message': {'text': title,'quick_replies' : elements}}
+        # Parse yes/no
+        # If last item is (Si/No) or Si/No
+        has_yesno = re.findall("Si/No",(t.split()[-1]))
+        if has_yesno:
+            title = ' '.join(t.split()[:-1])
+            elements = [{"content_type":"text",  "title":"Si", "payload":"Si"},
+                        {"content_type":"text",  "title":"No", "payload":"No"}]
+            payload = {'message': {'text': title,'quick_replies' : elements}}
         # this is a ref facebook id, temporary just for this message
         if URN.is_path_fb_ref(msg.urn_path):
             payload['recipient'] = dict(user_ref=URN.fb_ref_from_path(msg.urn_path))
