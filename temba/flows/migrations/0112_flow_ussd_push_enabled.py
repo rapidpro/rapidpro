2b4a2e2216b55f9f8f7050dd9e83053b28ce331b
Gyorgy Fekete <f@pulilab.com>
2017-09-01 20:23:41 +0200

    fix migrations

--- temba/flows/migrations/0112_flow_ussd_push_enabled.py
+++ temba/flows/migrations/0112_flow_ussd_push_enabled.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flows', '0110_auto_20170731_1046'),
+        ('flows', '0111_auto_20170824_1458'),
     ]
 
     operations = [
