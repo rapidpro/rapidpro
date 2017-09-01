5c8a534ab082d777afc1a4801a605f2fc75105bc
Nic Pottier <nicpottier@gmail.com>
2017-09-01 12:53:15 -0500

    test fixes

--- temba/contacts/tests.py
+++ temba/contacts/tests.py
@@ -2339,17 +2339,14 @@ class ContactTest(TembaTest):
         self.assertNotContains(response, "blow80")
 
         # try delete action
-        call = ChannelEvent.create(self.channel, six.text_type(self.frank.get_urn(TEL_SCHEME)), ChannelEvent.TYPE_CALL_OUT_MISSED,
-                                   timezone.now(), 5)
+        event = ChannelEvent.create(self.channel, six.text_type(self.frank.get_urn(TEL_SCHEME)),
+                                    ChannelEvent.TYPE_CALL_OUT_MISSED, timezone.now(), 5)
         post_data['action'] = 'delete'
         post_data['objects'] = self.frank.pk
 
         self.client.post(list_url, post_data)
         self.assertFalse(ChannelEvent.objects.filter(contact=self.frank))
-
-        # the call should be inactive now too
-        call.refresh_from_db()
-        self.assertFalse(call.is_active)
+        self.assertFalse(ChannelEvent.objects.filter(id=event.id))
 
     def test_number_normalized(self):
         self.org.country = None
