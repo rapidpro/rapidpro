bb42b643255818907308054bf24df554f7602c6b
Nic Pottier <nicpottier@gmail.com>
2017-09-10 12:55:11 -0500

    update migration

--- temba/channels/migrations/0079_auto_20170910_1731.py
+++ temba/channels/migrations/0079_auto_20170910_1731.py
@@ -11,15 +11,16 @@ def populate_twilio_auth(apps, schema_editor):
 
     # copy our org level configs into our channel configs
     for channel in Channel.objects.filter(channel_type__in=['T', 'TMS'], is_active=True).select_related('org'):
-        config = channel.config_json()
-        config['account_sid'] = channel.org.config_json().get('ACCOUNT_SID')
-        config['auth_token'] = channel.org.config_json().get('ACCOUNT_TOKEN')
+        config = json.loads(channel.config)
+        org_config = json.loads(channel.org.config)
+        config['account_sid'] = org_config.get('ACCOUNT_SID')
+        config['auth_token'] = org_config.get('ACCOUNT_TOKEN')
         channel.config = json.dumps(config)
         channel.save(update_fields=['config'])
 
     # for consistency, remap twilio message service keys as well
     for channel in Channel.objects.filter(channel_type=['TW'], is_active=True):
-        config = channel.config_json()
+        config = json.loads(channel.config)
         config['account_sid'] = config.get('ACCOUNT_SID')
         config['auth_token'] = config.get('ACCOUNT_TOKEN')
         del config['ACCOUNT_SID']
