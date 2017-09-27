10d1d43ef00220e43fc47e08c4f3a3df27c036b3
Norbert Kwizera <norkans7@gmail.com>
2017-09-27 21:04:56 +0200

    Do not mix the configured prefixes and the channel number

--- temba/orgs/models.py
+++ temba/orgs/models.py
@@ -507,9 +507,10 @@ class Org(SmartModel):
                 # if we have more than one match, find the one with the highest overlap
                 if len(senders) > 1:
                     for sender in senders:
-                        channel_number = sender.address.strip('+')
                         config = sender.config_json()
-                        channel_prefixes = config.get(Channel.CONFIG_SHORTCODE_MATCHING_PREFIXES, []) + [channel_number]
+                        channel_prefixes = config.get(Channel.CONFIG_SHORTCODE_MATCHING_PREFIXES, [])
+                        if not channel_prefixes or not isinstance(channel_prefixes, list):
+                            channel_prefixes = [sender.address.strip('+')]
 
                         for chan_prefix in channel_prefixes:
                             for idx in range(prefix, len(chan_prefix)):
