a291691b877e9d2e55e6dc5a57bbcfbc6d5f1124
Eric Newcomer <ericnewcomer@gmail.com>
2017-10-10 15:25:28 -0500

    Fix SaveToContactAction test to ignore e164

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -3330,7 +3330,7 @@ class ActionTest(TembaTest):
 
         # throw exception for other reserved words except name and first_name
         for word in Contact.RESERVED_FIELDS:
-            if word not in ['name', 'first_name'] + list(URN.VALID_SCHEMES):
+            if word not in ['name', 'first_name', 'tel_e164'] + list(URN.VALID_SCHEMES):
                 with self.assertRaises(Exception):
                     test = SaveToContactAction.from_json(self.org, dict(type='save', label=word, value='', field=word))
                     test.value = "Jen"
