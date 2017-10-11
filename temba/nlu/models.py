a92227b34551681a34b9562ebea0cc83f26a6a0e
vctrferreira <victtorferreiralima@gmail.com>
2017-10-11 19:09:16 -0300

    Add Nlu consumer class

--- temba/nlu/models.py
+++ temba/nlu/models.py
@@ -2,3 +2,50 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+
+NLU_API_NAME = 'NLU_API_NAME'
+NLU_API_KEY = 'NLU_API_KEY'
+
+NLU_BOTHUB_TAG = 'BTH'
+NLU_WIT_AI_TAG = 'WIT'
+
+NLU_API_CHOICES = (
+    (NLU_BOTHUB_TAG, 'BotHub'),
+    (NLU_WIT_AI_TAG, 'Wit.AI'),)
+
+
+class BaseConsumer(object):
+    def __init__(self, auth, nlu_type):
+        self.auth = auth
+        self.name = "%s Consumer" % dict(NLU_API_CHOICES).get(nlu_type)
+
+    def __str__(self):
+        return self.name
+
+    def list_bots(self):
+        pass
+
+    def predict(self, msg):
+        pass
+
+
+class BothubConsumer(BaseConsumer):
+    def predict(self, msg):
+        # CALL BOTHUB API
+        return "BOTHUB RETURN"
+
+
+class WitConsumer(BaseConsumer):
+    def predict(self, msg):
+        # CALL WIT API
+        return "WIT RETURN"
+
+
+class NluApiConsumer(object):
+    @staticmethod
+    def factory(nlu_type, auth):
+        if nlu_type == NLU_BOTHUB_TAG:
+            return BothubConsumer(auth, nlu_type)
+        elif nlu_type == NLU_WIT_AI_TAG:
+            return WitConsumer(auth, nlu_type)
