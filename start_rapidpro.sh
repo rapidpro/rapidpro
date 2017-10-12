33fbe5cc827f3983e2766cbb354276ca0f9f58a6
mvilchis <mvilchis@ciencias.unam.mx>
2017-10-12 17:19:44 -0500

    Change rp

--- start_rapidpro.sh
+++ start_rapidpro.sh
@@ -4,5 +4,5 @@ bower install --allow-root
 python manage.py collectstatic --noinput --no-post-process
 python manage.py compress --extension=".haml" --force -v0
 python manage.py migrate --fake-initial --noinput
-#/usr/local/bin/uwsgi --http-auto-chunked --http-keepalive
-python manage.py runserver 0.0.0.0:8000
+/usr/local/bin/uwsgi --http-auto-chunked --http-keepalive
+#python manage.py runserver 0.0.0.0:8000
