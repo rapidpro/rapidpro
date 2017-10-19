f3e3f90b28b56b4db06035ea932c89e6ae81ec5f
vctrferreira <victtorferreiralima@gmail.com>
2017-10-19 14:35:05 -0300

    Add test Wit.AI consumer

--- temba/nlu/tests.py
+++ temba/nlu/tests.py
@@ -11,7 +11,18 @@ class NluTest(TembaTest):
     def test_nlu_api_bothub_consumer(self):
         consumer = NluApiConsumer.factory(NLU_BOTHUB_TAG, 'BOT_KEY_STRING')
         self.assertEquals(six.text_type(consumer), 'BotHub Consumer')
-        self.assertEquals(consumer.get_headers(), {'Authorization': 'Bearer BOT_KEY_STRING'}) # mocka retorno da api e valores e fazer as chamadas das funções
+        self.assertEquals(consumer.get_headers(), {'Authorization': 'Bearer BOT_KEY_STRING'})
+
+        with patch('requests.get') as mock_get:
+            mock_get.return_value = MockResponse(200, """
+            [
+                {"slug": "bot-slug-16", "uuid": "e5bf3007-2629-44e3-8cbe-4505ecb130e2"},
+                {"slug": "bot-slug-15", "uuid": "53c800c6-9e90-4ede-b3b8-723596bd8b2e"}
+            ]
+            """)
+            self.assertEquals(consumer.list_bots(),
+                              (('e5bf3007-2629-44e3-8cbe-4505ecb130e2', 'bot-slug-16'),
+                               ('53c800c6-9e90-4ede-b3b8-723596bd8b2e', 'bot-slug-15')))
 
         with patch('requests.get') as mock_get:
             mock_get.return_value = MockResponse(200, """
@@ -69,5 +80,42 @@ class NluTest(TembaTest):
             self.assertEquals(entities.get('location'), 'center')
 
     def test_nlu_api_wit_consumer(self):
-        consumer = NluApiConsumer.factory(NLU_WIT_AI_TAG, None)
+        consumer = NluApiConsumer.factory(NLU_WIT_AI_TAG, 'SPECIFIC_BOT_KEY')
         self.assertEquals(six.text_type(consumer), 'Wit.AI Consumer')
+
+        with patch('requests.get') as mock_get:
+            mock_get.return_value = MockResponse(200, """
+            {
+                "msg_id": "0j1thaYcCT2iJX7dB",
+                "_text": "Eu quero um exame com um ortopedista",
+                "entities": {
+                    "exames": [
+                        {
+                            "confidence": 1,
+                            "value": "exame",
+                            "type": "value"
+                        }
+                    ],
+                    "medico": [
+                        {
+                            "confidence": 0.87037789125963,
+                            "value": "ortopedista",
+                            "type": "value"
+                        }
+                    ],
+                    "intent": [
+                        {
+                            "confidence": 0.89605580369856,
+                            "value": "atendimento"
+                        }
+                    ]
+                }
+            }
+            """)
+            intent, accurancy, entities = consumer.predict("Eu quero um exame com um ortopedista",
+                                                           None)
+            self.assertEquals(intent, 'atendimento')
+            self.assertEquals(accurancy, 0.89605580369856)
+            self.assertEquals(type(entities), dict)
+            self.assertEquals(entities.get('exames'), 'exame')
+            self.assertEquals(entities.get('medico'), 'ortopedista')
