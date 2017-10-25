cb63bb896088d3e6006ce5a31416b7ed957ac234
Nic Pottier <nicpottier@gmail.com>
2017-10-25 13:21:43 -0500

    wrap contact fields in format_date as well

--- temba/flows/migrations/0115_use_format_date.py
+++ temba/flows/migrations/0115_use_format_date.py
@@ -9,6 +9,7 @@ import json
 
 def use_format_date(apps, schema_editor):
     from temba.flows.models import RuleSet, ActionSet, Flow
+    from temba.contacts.models import ContactField
 
     # find all rulesets that create a date object
     date_rulesets = RuleSet.objects.filter(value_type='D')
@@ -44,13 +45,46 @@ def use_format_date(apps, schema_editor):
         if changed:
             dr.flow.update(dr.flow.as_json())
 
-    # TODO: do the same for date contact fields
+    # for every contact field that is a date and has dependencies
+    for cf in ContactField.objects.filter(is_active=True, value_type='D').exclude(dependent_flows=None):
+        # find plain references to this field in actionsets, we'll replace them with format_date
+        for flow in cf.dependent_flows.all():
+            changed = False
+            for ac in ActionSet.objects.filter(flow=dr.flow, actions__icontains="@contact." + cf.key):
+                pattern = "@contact\\." + cf.key + "([^0-9a-zA-Z\\.]|\.[^0-9a-zA-Z\\.])"
+                if re.search(pattern, ac.actions, flags=re.UNICODE | re.MULTILINE):
+                    orig = ac.actions
+                    try:
+                        json_actions = json.loads(ac.actions)
+                        for i, json_action in enumerate(json_actions):
+                            if json_action['type'] in ['reply', 'send', 'say']:
+                                json_actions[i] = json.loads(re.sub(pattern,
+                                                                    "@(format_date(contact." + cf.key + "))\\1",
+                                                                    json.dumps(json_action),
+                                                                    flags=re.UNICODE | re.MULTILINE))
+
+                        ac.actions = json.dumps(json_actions)
+                        ac.save(update_fields=['actions'])
+                        print("actionset(%d) replaced:\n%s\nwith:\n%s" % (ac.id, orig, ac.actions))
+                        changed = True
+
+                    except:
+                        import traceback
+                        traceback.print_exc()
+                        print("unable to parse actionset(%d): %s" % (ac.id, ac.actions))
+
+                        # create a new revision from this definition if things changed
+
+            if changed:
+                flow.update(dr.flow.as_json())
+
+
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flows', '0113_backfill_value_type'),
+        ('flows', '0114_auto_20171020_1747'),
     ]
 
     operations = [
