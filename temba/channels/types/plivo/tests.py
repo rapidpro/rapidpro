7621bdef966126d975af2b2c1447727231c1af42
Rowan Seymour <rowanseymour@gmail.com>
2017-12-01 12:01:17 -0500

    Fix another case of hitting an external server from a unit test

--- temba/channels/types/plivo/tests.py
+++ temba/channels/types/plivo/tests.py
@@ -29,9 +29,11 @@ class PlivoTypeTest(TembaTest):
             plivo_get.return_value = MockResponse(400, {})
             response = self.client.get(claim_plivo_url)
 
-        self.assertEqual(response.status_code, 302)
-        response = self.client.get(claim_plivo_url, follow=True)
-        self.assertEqual(response.request['PATH_INFO'], reverse('orgs.org_plivo_connect'))
+            self.assertEqual(response.status_code, 302)
+
+            response = self.client.get(claim_plivo_url, follow=True)
+
+            self.assertEqual(response.request['PATH_INFO'], reverse('orgs.org_plivo_connect'))
 
         with patch('requests.get') as plivo_get:
             plivo_get.return_value = MockResponse(400, json.dumps(dict()))
