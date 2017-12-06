aa4488e8268ca7a4d1909c2b761cce2ccecc0044
Nic Pottier <nicpottier@gmail.com>
2017-12-06 17:04:12 -0500

    only migrate active channels

--- temba/channels/migrations/0081_update-nexmo-callback-url-channel-config.py
+++ temba/channels/migrations/0081_update-nexmo-callback-url-channel-config.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         Channel = apps.get_model('channels', 'Channel')
 
         if settings.IS_PROD:
-            nexmo_channels = Channel.objects.filter(channel_type='NX').exclude(org=None)
+            nexmo_channels = Channel.objects.filter(channel_type='NX', is_active=True).exclude(org=None)
             for channel in nexmo_channels:
                 try:
                     org = Org.objects.get(pk=channel.org_id)
