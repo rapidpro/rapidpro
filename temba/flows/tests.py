82797a03e7eef325e3438750871d2ab97632e405
Eric Newcomer <ericnewcomer@gmail.com>
2017-12-06 13:02:34 -0500

    tweak query count

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -7691,7 +7691,7 @@ class FlowBatchTest(FlowFileTest):
         stopped.stop(self.admin)
 
         # start our flow, this will take two batches
-        with QueryTracker(assert_query_count=289, stack_count=10, skip_unique_queries=True):
+        with QueryTracker(assert_query_count=298, stack_count=10, skip_unique_queries=True):
             flow.start([], contacts)
 
         # ensure 11 flow runs were created
