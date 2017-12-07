6be24b46f0ae73a36a133da17702ba96c6c0e865
Rowan Seymour <rowanseymour@gmail.com>
2017-12-07 09:34:07 -0500

    Fix intermittent test failure

--- temba/msgs/tests.py
+++ temba/msgs/tests.py
@@ -1007,19 +1007,19 @@ class BroadcastTest(TembaTest):
         assertBroadcastStatus(msgs[3], 'F', 'F')
 
         # first make sure there are no failed messages
-        for msg in broadcast.get_messages():
+        for msg in broadcast.get_messages().order_by('-id'):
             msg.status = 'S'
-            msg.save()
+            msg.save(update_fields=('status',))
 
-        assertBroadcastStatus(broadcast.get_messages()[0], 'Q', 'Q')
+        assertBroadcastStatus(broadcast.get_messages().order_by('-id')[0], 'Q', 'Q')
         # test queued broadcast logic
 
         # test sent broadcast logic
         broadcast.get_messages().update(status='D')
-        assertBroadcastStatus(broadcast.get_messages()[0], 'S', 'S')
+        assertBroadcastStatus(broadcast.get_messages().order_by('-id')[0], 'S', 'S')
 
         # test delivered broadcast logic
-        assertBroadcastStatus(broadcast.get_messages()[0], 'D', 'D')
+        assertBroadcastStatus(broadcast.get_messages().order_by('-id')[0], 'D', 'D')
 
         self.assertEqual("Temba (%d)" % broadcast.id, str(broadcast))
 
