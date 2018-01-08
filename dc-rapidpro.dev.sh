a744f8804a8cbda210f683213b7044e48b8bc1bc
Joe Goulet <jrgoulet@gmail.com>
2018-01-08 13:30:14 -0500

    Remove pip-requires installation step

--- dc-rapidpro.dev.sh
+++ dc-rapidpro.dev.sh
@@ -2,7 +2,6 @@
 if [ ! -d "env" ]; then
   echo Virtual Environment not detected, initializing now.
   virtualenv env
-  env/bin/python -m pip install -r pip-requires.txt
   env/bin/python -m pip install -r pip-freeze.txt
 fi
 /usr/local/bin/docker-compose -f ./rapidpro-compose.dev.yaml -p rapidpro "$@"
