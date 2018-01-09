f0bc9416dafc8eaaed6ba14718113f2f2389454d
Rowan Seymour <rowanseymour@gmail.com>
2018-01-09 10:21:57 +0000

    Fix migration conflict

--- temba/flows/migrations/0141_auto_20171214_2053.py
+++ temba/flows/migrations/0141_auto_20171214_2053.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flows', '0138_flowrun_current_node_index'),
+        ('flows', '0140_flowrun_current_node_index'),
     ]
 
     operations = [
