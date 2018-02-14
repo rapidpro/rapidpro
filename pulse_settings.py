1e8e70437d91e9113d9768c70d5f661178f2df03
Andrew Axelrod <andrew.axelrod@istresearch.com>
2018-02-14 13:51:28 -0600

    Made updates and fixed a bug

--- pulse_settings.py
+++ pulse_settings.py
@@ -142,8 +142,8 @@ BRANDING = {
         'colors': dict([rule.split('=') for rule in env('BRANDING_COLORS', 'primary=#0c6596').split(';')]),
         'styles': ['brands/pulse/font/style.css'],
         'welcome_topup': 1000,
-        'email': env('BRANDING_EMAIL', 'join@rapidpro.io'),
-        'support_email': env('BRANDING_SUPPORT_EMAIL', 'join@rapidpro.io'),
+        'email': env('BRANDING_EMAIL', 'engage@istresearch.com'),
+        'support_email': env('BRANDING_SUPPORT_EMAIL', 'no-reply@istresearch.com'),
         'link': env('BRANDING_LINK', 'https://app.rapidpro.io'),
         'api_link': env('BRANDING_API_LINK', 'https://api.rapidpro.io'),
         'docs_link': env('BRANDING_DOCS_LINK', 'http://docs.rapidpro.io'),
@@ -154,7 +154,7 @@ BRANDING = {
         'allow_signups': env('BRANDING_ALLOW_SIGNUPS', False),
         'tiers': dict(import_flows=0, multi_user=0, multi_org=0),
         'bundles': [],
-        'welcome_packs': [dict(size=5000, name="Demo Account"), dict(size=100000, name="UNICEF Account")],
+        'welcome_packs': [dict(size=5000, name="Pulse Account"), dict(size=100000, name="Pulse Account")],
         'description': _("Pulse lets you visually build interactive SMS applications and launch them anywhere in the world."),
         'credits': _("Copyright &copy; 2012-{} IST Research Corp. All Rights Reserved.".format(
             datetime.now().strftime('%Y')
--- static/brands/pulse/README.md
+++ static/brands/pulse/README.md
@@ -1,12 +1,10 @@
 # RapidPro (PULSE Branding)
 
 ### Custom Theme
-The custome theme should only be modified in the brands/pulse/less directory so it doens't conflict with future RapidPro releases. There are two files: variables.less and styles.less. This is where the core RapidPro css is overwritten.
+The custom theme should be modified within the brands/pulse/less directory, so it doesn't conflict with future RapidPro releases. There are two files: variables.less and styles.less. This is where the core RapidPro css is overwritten.
 
 ### Pulse Logo
-Rapid Pro relies heavily on fonts. The Pulse Logo is treated as a font and uses an SVG backup for browsers that don't support fonts. The process to generate the PULSE logo is to first create as an SVG.  The SVG is then imported into IcoMoon (https://icomoon.io) where ttf, eot, and woff fonts are generated. These files need to live in brands/font/fonts.  
+Rapid Pro relies heavily on fonts. The Pulse Logo is treated as a font and uses an SVG backup for browsers that don't support fonts. The process to generate the PULSE logo is to first create as an SVG.  The SVG is imported into IcoMoon (https://icomoon.io) where ttf, eot, and woff fonts are generated. These files need to live in brands/font/fonts.  
 
 ### favicon
 The favicon is located at brand/pulse/pulse.ico.
-
-
