eb993f31c9237d651781f18b684b4c30ddeed028
Norbert Kwizera <norkans7@gmail.com>
2018-02-20 16:05:05 +0200

    Add needs cover

--- temba/channels/types/jiochat/tasks.py
+++ temba/channels/types/jiochat/tasks.py
@@ -6,7 +6,7 @@ from temba.utils.jiochat import JiochatClient
 
 
 @task(track_started=True, name='refresh_jiochat_access_tokens')
-def refresh_jiochat_access_tokens():
+def refresh_jiochat_access_tokens():  # pragma: needs cover
     jiochat_channels = Channel.objects.filter(channel_type='JC', is_active=True)
     for channel in jiochat_channels:
         client = JiochatClient.from_channel(channel)
