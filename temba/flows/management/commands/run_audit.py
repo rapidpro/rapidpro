f243fba5b5b2fa26326ac2f131cc71e77cde31de
Rowan Seymour <rowanseymour@gmail.com>
2018-04-17 11:40:17 -0500

    Audit runs whose path length is different to its step count, and runs with unparseable .fields column

--- temba/flows/management/commands/run_audit.py
+++ temba/flows/management/commands/run_audit.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import json
 import time
 
 from django.core.management.base import BaseCommand
 from django.db import connection
+from django.db.models import Count
+from django.db.models.expressions import RawSQL
 from temba.flows.models import FlowRun
 
 
@@ -21,45 +24,65 @@ def audit_runs():  # pragma: no cover
 
     empty_paths_active_flow = []
     empty_paths_inactive_flow = []
-    null_events = []
+    path_len_step_count_mismatch = []
     event_count_message_id_mismatch = []
+    unparseable_fields = []
 
     start = time.time()
 
-    while True:
-        run_batch = list(FlowRun.objects.filter(id__gt=max_run_id).order_by('id').select_related('flow').defer('fields')[:1000])
-        if not run_batch:
-            break
+    try:
+        while True:
+            run_batch = list(
+                FlowRun.objects
+                .filter(id__gt=max_run_id)
+                .annotate(num_steps=Count('steps'), fields_raw=RawSQL("fields", []))
+                .select_related('flow')
+                .defer('fields')
+                .order_by('id')[:1000]
+            )
+            if not run_batch:
+                break
 
-        for run in run_batch:
-            if len(run.path) == 0:
-                if run.flow.is_active:
-                    empty_paths_active_flow.append(run.id)
-                else:
-                    empty_paths_inactive_flow.append(run.id)
+            for run in run_batch:
+                if len(run.path) == 0:
+                    if run.flow.is_active:
+                        empty_paths_active_flow.append(run.id)
+                    else:
+                        empty_paths_inactive_flow.append(run.id)
 
-            if run.events is None:
-                null_events.append(run.id)
+                if len(run.path) != run.num_steps:
+                    path_len_step_count_mismatch.append(run.id)
 
-            if len(run.events or []) != len(set(run.message_ids or [])):
-                event_count_message_id_mismatch.append(run.id)
+                if len(run.events or []) != len(set(run.message_ids or [])):
+                    event_count_message_id_mismatch.append(run.id)
 
-        num_audited += len(run_batch)
-        max_run_id = run_batch[-1].id
-        time_taken = time.time() - start
-        time_per_run = time_taken / num_audited
-        time_remaining = (total_runs - num_audited) * time_per_run
+                if run.fields_raw is not None:
+                    try:
+                        json.loads(run.fields_raw)
+                    except ValueError:
+                        unparseable_fields.append(run.id)
 
-        print(" > Audited %d / ~%d runs (est %d mins remaining)" % (num_audited, total_runs, int(time_remaining / 60)))
+            num_audited += len(run_batch)
+            max_run_id = run_batch[-1].id
+            time_taken = time.time() - start
+            time_per_run = time_taken / num_audited
+            time_remaining = (total_runs - num_audited) * time_per_run
 
-    print("Found %d runs" % num_audited)
-    print("Found %d runs from active flows with empty paths: %s"
+            print(" > Audited %d / ~%d runs (est %d mins remaining)" % (num_audited, total_runs, int(time_remaining / 60)))
+    except KeyboardInterrupt:
+        pass
+
+    print("Found:")
+    print(" * %d runs" % num_audited)
+    print(" * %d runs from active flows with empty paths: %s"
           % (len(empty_paths_active_flow), ids_to_string(empty_paths_active_flow)))
-    print("Found %d runs from inactive flows with empty paths"
+    print(" * %d runs from inactive flows with empty paths"
           % len(empty_paths_inactive_flow))
-    print("Found %d runs with NULL events field" % len(null_events))
-    print("Found %d runs with difference in event count vs message_ids: %s"
+    print(" * %d runs with difference in path length vs step count: %s"
+          % (len(path_len_step_count_mismatch), ids_to_string(path_len_step_count_mismatch)))
+    print(" * %d runs with difference in event count vs message_ids: %s"
           % (len(event_count_message_id_mismatch), ids_to_string(event_count_message_id_mismatch)))
+    print(" * %d runs with unparseable .fields column: %s" % (len(unparseable_fields), ids_to_string(unparseable_fields)))
 
 
 class Command(BaseCommand):  # pragma: no cover
@@ -71,8 +94,8 @@ class Command(BaseCommand):  # pragma: no cover
 
 def ids_to_string(id_list, limit=100):
     subset = id_list[:limit]
-    str = ", ".join([str(i) for i in subset])
+    text = ", ".join([str(i) for i in subset])
     if len(subset) < len(id_list):
-        str += "..."
+        text += "..."
 
-    return str
+    return text
