e6687a9a0d58819921eea3e29fb5eb67df3fe2b2
Rowan Seymour <rowanseymour@gmail.com>
2018-04-18 10:17:59 -0500

    Be more fussy about what is an acual problem on a run and output problems immediately

--- temba/flows/management/commands/run_audit.py
+++ temba/flows/management/commands/run_audit.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import json
+import six
 import time
 
 from django.core.management.base import BaseCommand
@@ -20,70 +21,61 @@ def audit_runs():  # pragma: no cover
 
     max_run_id = 0
     num_audited = 0
+    num_problems = 0
 
-    empty_paths_active_flow = []
-    empty_paths_inactive_flow = []
-    path_len_step_count_mismatch = []
-    event_count_message_id_mismatch = []
-    unparseable_fields = []
+    problem_finders = {
+        'unparseable_fields': has_unparseble_fields,
+        'less_events_than_message_ids': has_less_events_than_message_ids,
+        'step_count_path_length_mismatch': has_step_count_path_length_mismatch
+    }
 
     start = time.time()
 
-    try:
-        while True:
-            run_batch = list(
-                FlowRun.objects
-                .filter(id__gt=max_run_id)
-                .annotate(num_steps=Count('steps'))
-                .extra(select={'fields_raw': 'fields'})
-                .prefetch_related(Prefetch('flow', queryset=Flow.objects.only('id', 'is_active')))
-                .defer('fields')
-                .order_by('id')[:5000]
-            )
-            if not run_batch:
-                break
+    while True:
+        run_batch = list(
+            FlowRun.objects
+            .filter(id__gt=max_run_id)
+            .annotate(num_steps=Count('steps'))
+            .extra(select={'fields_raw': 'fields'})
+            .prefetch_related(Prefetch('flow', queryset=Flow.objects.only('id', 'is_active')))
+            .defer('fields')
+            .order_by('id')[:5000]
+        )
+        if not run_batch:
+            break
 
-            for run in run_batch:
-                if run.num_steps > 0:  # don't include runs whose steps were purged when their flow was deleted
-                    if len(run.path) == 0:
-                        if run.flow.is_active:
-                            empty_paths_active_flow.append(run.id)
-                        else:
-                            empty_paths_inactive_flow.append(run.id)
+        for run in run_batch:
+            for problem_name, problem_finder in six.iteritems(problem_finders):
+                if problem_finder(run):
+                    print("   ! Run #%d has problem: %s" % (run.id, problem_name))
+                    num_problems += 1
 
-                    if len(run.path) != run.num_steps and run.num_steps < 100:  # don't include trimmed paths
-                        path_len_step_count_mismatch.append(run.id)
+        num_audited += len(run_batch)
+        max_run_id = run_batch[-1].id
+        time_taken = time.time() - start
+        time_per_run = time_taken / num_audited
+        time_remaining = (total_runs - num_audited) * time_per_run
 
-                if len(run.events or []) < len(set(run.message_ids or [])):  # events might include purged messages
-                    event_count_message_id_mismatch.append(run.id)
+        print(" > Audited %d / ~%d runs (est %d mins remaining, %d problems found)" % (num_audited, total_runs, int(time_remaining / 60), num_problems))
 
-                if run.fields_raw is not None:
-                    try:
-                        json.loads(run.fields_raw)
-                    except ValueError:
-                        unparseable_fields.append(run.id)
+    print("Finished run audit in %.1f secs" % (time.time() - start))
 
-            num_audited += len(run_batch)
-            max_run_id = run_batch[-1].id
-            time_taken = time.time() - start
-            time_per_run = time_taken / num_audited
-            time_remaining = (total_runs - num_audited) * time_per_run
 
-            print(" > Audited %d / ~%d runs (est %d mins remaining)" % (num_audited, total_runs, int(time_remaining / 60)))
-    except KeyboardInterrupt:
-        pass
+def has_unparseble_fields(run):
+    if run.fields_raw is not None:
+        try:
+            json.loads(run.fields_raw)
+        except ValueError:
+            return True
+    return False
 
-    print("Found:")
-    print(" * %d runs" % num_audited)
-    print(" * %d runs from active flows with steps but empty paths: %s"
-          % (len(empty_paths_active_flow), ids_to_string(empty_paths_active_flow)))
-    print(" * %d runs from inactive flows with steps but empty paths"
-          % len(empty_paths_inactive_flow))
-    print(" * %d runs with difference in path length vs step count: %s"
-          % (len(path_len_step_count_mismatch), ids_to_string(path_len_step_count_mismatch)))
-    print(" * %d runs with difference in event count vs message_ids: %s"
-          % (len(event_count_message_id_mismatch), ids_to_string(event_count_message_id_mismatch)))
-    print(" * %d runs with unparseable .fields column: %s" % (len(unparseable_fields), ids_to_string(unparseable_fields)))
+
+def has_step_count_path_length_mismatch(run):
+    return min(len(run.path), 100) != min(run.num_steps, 100)  # take path trimming into account
+
+
+def has_less_events_than_message_ids(run):
+    return len(run.events or []) < len(set(run.message_ids or []))  # events might include purged messages
 
 
 class Command(BaseCommand):  # pragma: no cover
