722ebad132cca0fb132e738552f8209bec3cd258
Rowan Seymour <rowanseymour@gmail.com>
2018-05-23 16:57:06 -0500

    Fix migration conflict

--- temba/flows/migrations/0159_auto_20180522_1851.py
+++ temba/flows/migrations/0159_auto_20180522_1851.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flows', '0157_update_path_trigger'),
+        ('flows', '0158_backfill_recent_step_uuids'),
     ]
 
     operations = [
