01010fb6ab76c3c3c8edca9c5ee0afc4d7327003
Rowan Seymour <rowanseymour@gmail.com>
2018-05-29 11:10:51 -0500

    Make 2 more migrations non-atomic to fix deadlocking

--- temba/channels/migrations/0078_auto_20170831_1830.py
+++ temba/channels/migrations/0078_auto_20170831_1830.py
@@ -91,6 +91,8 @@ def noop(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
+    atomic = False
+
     dependencies = [("channels", "0077_auto_20170824_1555"), ("msgs", "0076_install_triggers")]
 
     operations = [
