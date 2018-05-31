3ff2b25efaf463ab98fb7a58dde45a4ed8f794a0
Dražen Odobašić <dodobas@quaternion.co>
2018-05-31 18:42:09 +0200

    Fix code quality

--- temba/archives/migrations/0004_auto_20180522_1502.py
+++ temba/archives/migrations/0004_auto_20180522_1502.py
@@ -7,14 +7,16 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('archives', '0003_auto_20180517_1812'),
-    ]
+    dependencies = [("archives", "0003_auto_20180517_1812")]
 
     operations = [
         migrations.AlterField(
-            model_name='archive',
-            name='archive_type',
-            field=models.CharField(choices=[('message', 'Message'), ('run', 'Run')], help_text='The type of record this is an archive for', max_length=16),
-        ),
+            model_name="archive",
+            name="archive_type",
+            field=models.CharField(
+                choices=[("message", "Message"), ("run", "Run")],
+                help_text="The type of record this is an archive for",
+                max_length=16,
+            ),
+        )
     ]
