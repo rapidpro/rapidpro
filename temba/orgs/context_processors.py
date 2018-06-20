615dacfc3ef3e5acab90f8dc3eeb73a25c2369aa
Eric Newcomer <ericnewcomer@gmail.com>
2018-06-20 09:57:24 -0500

    deal with anon users when determining orgs

--- temba/orgs/context_processors.py
+++ temba/orgs/context_processors.py
@@ -43,8 +43,11 @@ class GroupPermWrapper(object):
 
 
 def user_orgs_for_brand(request):
-    user_orgs = request.user.get_user_orgs(request.branding.get("brand"))
-    return dict(user_orgs=user_orgs)
+    if hasattr(request, "user"):
+        if not request.user.is_anonymous():
+            user_orgs = request.user.get_user_orgs(request.branding.get("brand"))
+            return dict(user_orgs=user_orgs)
+    return []
 
 
 def user_group_perms_processor(request):
