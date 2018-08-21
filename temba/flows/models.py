bb7031637ee5d2ca00d26fd4e847fe2f928a2f96
mvilchis <mvilchis@ciencias.unam.mx>
2018-08-21 10:24:46 -0500

    Add change to media url

--- temba/flows/models.py
+++ temba/flows/models.py
@@ -5784,10 +5784,11 @@ class ReplyAction(Action):
                 media_type, media_url = run.flow.get_localized_text(self.media, run.contact).split(':', 1)
 
                 # if we have a localized media, create the url
-                if media_url and len(media_type.split("/")) > 1:
-                    attachments = ["%s:https://%s%s%s" % (media_type, settings.TEMBA_HOST,settings.MEDIA_URL, media_url)]
-                else:
-                    attachments = ["%s:%s" % (media_type, media_url)]
+                if media_url:
+                    if "https" in media_url or "http"  in media_url :
+                        attachments = ["%s:%s" % (media_type, media_url)]
+                    else:
+                        attachments = ["%s:https://%s%s%s" % (media_type, settings.TEMBA_HOST,settings.MEDIA_URL, media_url)]
 
             if offline_on:
                 context = None
