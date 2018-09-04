bf0cdf4ddf1df3277b265044ba22f30df147258f
mvilchis <mvilchis@ciencias.unam.mx>
2018-09-04 17:00:18 -0500

    Add index stuff

--- start_rapidpro.sh
+++ start_rapidpro.sh
@@ -7,3 +7,5 @@ python3 manage.py compress --extension=".haml" --force -v0
 #python manage.py migrate --fake-initial --noinput
 /usr/local/bin/uwsgi --http-auto-chunked --http-keepalive --single-interpreter --enable-threads
 #python manage.py runserver 0.0.0.0:8000
+#./index_stuff/rp-indexer -db postgresql://temba@$POSTGRES_PORT_5432_TCP_ADDR:5432/temba?sslmode=disable -rebuild -elastic-url http://$ELASTICSEARCH_PORT_9200_TCP_ADDR:9200
+#./index_stuff/rp-indexer -db postgresql://rapidpro:rapidpro@$POSTGRES_PORT_5432_TCP_ADDR:5432/temba?sslmode=disable -rebuild -elastic-url http://$ELASTICSEARCH_PORT_9200_TCP_ADDR:9200
