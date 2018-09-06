e70d755f56b24256bceef4eb3db4af6f95ae3e20
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-09-06 11:26:09 -0300

    Remove future imports for novo channel

--- temba/channels/types/novo/tests.py
+++ temba/channels/types/novo/tests.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import pytz
-
 from django.urls import reverse
 
 from temba.tests import TembaTest
-
 from ...models import Channel
 
 
