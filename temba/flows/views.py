0a90c7e0aafcc6d1f4891168b9f321669e7bbc20
Norbert Kwizera <norkans7@gmail.com>
2018-09-07 17:00:20 +0200

    Use channel type and name to describe channel

--- temba/flows/views.py
+++ temba/flows/views.py
@@ -1631,10 +1631,7 @@ class FlowCRUDL(SmartCRUDL):
 
             # all the channels available for our org
             channels = [
-                dict(
-                    uuid=chan.uuid,
-                    name=f"{chan.get_channel_type_display()}: {chan.get_address_display()} - {chan.name}",
-                )
+                dict(uuid=chan.uuid, name=f"{chan.get_channel_type_display()}: {chan.name}")
                 for chan in flow.org.channels.filter(is_active=True)
             ]
             return JsonResponse(
