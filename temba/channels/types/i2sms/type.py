b033f0ccf573063dd22548808589f5ae5e0c568d
Nic Pottier <nicpottier@gmail.com>
2018-10-22 13:05:35 -0500

    wip

---
+++ temba/channels/types/i2sms/type.py
@@ -0,0 +1,73 @@
+from django.utils.translation import ugettext_lazy as _
+
+from temba.channels.types.africastalking.views import ClaimView
+from temba.contacts.models import TEL_SCHEME
+
+from ...models import ChannelType
+
+
+class I2SMSType(ChannelType):
+    """
+    An I2SMS channel (https://www.i2sms.com/)
+    """
+
+    code = "I2"
+    category = ChannelType.Category.PHONE
+
+    courier_url = r"^i2/(?P<uuid>[a-z0-9\-]+)/(?P<action>receive|status)$"
+
+    name = "I2SMS"
+    icon = "icon-channel-external"
+
+    claim_blurb = _(
+        """If you are have a long number or shortcode with I2SMS (https://www.i2sms.com/) you can connect it in a few
+        easy steps."""
+    )
+    claim_view = ClaimView
+
+    schemes = [TEL_SCHEME]
+    max_length = 160
+    attachment_support = False
+
+    configuration_blurb = _(
+        """
+        To finish configuring your Africa's Talking connection you'll need to set the following callback URLs
+        on the Africa's Talking website under your account.
+        """
+    )
+
+    configuration_urls = (
+        dict(
+            label=_("Callback URL"),
+            url="https://{{ channel.callback_domain }}{% url 'courier.at' channel.uuid 'receive' %}",
+            description=_(
+                """
+                You can set the callback URL on your Africa's Talking account by visiting the SMS Dashboard page, then clicking on
+                <a href="http://www.africastalking.com/account/sms/smscallback" target="africastalking">Callback URL</a>.
+                """
+            ),
+        ),
+        dict(
+            label=_("Delivery URL"),
+            url="https://{{ channel.callback_domain }}{% url 'courier.at' channel.uuid 'status' %}",
+            description=_(
+                """
+                You can set the delivery URL on your Africa's Talking account by visiting the SMS Dashboard page, then clicking on
+                <a href="http://www.africastalking.com/account/sms/dlrcallback" target="africastalking">Delivery Reports</a>.
+                """
+            ),
+        ),
+    )
+
+    def is_available_to(self, user):
+        org = user.get_org()
+        return org.timezone and str(org.timezone) in [
+            "Africa/Nairobi",
+            "Africa/Kampala",
+            "Africa/Lilongwe",
+            "Africa/Kigali",
+            "Africa/Lagos",
+        ]
+
+    def is_recommended_to(self, user):
+        return self.is_available_to(user)
