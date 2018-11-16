d14b6f26a53aed971ca839d9e3a33e4d317c7424
Eduardo Douglas <edudouglas@gmail.com>
2018-11-16 13:17:27 -0300

    Run black

--- temba/channels/types/playmobile/views.py
+++ temba/channels/types/playmobile/views.py
@@ -11,7 +11,9 @@ from ...views import ClaimViewMixin
 class ClaimView(ClaimViewMixin, SmartFormView):
     class PMClaimForm(ClaimViewMixin.Form):
         base_url = forms.URLField(label=_("Base URL"), help_text=_("The base URL for PlayMobile"))
-        shortcode = forms.CharField(label=_("Shortcode"), max_length=15, min_length=1, help_text=_("The short code you are connecting"))
+        shortcode = forms.CharField(
+            label=_("Shortcode"), max_length=15, min_length=1, help_text=_("The short code you are connecting")
+        )
         username = forms.CharField(label=_("Username"), help_text=_("The username for your API account"))
         password = forms.CharField(label=_("Password"), help_text=_("The password for your API account"))
 
