2eef53abb96666517809211e9dd9fcd0b207ebe8
Rowan Seymour <rowanseymour@gmail.com>
2019-01-10 17:46:12 -0500

    Only remove modified_by

--- temba/channels/models.py
+++ temba/channels/models.py
@@ -2243,6 +2243,26 @@ class ChannelSession(models.Model):
         (ENDING, "Ending"),
     )
 
+    is_active = models.BooleanField(
+        default=True, help_text="Whether this item is active, use this instead of deleting"
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="%(class)ss",
+        help_text="The user which originally created this connection",
+        null=True,
+    )
+
+    created_on = models.DateTimeField(
+        default=timezone.now, editable=False, blank=True, help_text="When this item was originally created"
+    )
+
+    modified_on = models.DateTimeField(
+        default=timezone.now, editable=False, blank=True, help_text="When this item was last modified"
+    )
+
     external_id = models.CharField(max_length=255, help_text="The external id for this session, our twilio id usually")
     status = models.CharField(
         max_length=1, choices=STATUS_CHOICES, default=PENDING, help_text="The status of this session"
@@ -2274,18 +2294,6 @@ class ChannelSession(models.Model):
     )
     next_attempt = models.DateTimeField(
         verbose_name=_("Next Attempt"), help_text="When we should next attempt to make this call", null=True
-    )
-
-    is_active = models.BooleanField(
-        default=True, help_text="Whether this item is active, use this instead of deleting"
-    )
-
-    created_on = models.DateTimeField(
-        default=timezone.now, editable=False, blank=True, help_text="When this item was originally created"
-    )
-
-    modified_on = models.DateTimeField(
-        default=timezone.now, editable=False, blank=True, help_text="When this item was last modified"
     )
 
     def __init__(self, *args, **kwargs):
