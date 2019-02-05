f907132c7638fedc89d39b7d1e25b712ad450d47
Nic Pottier <nicpottier@gmail.com>
2019-02-05 12:26:52 -0800

    dont overwrite ivr handlers

--- temba/ivr/urls.py
+++ temba/ivr/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import CallHandler, MailroomHandler
 
 urlpatterns = [url(r"^handle/(?P<pk>\d+)/$", CallHandler.as_view(), name="ivr.ivrcall_handle")]
-urlpatterns = [
+urlpatterns += [
     url(
         r"^mr/ivr/c/(?P<uuid>[0-9a-f-]+)/(?P<action>handle|status|incoming)$",
         MailroomHandler.as_view(),
