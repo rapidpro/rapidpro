0c94b64606d02bfde098c12a58711db3ff94c60c
Nic Pottier <nicpottier@gmail.com>
2019-03-26 10:29:38 -0500

    add test for no variable case

--- temba/channels/types/whatsapp/tests.py
+++ temba/channels/types/whatsapp/tests.py
@@ -20,6 +20,7 @@ class WhatsAppTypeTest(TembaTest):
     def test_calculate_variable_count(self):
         self.assertEqual(2, _calculate_variable_count("Hi {{1}} how are you? {{2}}"))
         self.assertEqual(2, _calculate_variable_count("Hi {{1}} how are you? {{2}} {{1}}"))
+        self.assertEqual(0, _calculate_variable_count("Hi there."))
 
     def test_claim(self):
         Channel.objects.all().delete()
