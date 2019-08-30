cf59eab4bcc735d2ff2c7d8941f3b1d86fc52141
teehamaral <contato@teehamaral.com.br>
2019-08-29 22:44:24 -0300

    Docker implementation

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+case $1 in
+    supervisor)
+        /rapidpro/env/bin/python3.6 manage.py compress --extension=.haml --force
+        /usr/bin/supervisord -n -c docker/supervisor-app.conf
+    ;;
+    celery)
+        /usr/bin/supervisord -n -c docker/supervisor-celery.conf
+    ;;
+        
+esac
+
+exec "$@"
