00fbd89c4c8a28d1f36534c3f694b84d24f79c16
teehamaral <contato@teehamaral.com.br>
2019-09-03 20:20:15 -0300

    Add --noinput to migrate cmd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ case $1 in
     supervisor)
         python3.6 manage.py compress --extension=.haml --force
         python3.6 docker/clear-compressor-cache.py
-        python3.6 manage.py migrate
+        python3.6 manage.py migrate --noinput
         /usr/bin/supervisord -n -c docker/supervisor-app.conf
     ;;
     celery)
