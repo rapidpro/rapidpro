718f7e812de500a11330dff9c9b2aaffe2a13e72
teehamaral <contato@teehamaral.com.br>
2019-09-13 12:30:26 -0300

    Rename env variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ case $1 in
         python3.6 manage.py compress --extension=.haml --force
         python3.6 docker/clear-compressor-cache.py
         python3.6 manage.py migrate --noinput
-        envsubst '${COURIER_URL} ${MAILROOM_URL}' < docker/nginx.conf > /etc/nginx/sites-enabled/nginx.conf
+        envsubst '${NGINX_COURIER_URL} ${NGINX_MAILROOM_URL}' < docker/nginx.conf > /etc/nginx/sites-enabled/nginx.conf
         /usr/bin/supervisord -n -c docker/supervisor-app.conf
     ;;
         
