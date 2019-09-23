34fb09ecc9d7faabe449de755399ece2492f6d17
Eric Newcomer <ericnewcomer@gmail.com>
2019-09-23 13:30:16 -0700

    Add webpack plugin to create django template includes

--- components/webpack.config.js
+++ components/webpack.config.js
@@ -1,21 +1,20 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const { createCompatibilityConfig } = require('@open-wc/building-webpack');
-
-const BundleTracker = require('webpack-bundle-tracker');
-
-/* const config = createDefaultConfig({
-    input: path.resolve(__dirname, './public/index.html')
-});*/
-
-const configs = createCompatibilityConfig({
-    input: path.resolve(__dirname, './public/index.html')
-});
+const { createDefaultConfig } = require('@open-wc/building-webpack');
+const WebpackIndexHTMLPlugin = require('@open-wc/webpack-index-html-plugin');
+const getDefaultMode = require('@open-wc/building-webpack/src/get-default-mode');
+const CreateIncludesPlugin = require('./CreateIncludesPlugin');
+const mode = getDefaultMode();
+const configs = [
+    createDefaultConfig({
+        input: path.resolve(__dirname, './public/index.html')
+    })
+];
 
 module.exports = configs.map(config => {
     const legacy = config.output.filename.indexOf('legacy') === 0;
     const prefix = legacy ? 'legacy/' : '';
-    const outputChunkFilename = `${prefix}${'chunk-[id].js'}`;
+    // const outputChunkFilename = `${prefix}${'chunk-[id].js'}`;
 
     const conf = merge(config, {
         resolve: {
@@ -25,57 +24,46 @@ module.exports = configs.map(config => {
             path: path.resolve(process.cwd(), '..', 'static', 'components'),
             library: 'rp-components',
             libraryTarget: 'umd',
-            filename: prefix + 'rp-components.js',
-            chunkFilename: outputChunkFilename
+            filename: `${prefix}rp-components.[hash].js`
         },
-        devtool: 'inline-source-map',
         module: {
             rules: [
                 { test: /\.ts$/, loader: 'ts-loader' },
                 {
                     test: /\.scss$/,
-                    use: [
-                        'style-loader',
-                        // MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        'sass-loader'
-                    ]
+                    use: ['style-loader', 'css-loader', 'sass-loader']
                 },
                 { test: /\.css$/, use: ['style-loader', 'css-loader'] },
                 { test: /\.(png|svg|jpg|gif)$/, loader: 'file-loader' }
             ]
-        }
+        },
+        devtool: mode !== 'production' ? 'inline-source-map' : false
     });
 
+    if (mode === 'production') {
+        conf['entry'] = path.resolve(__dirname, './src/rp-components.js');
+        conf['plugins'][1] = new WebpackIndexHTMLPlugin({
+            template: ({ assets, entries, legacyEntries, variation }) => ``,
+            polyfills: {
+                coreJs: true,
+                regeneratorRuntime: true,
+                webcomponents: true,
+                fetch: true
+            },
+            loader: 'external'
+        });
+
+        // create our django template includes
+        conf['plugins'].push(
+            new CreateIncludesPlugin({
+                templates: path.resolve(
+                    process.cwd(),
+                    '..',
+                    'templates',
+                    'includes'
+                )
+            })
+        );
+    }
     return conf;
 });
-
-/* module.exports = merge(config, {
-    resolve: {
-        extensions: ['.ts', '.js', '.json', '.css']
-    },
-    output: {
-        path: path.resolve(process.cwd(), '..', 'static', 'components'),
-        library: 'rp-components',
-        libraryTarget: 'umd',
-        filename: 'rp-components.js'
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            { test: /\.ts$/, loader: 'ts-loader' },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    // MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(png|svg|jpg|gif)$/, loader: 'file-loader' }
-        ]
-    }
-});
-*/
