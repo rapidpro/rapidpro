c44cb8299fc4bfaeaed4be100a70cbe2ec30afe5
Teeh Amaral <contato@teehamaral.me>
2019-12-10 16:50:36 -0300

    Code formatted

--- temba/flows/migrations/0205_auto_20191002_1123.py
+++ temba/flows/migrations/0205_auto_20191002_1123.py
@@ -5,14 +5,33 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('flows', '0204_flowstart_campaign_event'),
-    ]
+    dependencies = [("flows", "0204_flowstart_campaign_event")]
 
     operations = [
         migrations.AlterField(
-            model_name='ruleset',
-            name='ruleset_type',
-            field=models.CharField(choices=[('wait_message', 'Wait for message'), ('wait_menu', 'Wait for USSD menu'), ('wait_ussd', 'Wait for USSD message'), ('wait_recording', 'Wait for recording'), ('wait_digit', 'Wait for digit'), ('wait_digits', 'Wait for digits'), ('subflow', 'Subflow'), ('webhook', 'Webhook'), ('resthook', 'Resthook'), ('lookup', 'Lookup'), ('airtime', 'Transfer Airtime'), ('form_field', 'Split by message form'), ('contact_field', 'Split on contact field'), ('expression', 'Split by expression'), ('random', 'Split Randomly')], help_text='The type of ruleset', max_length=16, null=True),
-        ),
+            model_name="ruleset",
+            name="ruleset_type",
+            field=models.CharField(
+                choices=[
+                    ("wait_message", "Wait for message"),
+                    ("wait_menu", "Wait for USSD menu"),
+                    ("wait_ussd", "Wait for USSD message"),
+                    ("wait_recording", "Wait for recording"),
+                    ("wait_digit", "Wait for digit"),
+                    ("wait_digits", "Wait for digits"),
+                    ("subflow", "Subflow"),
+                    ("webhook", "Webhook"),
+                    ("resthook", "Resthook"),
+                    ("lookup", "Lookup"),
+                    ("airtime", "Transfer Airtime"),
+                    ("form_field", "Split by message form"),
+                    ("contact_field", "Split on contact field"),
+                    ("expression", "Split by expression"),
+                    ("random", "Split Randomly"),
+                ],
+                help_text="The type of ruleset",
+                max_length=16,
+                null=True,
+            ),
+        )
     ]
