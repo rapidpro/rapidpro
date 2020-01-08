32baaaca526a16475dfc241d8506c15ba24e2f95
Teeh Amaral <contato@teehamaral.me>
2020-01-08 16:29:18 -0300

    Starting supervisord using Python 3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,9 @@ case $1 in
         python3.6 docker/clear-compressor-cache.py
         python3.6 manage.py migrate --noinput
         envsubst '${NGINX_COURIER_URL} ${NGINX_MAILROOM_URL}' < docker/nginx.conf > /etc/nginx/sites-enabled/nginx.conf
-        /usr/bin/supervisord -n -c docker/supervisor-app.conf
+        /usr/local/bin/supervisord -n -c docker/supervisor-app.conf
     ;;
-        
+
 esac
 
 exec "$@"
