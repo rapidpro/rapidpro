b64872a2c0c1231bd065812ccbb2d21c9554148a
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-04-09 15:54:38 -0300

    Update app two factor

--- temba/two_factor/models.py
+++ temba/two_factor/models.py
@@ -3,26 +3,20 @@ from os import urandom
 
 from smartmin.models import SmartModel
 
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from temba.orgs.models import UserSettings
 
 
 def generate_token():  # pragma: no cover
     return b32encode(urandom(5)).decode("utf-8").lower()
 
 
-class Profile(SmartModel):
-    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
-    otp_secret = models.CharField(verbose_name=_("OTP Secret"), max_length=18)
-    two_factor_enabled = models.BooleanField(verbose_name=_("Two Factor Enabled"), default=False)
-
-    def __str__(self):
-        return f"{self.user.username}"
-
-
 class BackupToken(SmartModel):
-    profile = models.ForeignKey(Profile, verbose_name=_("Used"), related_name="backups", on_delete=models.CASCADE)
+    settings = models.ForeignKey(
+        UserSettings, verbose_name=_("Settings"), related_name="backups", on_delete=models.CASCADE
+    )
     token = models.CharField(verbose_name=_("Token"), max_length=18, default=generate_token)
     used = models.BooleanField(verbose_name=_("Used"), default=False)
 
