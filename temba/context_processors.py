f4ccb3426875b25f3ae663271bf17b5aec014b60
Teeh Amaral <contato@teehamaral.me>
2020-04-15 17:51:48 -0300

    Adding recaptcha and 2FA using Twilio Authy

--- temba/context_processors.py
+++ temba/context_processors.py
@@ -1,5 +1,12 @@
+from django.conf import settings
+
+
 def branding(request):
     """
     Stuff our branding into the context
     """
-    return dict(brand=request.branding)
+    return dict(
+        brand=request.branding,
+        recaptcha_site_key=settings.RECAPTCHA_SITE_KEY,
+        recaptcha_secrete_key=settings.RECAPTCHA_SECRET_KEY
+    )
