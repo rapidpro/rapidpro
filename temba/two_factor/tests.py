973deebff695977243a9012a689cd92fbd3c74c9
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-04-22 17:09:37 -0300

    Update tests

--- temba/two_factor/tests.py
+++ temba/two_factor/tests.py
@@ -9,7 +9,3 @@ class LoginTest(TembaTest):
     def setUp(self):
         self.user = User.objects.create(username="test", email="test@test.com", password="test")
         self.user_settings = UserSettings.objects.create(user=self.user, two_factor_enabled=True)
-
-    def test_login_with_two_factor_enabled(self):
-        response = self.client.post(reverse("two_factor.login"), dict(username="test", password="test"))
-        self.assertRedirect(response, reverse("two_factor.token"))
