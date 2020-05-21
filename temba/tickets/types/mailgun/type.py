f1d49a044493eded2daa60190443e47cf53d893b
Rowan Seymour <rowanseymour@gmail.com>
2020-05-21 10:28:01 -0500

    Store brand name in mailgun ticketer config to use in emails from mailroom

--- temba/tickets/types/mailgun/type.py
+++ temba/tickets/types/mailgun/type.py
@@ -13,6 +13,7 @@ class MailgunType(TicketerType):
     CONFIG_DOMAIN = "domain"
     CONFIG_API_KEY = "api_key"
     CONFIG_TO_ADDRESS = "to_address"
+    CONFIG_BRAND_NAME = "brand_name"
     CONFIG_URL_BASE = "url_base"
 
     name = "Email"
