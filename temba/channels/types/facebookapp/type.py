b6be84d42eb7b9ea165d65d498f4ae422bb03347
Norbert Kwizera <norkans7@gmail.com>
2020-05-26 21:16:32 +0200

    Add Facebook App channel type

---
+++ temba/channels/types/facebookapp/type.py
@@ -0,0 +1,37 @@
+import requests
+
+from django.utils.translation import ugettext_lazy as _
+
+from temba.contacts.models import FACEBOOK_SCHEME
+from temba.triggers.models import Trigger
+
+from ...models import Channel, ChannelType
+from .views import ClaimView
+
+
+class FacebookappType(ChannelType):
+    """
+    A Facebook channel
+    """
+
+    code = "FBA"
+    category = ChannelType.Category.SOCIAL_MEDIA
+
+    courier_url = r"^fba/receive"
+
+    name = "Facebook"
+    icon = "icon-facebook-official"
+
+    claim_blurb = _(
+        """Add a <a href="http://facebook.com">Facebook</a> bot to send and receive messages on behalf
+    of one of your Facebook pages for free. You will need to connect your page by logging into your facebook and chekcing the Facebook page to connect"""
+    )
+    claim_view = ClaimView
+
+    schemes = [FACEBOOK_SCHEME]
+    max_length = 320
+    attachment_support = True
+    free_sending = True
+
+    def is_available_to(self, user):
+        return False
