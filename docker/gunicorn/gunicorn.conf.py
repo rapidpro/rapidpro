c737ebcb6c60ac169626e9ab2e730b277bfb8a41
Cloves Oliveira <clovesoliveiracc@gmail.com>
2020-05-29 17:34:06 -0300

    Updation docker config

--- docker/gunicorn/gunicorn.conf.py
+++ docker/gunicorn/gunicorn.conf.py
@@ -1,16 +1,11 @@
 import multiprocessing
 
-
-bind = '0.0.0.0:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = workers
-name = 'rapidpro'
-env = 'DJANGO_SETTINGS_MODULE=temba.settings'
+# env = 'DJANGO_SETTINGS_MODULE=temba.settings'
 proc_name = 'rapidpro'
 default_proc_name = proc_name
-loglevel = 'debug'
+# loglevel = 'debug'
 accesslog = 'gunicorn.access'
 errorlog = 'gunicorn.error'
-timeout = 120
-chdir = '/app'
 capture_output = True
