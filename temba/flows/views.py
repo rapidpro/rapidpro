438adec76954e29bef01bd604ad5b769a954bf1a
Ihor Palii <ithor@communityconnectlabs.com>
2020-06-02 22:45:07 +0300

    Remove downoad images functional from phone and surveyor flows

--- temba/flows/views.py
+++ temba/flows/views.py
@@ -1514,7 +1514,7 @@ class FlowCRUDL(SmartCRUDL):
             if self.has_org_perm("flows.flow_pdf_export"):
                 links.append(dict(title=_("Export to PDF"), href="javascript:;", js_class="pdf_export_submit"))
 
-            if self.has_org_perm("flows.flow_results"):
+            if self.has_org_perm("flows.flow_results") and flow.flow_type == Flow.TYPE_MESSAGE:
                 links.append(
                     dict(
                         title=_("Download Images"),
@@ -1678,7 +1678,7 @@ class FlowCRUDL(SmartCRUDL):
             if self.has_org_perm("orgs.org_export"):
                 links.append(dict(title=_("Export"), href="%s?flow=%s" % (reverse("orgs.org_export"), flow.id)))
 
-            if self.has_org_perm("flows.flow_results"):
+            if self.has_org_perm("flows.flow_results") and flow.flow_type == Flow.TYPE_MESSAGE:
                 links.append(
                     dict(
                         title=_("Download Images"),
