612d9c1eb1d2fa3578adeba1acf04fa96e668c4a
Cloves Oliveira <clovesoliveiracc@gmail.com>
2020-06-03 13:06:00 -0300

    Using nginx to serve static files

--- docker/gunicorn/gunicorn.conf.py
+++ docker/gunicorn/gunicorn.conf.py
@@ -2,10 +2,8 @@ import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = workers
-# env = 'DJANGO_SETTINGS_MODULE=temba.settings'
 proc_name = 'rapidpro'
 default_proc_name = proc_name
-# loglevel = 'debug'
 accesslog = 'gunicorn.access'
 errorlog = 'gunicorn.error'
 capture_output = True
