59c5c07be2f7698309928e2b7c7cff2c0f233d88
Rowan Seymour <rowanseymour@gmail.com>
2020-08-12 11:02:40 -0500

    Add test for migration.. and fix it

--- temba/contacts/migrations/0112_last_seen_on_sys_field.py
+++ temba/contacts/migrations/0112_last_seen_on_sys_field.py
@@ -2,24 +2,29 @@
 
 from django.db import migrations
 
+FIELD_TYPE_SYSTEM = "S"
+KEY_LAST_SEEN_ON = "last_seen_on"
+TYPE_DATETIME = "D"
+
 
 def add_last_seen_on_sys_field(apps, schema_editor):
     Org = apps.get_model("orgs", "Org")
     for org in Org.objects.all():
-        org.contactfields.create(
-            field_type="S",
-            key="last_seen_on",
-            label="Last Seen On",
-            value_type="D",
-            show_in_table=False,
-            created_by=org.created_by,
-            modified_by=org.modified_by,
-        )
+        if not org.contactfields.filter(field_type=FIELD_TYPE_SYSTEM, key=KEY_LAST_SEEN_ON).exists():
+            org.contactfields.create(
+                field_type=FIELD_TYPE_SYSTEM,
+                key=KEY_LAST_SEEN_ON,
+                label="Last Seen On",
+                value_type=TYPE_DATETIME,
+                show_in_table=False,
+                created_by=org.created_by,
+                modified_by=org.modified_by,
+            )
 
 
 def reverse(apps, schema_editor):  # pragma: no cover
     ContactField = apps.get_model("contacts", "ContactField")
-    ContactField.all_fields.filter(field_type="S", key="last_seen_on").delete()
+    ContactField.all_fields.filter(field_type=FIELD_TYPE_SYSTEM, key=KEY_LAST_SEEN_ON).delete()
 
 
 class Migration(migrations.Migration):
