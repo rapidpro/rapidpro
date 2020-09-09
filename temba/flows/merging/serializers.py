0cf8894d5f82848ab7839cb3a3054b134eb7f618
Ihor Palii <ithor@communityconnectlabs.com>
2020-09-09 17:08:32 +0300

    Fix for bug with accesing None instance origin node

--- temba/flows/merging/serializers.py
+++ temba/flows/merging/serializers.py
@@ -106,10 +106,10 @@ class DiffGraphSerializer(serializers.Serializer):
             if node_serializer.is_valid():
                 node_obj = node_serializer.save()
                 node_obj.left_origin_node = self.get_origin_node(
-                    node.get("left_origin_node", {}).get("uuid", None), diff_graph.left_graph
+                    (node.get("left_origin_node") or {}).get("uuid", None), diff_graph.left_graph
                 )
                 node_obj.right_origin_node = self.get_origin_node(
-                    node.get("right_origin_node", {}).get("uuid", None), diff_graph.right_graph
+                    (node.get("right_origin_node") or {}).get("uuid", None), diff_graph.right_graph
                 )
                 nodes_map[node_obj.uuid] = node_obj
 
