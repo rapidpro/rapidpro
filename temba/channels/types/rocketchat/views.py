bb9f90e7112e6084194a46706a6dbd82ba1e3284
joao <jcbalmeida@gmail.com>
2020-09-13 17:11:27 -0300

    Moves some Form methods to ClaimView

--- temba/channels/types/rocketchat/views.py
+++ temba/channels/types/rocketchat/views.py
@@ -1,4 +1,4 @@
-import requests
+import re
 from smartmin.views import SmartFormView
 
 from django import forms
@@ -10,7 +10,6 @@ from temba.utils.uuid import uuid4
 
 from ...models import Channel
 from ...views import ClaimViewMixin
-from .type import RocketChatType, RE_HOST
 from .client import Client, ClientError
 
 UUID_PATTERN = r"[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}"
@@ -61,6 +60,7 @@ class ClaimView(ClaimViewMixin, SmartFormView):
             return self.cleaned_data
 
         def clean_base_url(self):
+            from .type import RocketChatType
 
             org = self.request.user.get_org()
             base_url = RE_BASE_URL.search(self.cleaned_data.get("base_url", ""))
@@ -83,58 +83,61 @@ class ClaimView(ClaimViewMixin, SmartFormView):
 
             return base_url
 
-        def get_secret(self):
-            if self._secret:
-                return self.get_secret
-
-            self._secret = self.request.session.get(self.SESSION_KEY)
-            if not self._secret or self.request.method.lower() != "post":
-                self.request.session[self.SESSION_KEY] = self._secret = random_string(
-                    SECRET_LENGTH
-                )
-
+    def get_secret(self):
+        if self._secret:
             return self._secret
 
-        def derive_initial(self):
-            initial = super().derive_initial()
-            initial["secret"] = self.get_secret()
-            return initial
-
-        def form_valid(self, form):
-
-            base_url = form.cleaned_data["base_url"]
-            bot_username = form.cleaned_data["bot_username"]
-            secret = form.cleaned_data["secret"]
-            config = {
-                RocketChatType.CONFIG_BASE_URL: base_url,
-                RocketChatType.CONFIG_BOT_USERNAME: bot_username,
-                RocketChatType.CONFIG_SECRET:secret,
-            }
-
-            self.object = Channel(
-                uuid=uuid4(),
-                org=self.org,
-                channel_type=RocketChatType.slug,
-                config=config,
-                name=truncate(
-                    f"{RocketChatType.name}: {RE_HOST.search(url).group('domain')}",
-                    Channel._meta.get_field("name").max_length,
-                ),
-                created_by=self.request.user,
-                modified_by=self.request.user,
+        self._secret = self.request.session.get(self.SESSION_KEY)
+        if not self._secret or self.request.method.lower() != "post":
+            self.request.session[self.SESSION_KEY] = self._secret = random_string(
+                SECRET_LENGTH
             )
 
-            try:
-                client = Client(**config)
-                client.settings(self.request.build_absolute_uri("/"), self.object)
-            except ClientError as err:
-                messages.error(self.request,err.msg if err.msg else _("Configuration has failed"))
-                return super().get(self.request,*self.args,**self.kwargs)
-            else:
-                self.request.session.pop(self.SESSION_KEY,None)
+        return self._secret
 
-            self.object.save()
-            return super().form_valid(form)
+    def derive_initial(self):
+        initial = super().derive_initial()
+        initial["secret"] = self.get_secret()
+        return initial
+
+    def form_valid(self, form):
+        from .type import RocketChatType, RE_HOST
+
+        base_url = form.cleaned_data["base_url"]
+        bot_username = form.cleaned_data["bot_username"]
+        secret = form.cleaned_data["secret"]
+        config = {
+            RocketChatType.CONFIG_BASE_URL: base_url,
+            RocketChatType.CONFIG_BOT_USERNAME: bot_username,
+            RocketChatType.CONFIG_SECRET: secret,
+        }
+
+        self.object = Channel(
+            uuid=uuid4(),
+            org=self.org,
+            channel_type=RocketChatType.slug,
+            config=config,
+            name=truncate(
+                f"{RocketChatType.name}: {RE_HOST.search(url).group('domain')}",
+                Channel._meta.get_field("name").max_length,
+            ),
+            created_by=self.request.user,
+            modified_by=self.request.user,
+        )
+
+        try:
+            client = Client(**config)
+            client.settings(self.request.build_absolute_uri("/"), self.object)
+        except ClientError as err:
+            messages.error(
+                self.request, err.msg if err.msg else _("Configuration has failed")
+            )
+            return super().get(self.request, *self.args, **self.kwargs)
+        else:
+            self.request.session.pop(self.SESSION_KEY, None)
+
+        self.object.save()
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         kwargs["secret"] = self.get_secret()
