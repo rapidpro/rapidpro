ec54deb94e29bd9bf162368ea3c4f3e4c2dcaa81
Rowan Seymour <rowanseymour@gmail.com>
2020-10-07 13:02:09 -0500

    Remove unused ModelESSearch class

--- temba/contacts/search/elastic.py
+++ temba/contacts/search/elastic.py
@@ -8,44 +8,24 @@ from .mailroom import parse_query
 ES = Elasticsearch(hosts=[settings.ELASTICSEARCH_URL])
 
 
-class ModelESSearch(es_Search):
-    """
-    Adds Django model information to the elasticserach_dsl search class
-    """
-
-    is_none = False
-
-    def __init__(self, **kwargs):
-        self.model = kwargs.pop("model", None)
-
-        super().__init__(**kwargs)
-
-    def _clone(self):
-        new_search = super()._clone()
-        new_search.model = self.model  # copy extra attributes
-        return new_search
-
-
 def query_contact_ids(org, query, *, group=None):
-    from temba.contacts.models import Contact
-
+    """
+    Returns the contact ids for the given query
+    """
     parsed = parse_query(org, query, group=group)
     results = (
-        ModelESSearch(model=Contact, index="contacts")
-        .source(include=["id"])
-        .params(routing=org.id)
-        .using(ES)
-        .query(parsed.elastic_query)
+        es_Search(index="contacts").source(include=["id"]).params(routing=org.id).using(ES).query(parsed.elastic_query)
     )
 
     return [int(r.id) for r in results.scan()]
 
 
 def get_last_modified():
-    from temba.contacts.models import Contact
-
+    """
+    Gets the last modified contact if there are any contacts
+    """
     results = (
-        ModelESSearch(model=Contact, index="contacts")
+        es_Search(index="contacts")
         .params(size=1)
         .sort("-modified_on_mu")
         .source(include=["modified_on", "id"])
