c0403c4ca568d6d2461d15432e33a17c19576cb6
Teeh Amaral <contato@teehamaral.me>
2020-11-18 17:40:59 -0300

    Remove modified on statement for flow results export

--- temba/flows/models.py
+++ temba/flows/models.py
@@ -3851,8 +3851,7 @@ class ExportFlowResultsTask(BaseExportTask):
 
         # secondly get runs from database
         runs = FlowRun.objects.filter(flow__in=flows).order_by("modified_on")
-        if last_modified_on:
-            runs = runs.filter(modified_on__gt=last_modified_on)
+
         if responded_only:
             runs = runs.filter(responded=True)
         run_ids = array(str("l"), runs.values_list("id", flat=True))
