7bdea8153de46768dac3e7396cbf244b2c87333f
Rowan Seymour <rowanseymour@gmail.com>
2020-12-03 14:51:42 -0500

    Add security settings file

---
+++ temba/security_settings.py
@@ -0,0 +1,19 @@
+# require cookies to be on https (ergo logged in users must be on https)
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_AGE = 1209600  # 2 weeks
+
+# settings used by SecurityMiddleware
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = False
+SECURE_HSTS_SECONDS = 86400  # 24 hours
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "HTTPS")
+SECURE_REDIRECT_EXEMPT = []
+SECURE_SSL_HOST = None
+SECURE_SSL_REDIRECT = False
+
+# cross-site request forgery prevention
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "Strict"
+CSRF_COOKIE_AGE = 10800
