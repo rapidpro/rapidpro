7bf38416b737b3e348a2549d85b23470b3822d4d
Rowan Seymour <rowanseymour@gmail.com>
2020-12-08 09:45:16 -0500

    Fix migration

--- temba/globals/migrations/0004_auto_20201118_1723.py
+++ temba/globals/migrations/0004_auto_20201118_1723.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("globals", "0001_initial"),
+        ("globals", "0003_squashed"),
     ]
 
     operations = [
