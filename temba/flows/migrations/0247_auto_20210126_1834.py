71c1a14d42caf5f27b7923a6cf56e67b43fa5518
Rowan Seymour <rowanseymour@gmail.com>
2021-01-26 13:49:37 -0500

    Remove unused function

--- temba/flows/migrations/0247_auto_20210126_1834.py
+++ temba/flows/migrations/0247_auto_20210126_1834.py
@@ -7,9 +7,11 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("flows", "0246_auto_20210119_1717"),
+        ("sql", "0002_squashed"),
     ]
 
     operations = [
+        migrations.RunSQL("DROP FUNCTION temba_flow_for_run(_run_id INT);"),
         migrations.AlterField(
             model_name="flowpathrecentrun", name="id", field=models.BigAutoField(primary_key=True, serialize=False),
         ),
