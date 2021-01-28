63513ef0710aa17ffda188b4f4ece6419bd19c81
Norbert Kwizera <norkans7@gmail.com>
2021-01-28 18:24:23 +0200

    Add Django management command to verify sentry reports correctly

---
+++ temba/utils/management/commands/verify_sentry.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from celery.task import task
+
+@task
+def failing_task():
+    foo = 1/0
+
+
+class Command(BaseCommand):
+    help = "Verify Sentry reports with verify_sentry"
+
+    def handle(self, *args, **kwargs):
+
+        failing_task.delay()
+        foo = 1/0
+        print(foo)
+
+
