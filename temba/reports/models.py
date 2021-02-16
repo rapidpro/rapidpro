8c2b6be88fdf8df32ac1859f444432e9fe6c9495
Ihor Palii <ithor@communityconnectlabs.com>
2021-02-16 16:06:16 +0200

    Try to calculate data for report

--- temba/reports/models.py
+++ temba/reports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .managers import MessagesReportManager
+from ..utils.models import JSONField
 
-# Create your models here.
+
+class MessagesDailyCount(models.Model):
+    org = models.ForeignKey(to="orgs.Org", on_delete=models.CASCADE)
+    date = models.DateField(null=False)
+    flow = models.ManyToManyField(to="flows.Flow", null=True)
+    channel = models.ManyToManyField(to="channels.Channel", null=True)
+    calculated_data = JSONField()
+    objects = MessagesReportManager
