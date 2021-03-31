9d4de71607e4416d12351b5b1c0c2d7272d386d5
Ihor Palii <ithor@communityconnectlabs.com>
2021-03-31 15:23:09 +0300

    Fixed pagination of filtered data on channel log page.

--- temba/channels/views.py
+++ temba/channels/views.py
@@ -2719,6 +2719,32 @@ class ChannelLogCRUDL(SmartCRUDL):
             ]
             return context
 
+        def paginate_queryset(self, queryset, page_size):
+            paginator = self.get_paginator(
+                queryset, page_size, orphans=self.get_paginate_orphans(),
+                allow_empty_first_page=self.get_allow_empty())
+
+            page_kwarg = self.page_kwarg
+            page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
+            try:
+                page_number = int(page)
+            except ValueError:
+                if page == 'last':
+                    page_number = paginator.num_pages
+                else:
+                    raise Http404(_("Page is not 'last', nor can it be converted to an int."))
+
+            from django.core.paginator import InvalidPage
+            try:
+                paginator.count = len(queryset)
+                page = paginator.page(page_number)
+                return (paginator, page, page.object_list, page.has_other_pages())
+            except InvalidPage as e:
+                raise Http404(_('Invalid page (%(page_number)s): %(message)s') % {
+                    'page_number': page_number,
+                    'message': str(e)
+                })
+
     class Connection(AnonMixin, SmartReadView):
         model = ChannelConnection
 
