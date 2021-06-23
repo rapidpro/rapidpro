5ad955e89c9d1f8cde5a476b3d18e7cd4510fc04
Ihor Palii <ithor@communityconnectlabs.com>
2021-06-23 15:45:35 +0300

    Handled case when LinkContacts have duplicates.

--- temba/links/tasks.py
+++ temba/links/tasks.py
@@ -20,9 +20,10 @@ def handle_link_task(link_id, contact_id):
     link = Link.objects.filter(pk=link_id).only("created_by", "modified_by").first()
     if link and contact_id:
         # to count unique clicks
-        LinkContacts.objects.get_or_create(
-            link_id=link.id, contact_id=contact_id, created_by=link.created_by, modified_by=link.modified_by
-        )
+        if not link.contacts.filter(contact_id=contact_id).only("id").exists():
+            LinkContacts.objects.create(
+                link_id=link.id, contact_id=contact_id, created_by=link.created_by, modified_by=link.modified_by
+            )
         # to count all clicks
         link.clicks_count += 1
         link.save(update_fields=["clicks_count"])
