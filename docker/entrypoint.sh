7d4bbc0b516627fc8f5f5930d252d1c2dc5c9974
Teeh Amaral <contato@teehamaral.me>
2021-07-01 18:16:11 -0300

    Celery apart from the UI + remove nginx proxy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,16 @@
 set -e
 
 case $1 in
-    supervisor)
+    app)
         python3.6 manage.py compress --extension=.haml --force
         python3.6 docker/clear-compressor-cache.py
         python3.6 manage.py migrate --noinput
-        envsubst '${NGINX_COURIER_URL} ${NGINX_MAILROOM_URL}' < docker/nginx.conf > /etc/nginx/sites-enabled/nginx.conf
         /usr/local/bin/supervisord -n -c docker/supervisor-app.conf
+    ;;
+    celery)
+        python3.6 docker/clear-compressor-cache.py
+        python3.6 manage.py migrate --noinput
+        /usr/local/bin/supervisord -n -c docker/supervisor-celery.conf
     ;;
 
 esac
