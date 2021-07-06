2d4ca8e5e0b2d636847184c34c51d4db96ae39e3
Rowan Seymour <rowanseymour@gmail.com>
2021-07-06 09:28:30 -0500

    Coverage

--- temba/orgs/tests.py
+++ temba/orgs/tests.py
@@ -77,6 +77,10 @@ class OrgRoleTest(TembaTest):
         self.assertEqual(OrgRole.EDITOR, OrgRole.from_code("E"))
         self.assertIsNone(OrgRole.from_code("X"))
 
+    def test_from_group(self):
+        self.assertEqual(OrgRole.EDITOR, OrgRole.from_group(Group.objects.get(name="Editors")))
+        self.assertIsNone(OrgRole.from_group(Group.objects.get(name="Beta")))
+
     def test_group(self):
         self.assertEqual(Group.objects.get(name="Editors"), OrgRole.EDITOR.group)
         self.assertEqual(Group.objects.get(name="Agents"), OrgRole.AGENT.group)
