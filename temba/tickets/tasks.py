dd7fb5b958df6ee9d9632c7114e1d39033f6c9db
Rowan Seymour <rowanseymour@gmail.com>
2021-07-26 13:30:28 -0500

    Fix celery task name

--- temba/tickets/tasks.py
+++ temba/tickets/tasks.py
@@ -3,6 +3,6 @@ from temba.utils.celery import nonoverlapping_task
 from .models import TicketCount
 
 
-@nonoverlapping_task(track_started=True, lock_timeout=7200)
+@nonoverlapping_task(track_started=True, name="squash_ticketcounts", lock_timeout=7200)
 def squash_ticketcounts():
     TicketCount.squash()
