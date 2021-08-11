447ee092f27ddbcd070d6b985101563ac355cc55
Norbert Kwizera <norkans7@gmail.com>
2021-08-11 22:16:01 +0200

    Run code_checks

--- temba/channels/migrations/0130_channellog_channels_log_error_created.py
+++ temba/channels/migrations/0130_channellog_channels_log_error_created.py
@@ -6,12 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('channels', '0129_auto_20210810_2257'),
+        ("channels", "0129_auto_20210810_2257"),
     ]
 
     operations = [
         migrations.AddIndex(
-            model_name='channellog',
-            index=models.Index(condition=models.Q(('is_error', True)), fields=['channel', 'is_error', '-created_on'], name='channels_log_error_created'),
+            model_name="channellog",
+            index=models.Index(
+                condition=models.Q(("is_error", True)),
+                fields=["channel", "is_error", "-created_on"],
+                name="channels_log_error_created",
+            ),
         ),
     ]
