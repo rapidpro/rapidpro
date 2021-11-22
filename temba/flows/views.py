38d330e6d64f865866e0e479916eb4e2808f1986
Ihor Palii <ithor@communityconnectlabs.com>
2021-11-22 19:28:26 +0200

    Removed configurations

--- temba/flows/views.py
+++ temba/flows/views.py
@@ -1418,7 +1418,8 @@ class FlowCRUDL(SmartCRUDL):
             context["feature_filters"] = json.dumps(feature_filters)
 
             context["mergeable_flows"] = self.get_mergeable_flows()
-            context["has_translator"] = self.org.has_translation_service()
+            org = self.derive_org()
+            context["has_translator"] = org.has_translation_service() if org else None
 
             # check if there is no other users that edititing current flow
             # then make this user as main editor and set expiration time of editing to this user
