4db44e118a3485003b5b3f13dc8199e5992ac320
Eric Newcomer <ericnewcomer@gmail.com>
2021-12-10 15:10:30 -0800

    Fix join syntax, argh

--- temba/flows/views.py
+++ temba/flows/views.py
@@ -1506,7 +1506,7 @@ class FlowCRUDL(SmartCRUDL):
                 analytics.track(
                     self.request.user,
                     "temba.responses_export_started" if responded_only else "temba.results_export_started",
-                    dict(flows=[f.uuid for f in flows].join(", ")),
+                    dict(flows=", ".join([f.uuid for f in flows].join(", ")))
                 )
 
                 if not getattr(settings, "CELERY_TASK_ALWAYS_EAGER", False):  # pragma: needs cover
