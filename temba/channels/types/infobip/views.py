6a304acad8574cabfe46df0c86cb628229011923
Musa Musa <musakunte@gmail.com>
2022-01-10 15:54:10 +0100

    added number validation

--- temba/channels/types/infobip/views.py
+++ temba/channels/types/infobip/views.py
@@ -27,6 +27,25 @@ class ClaimView(AuthenticatedExternalClaimView):
             label=_("API Key"), help_text=_("The API Key provided by the provider to use their API")
         )
 
+        def clean_number(self):
+            number = self.data["number"]
+
+            # number is a shortcode, accept as is
+            if len(number) > 0 and len(number) < 7:
+                return number
+
+            # otherwise, try to parse into an international format
+            if number and number[0] != "+":
+                number = "+" + number
+
+            try:
+                cleaned = phonenumbers.parse(number, None)
+                return phonenumbers.format_number(cleaned, phonenumbers.PhoneNumberFormat.E164)
+            except Exception:  # pragma: needs cover
+                raise forms.ValidationError(
+                    _("Invalid phone number, please include the country code. ex: +250788123123")
+                )
+
     form_class = InfoBipForm
 
     def get_channel_config(self, org, data):
@@ -36,19 +55,11 @@ class ClaimView(AuthenticatedExternalClaimView):
         org = self.request.user.get_org()
 
         data = form.cleaned_data
-        phone_number = data["number"]
-        try:
-            parsed = phonenumbers.parse(phone_number, None)
-            phone = phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
-        except Exception:
-            # this is a shortcode, just use it plain
-            phone = phone_number
-
         self.object = Channel.add_config_external_channel(
             org,
             self.request.user,
             self.get_submitted_country(data),
-            phone,
+            data["number"],
             self.channel_type,
             dict(send_url=data["url"], api_key=data["api_key"]),
         )
