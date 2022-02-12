e3af5a5b4127d7ac3cf5e082b5000776c4f49801
Eric Newcomer <ericnewcomer@gmail.com>
2022-02-11 17:49:48 -0800

    Add sentry id for 500 page

--- temba/urls.py
+++ temba/urls.py
@@ -58,8 +58,9 @@ def handler500(request):
     Templates: `500.html`
     Context: None
     """
+    from sentry_sdk import last_event_id
     from django.template import loader
     from django.http import HttpResponseServerError
 
     t = loader.get_template("500.html")
-    return HttpResponseServerError(t.render({"request": request}))  # pragma: needs cover
+    return HttpResponseServerError(t.render({"request": request, "sentry_id": last_event_id()}))  # pragma: needs cover
