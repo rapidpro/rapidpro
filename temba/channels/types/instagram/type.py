b662a62553539bffc97127dbb4e60e6e91d8389e
Norbert Kwizera <norkans7@gmail.com>
2022-03-24 15:43:29 +0200

    Revert "Disable refresh token buttons"
    This reverts commit 40151aae07f6e952de767f4b3d0a922a6e962fad.

--- temba/channels/types/instagram/type.py
+++ temba/channels/types/instagram/type.py
@@ -14,12 +14,12 @@ class InstagramType(ChannelType):
     A Instagram channel
     """
 
-    # extra_links = [
-    #     dict(
-    #         name=_("Reconnect Instagram Business Account"),
-    #         link="channels.types.instagram.refresh_token",
-    #     )
-    # ]
+    extra_links = [
+        dict(
+            name=_("Reconnect Instagram Business Account"),
+            link="channels.types.instagram.refresh_token",
+        )
+    ]
 
     code = "IG"
     category = ChannelType.Category.SOCIAL_MEDIA
