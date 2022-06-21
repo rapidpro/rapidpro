11d95968981bca3d9fd8d8e951c07f50c99cbe20
Eric Newcomer <ericnewcomer@gmail.com>
2022-06-21 15:16:13 -0700

    Add ticket link on contact spa page

--- temba/tickets/models.py
+++ temba/tickets/models.py
@@ -391,7 +391,6 @@ class AllFolder(TicketFolder):
     def get_queryset(self, org, user, ordered):
         return super().get_queryset(org, user, ordered)
 
-
 FOLDERS = {f.slug: f() for f in TicketFolder.__subclasses__()}
 
 
