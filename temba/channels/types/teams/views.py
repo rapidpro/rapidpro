ad4ca7b1a0f3c1261b46411a4e707257a04de8a5
Robi9 <robertasousa714@gmail.com>
2022-07-01 16:06:47 -0300

    Go back to using ugettext_lazy

--- temba/channels/types/teams/views.py
+++ temba/channels/types/teams/views.py
@@ -1,4 +1,4 @@
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from smartmin.views import SmartFormView
 from django.core.exceptions import ValidationError
 from ...models import Channel
