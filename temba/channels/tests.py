26cb8b946cd733e5fded21ff56c9cc498a4f1782
Rowan Seymour <rowanseymour@gmail.com>
2022-09-16 12:19:57 -0500

    Add test for data migration to delete non-IVR connections

--- temba/channels/tests.py
+++ temba/channels/tests.py
@@ -3049,49 +3049,31 @@ class CourierTest(TembaTest):
         self.assertEqual(response.content, b"this URL should be mapped to a Courier instance")
 
 
-class DropLegacyLogsMigrationTest(MigrationTest):
+class RemoveNonIVRConnectionsMigrationTest(MigrationTest):
     app = "channels"
-    migrate_from = "0144_channelconnection_log_uuids"
-    migrate_to = "0145_drop_legacy_logs"
+    migrate_from = "0146_alter_channellog_elapsed_ms_and_more"
+    migrate_to = "0147_remove_non_ivr_connections"
 
     def setUpBeforeMigration(self, apps):
-        Channel = apps.get_model("channels", "Channel")
-        ChannelLog = apps.get_model("channels", "ChannelLog")
+        contact = self.create_contact("Bob", phone="+1234567890")
 
-        channel = Channel.objects.create(
-            org_id=self.org.id,
-            channel_type="T",
-            created_by_id=self.admin.id,
-            modified_by_id=self.admin.id,
+        self.connection1 = ChannelConnection.objects.create(
+            org=self.org,
+            channel=self.channel,
+            contact=contact,
+            contact_urn=contact.urns.get(),
+            status=ChannelConnection.STATUS_WIRED,
+            connection_type="V",
         )
-
-        self.new_log = ChannelLog.objects.create(
-            uuid="0e853ebf-1347-4d00-8ee6-9c1043e32907",
-            channel=channel,
-            log_type="msg_send",
-            is_error=False,
-            http_logs=[
-                {
-                    "url": "https://api.telegram.org/65474/sendMessage",
-                    "status_code": 200,
-                    "request": "POST /65474/sendMessage HTTP/1.1\r\nHost: api.telegram.org\r\nUser-Agent: Courier/1.2.159\r\nContent-Length: 231\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept-Encoding: gzip\r\n\r\nchat_id=3527065&reply_markup=%7B%22resize_keyboard%22%3Atrue%2C%22one_time_keyboard%22%3Atrue%2C%22keyboard%22%3A%5B%5B%7B%22text%22%3A%22blackjack%22%7D%2C%7B%22text%22%3A%22balance%22%7D%5D%5D%7D&text=Your+balance+is+now+%246.00.",
-                    "response": 'HTTP/1.1 200 OK\r\nContent-Length: 298\r\nAccess-Control-Allow-Methods: GET, POST, OPTIONS\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Expose-Headers: Content-Length,Content-Type,Date,Server,Connection\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nDate: Tue, 11 Jun 2019 15:33:06 GMT\r\nServer: nginx/1.12.2\r\nStrict-Transport-Security: max-age=31536000; includeSubDomains; preload\r\n\r\n{"ok":true,"result":{"message_id":1440,"from":{"id":678777066,"is_bot":true,"first_name":"textit_staging","username":"textit_staging_bot"},"chat":{"id":3527065,"first_name":"Nic","last_name":"Pottier","username":"Nicpottier","type":"private"},"date":1560267186,"text":"Your balance is now $6.00."}}',
-                    "elapsed_ms": 12,
-                    "retries": 0,
-                    "created_on": "2022-01-01T00:00:00Z",
-                }
-            ],
-        )
-        self.legacy_log = ChannelLog.objects.create(
-            channel=channel,
-            description="Message Sent",
-            is_error=False,
-            url="https://api.telegram.org/65474/sendMessage",
-            response_status=200,
-            request="POST /65474/sendMessage HTTP/1.1\r\nHost: api.telegram.org\r\nUser-Agent: Courier/1.2.159\r\nContent-Length: 231\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept-Encoding: gzip\r\n\r\nchat_id=3527065&reply_markup=%7B%22resize_keyboard%22%3Atrue%2C%22one_time_keyboard%22%3Atrue%2C%22keyboard%22%3A%5B%5B%7B%22text%22%3A%22blackjack%22%7D%2C%7B%22text%22%3A%22balance%22%7D%5D%5D%7D&text=Your+balance+is+now+%246.00.",
-            response='HTTP/1.1 200 OK\r\nContent-Length: 298\r\nAccess-Control-Allow-Methods: GET, POST, OPTIONS\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Expose-Headers: Content-Length,Content-Type,Date,Server,Connection\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nDate: Tue, 11 Jun 2019 15:33:06 GMT\r\nServer: nginx/1.12.2\r\nStrict-Transport-Security: max-age=31536000; includeSubDomains; preload\r\n\r\n{"ok":true,"result":{"message_id":1440,"from":{"id":678777066,"is_bot":true,"first_name":"textit_staging","username":"textit_staging_bot"},"chat":{"id":3527065,"first_name":"Nic","last_name":"Pottier","username":"Nicpottier","type":"private"},"date":1560267186,"text":"Your balance is now $6.00."}}',
+        self.connection2 = ChannelConnection.objects.create(
+            org=self.org,
+            channel=self.channel,
+            contact=contact,
+            contact_urn=contact.urns.get(),
+            status=ChannelConnection.STATUS_ERRORED,
+            connection_type="U",
         )
 
     def test_migration(self):
-        self.assertTrue(ChannelLog.objects.filter(id=self.new_log.id).exists())
-        self.assertFalse(ChannelLog.objects.filter(id=self.legacy_log.id).exists())
+        self.assertTrue(ChannelConnection.objects.filter(id=self.connection1.id).exists())
+        self.assertFalse(ChannelConnection.objects.filter(id=self.connection2.id).exists())
