f99b67433540f0fb579b16d3809f3ac9ae7d9546
susanm74 <susan.m.lister@gmail.com>
2022-10-10 16:02:08 +0700

    unit test updates

--- temba/msgs/tests.py
+++ temba/msgs/tests.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime, timedelta
+import html
 from unittest.mock import PropertyMock, patch
 
 import pytz
@@ -2305,6 +2306,44 @@ class BroadcastCRUDLTest(TembaTest, CRUDLTestMixin):
         self.assertEqual(broadcast.text, {"base": "Dinner reminder"})
         self.assertEqual(broadcast.base_language, "base")
         self.assertEqual(set(broadcast.contacts.all()), {self.frank})
+
+    def test_scheduled_delete(self):
+        self.login(self.editor)
+        schedule = Schedule.create_schedule(self.org, self.admin, timezone.now(), "D", repeat_days_of_week="MWF")
+        broadcast = self.create_broadcast(
+            self.admin,
+            "Daily reminder",
+            groups=[self.joe_and_frank],
+            schedule=schedule,
+        )
+        broadcast = Broadcast.objects.get()
+        # print(f"broadcast={broadcast}")
+
+        delete_url = reverse("msgs.broadcast_scheduled_delete", args=[broadcast.id])
+        # print(f"delete_url={delete_url}")
+
+        # fetch the delete modal
+        response = self.client.get(delete_url)
+        # print(f"delete_fetch_response={response}")
+        # print(f"delete_fetch_response_context_data={response.context_data}")        
+        # TODO figure out why this isn't working
+        # self.assertContains(response, "You are about to delete")
+
+        # fetch the delete modal
+        response = self.assertDeleteFetch(delete_url, allow_editors=True)
+        # print(f"delete_fetch_response={response}")
+        # print(f"delete_fetch_response_context_data={response.context_data}")
+        # TODO figure out why this isn't working
+        # self.assertContains(response, "You are about to delete")
+
+        # submit the delete modal
+        response = self.assertDeleteSubmit(delete_url, object_deleted=broadcast, success_status=200)
+        # print(f"delete_submit_response={response}")
+        # print(f"delete_submit_response_value={response.getvalue()}")
+        # print(f"delete_submit_response_items={response.items()}")
+        # TODO figure out why this is expecting "broadcast" and not "messages"
+        # or TODO figure out where "broadcast" is getting changed to "messages"
+        self.assertEqual("/broadcast/scheduled/", response["Temba-Success"])
 
     def test_missing_contacts(self):
         self.login(self.editor)
