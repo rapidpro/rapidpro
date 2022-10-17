778c26d362dfba71c95dfde5e6cde91272203aa2
susanm74 <susan.m.lister@gmail.com>
2022-10-17 12:28:56 +0700

    post-running code checks

--- temba/msgs/migrations/0194_broadcast_is_active.py
+++ temba/msgs/migrations/0194_broadcast_is_active.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('msgs', '0193_alter_msg_failed_reason'),
+        ("msgs", "0193_alter_msg_failed_reason"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='broadcast',
-            name='is_active',
+            model_name="broadcast",
+            name="is_active",
             field=models.BooleanField(default=True, null=True),
         ),
     ]
