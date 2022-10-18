606a64d33b7de38cde9ba7b7bb5af55752b4ae21
susanm74 <susan.m.lister@gmail.com>
2022-10-18 14:14:39 +0700

    added update_broadcast_is_active backfill migration

---
+++ temba/msgs/migrations/0195_update_broadcast_is_active.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+
+def update_broadcast_is_active(apps, schema_editor):  # pragma: no cover
+    Broadcast = apps.get_model("msgs", "Broadcast")
+
+    num_updated = 0
+    while True:
+        batch = list(Broadcast.objects.all()[:100])
+        if not batch:
+            break
+
+        Broadcast.objects.filter(id__in=[b.id for b in batch]).update(is_active=True)
+
+        num_updated += len(batch)
+        print(f"Updated {num_updated} broadcasts without an is_active")
+
+def reverse(apps, schema_editor):  # pragma: no cover
+    pass
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('msgs', '0194_broadcast_is_active'),
+    ]
+
+    operations = [migrations.RunPython(update_broadcast_is_active, reverse)]
