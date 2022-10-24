ad0599a260f969a274a0e5bfc25099ed5110bef4
Rowan Seymour <rowanseymour@gmail.com>
2022-10-24 16:47:17 -0500

    Fix migration conflict

--- temba/flows/migrations/0301_exportflowresultstask_with_groups.py
+++ temba/flows/migrations/0301_exportflowresultstask_with_groups.py
@@ -7,7 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("contacts", "0169_alter_contact_language_alter_contact_name"),
-        ("flows", "0299_session_status_trigger"),
+        ("flows", "0300_remove_flowsession_connection_and_more"),
     ]
 
     operations = [
