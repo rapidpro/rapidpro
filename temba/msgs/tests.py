0249a2393b445afadcba04428949cd1e3da928ba
Rowan Seymour <rowanseymour@gmail.com>
2022-10-31 11:16:46 -0500

    Fix tests

--- temba/msgs/tests.py
+++ temba/msgs/tests.py
@@ -1458,7 +1458,7 @@ class MsgCRUDLTest(TembaTest, CRUDLTestMixin):
 
         # check query count
         self.login(self.admin)
-        with self.assertNumQueries(16):
+        with self.assertNumQueries(15):
             self.client.get(inbox_url)
 
         response = self.assertListFetch(
@@ -1564,7 +1564,7 @@ class MsgCRUDLTest(TembaTest, CRUDLTestMixin):
 
         # check query count
         self.login(self.admin)
-        with self.assertNumQueries(16):
+        with self.assertNumQueries(15):
             self.client.get(archived_url)
 
         response = self.assertListFetch(
@@ -1607,7 +1607,7 @@ class MsgCRUDLTest(TembaTest, CRUDLTestMixin):
 
         # check query count
         self.login(self.admin)
-        with self.assertNumQueries(17):
+        with self.assertNumQueries(16):
             self.client.get(outbox_url)
 
         # messages sorted by created_on
@@ -1669,7 +1669,7 @@ class MsgCRUDLTest(TembaTest, CRUDLTestMixin):
 
         # check query count
         self.login(self.admin)
-        with self.assertNumQueries(15):
+        with self.assertNumQueries(14):
             self.client.get(sent_url)
 
         # messages sorted by sent_on
@@ -1700,7 +1700,7 @@ class MsgCRUDLTest(TembaTest, CRUDLTestMixin):
 
         # check query count
         self.login(self.admin)
-        with self.assertNumQueries(15):
+        with self.assertNumQueries(14):
             self.client.get(failed_url)
 
         response = self.assertListFetch(
