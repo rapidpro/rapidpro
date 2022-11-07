dcd367c0a53f3726299b07b088697ba289ecf1d8
Rowan Seymour <rowanseymour@gmail.com>
2022-11-07 09:03:38 -0500

    Update mailroom_db command to use postgresql 13

--- temba/utils/management/commands/mailroom_db.py
+++ temba/utils/management/commands/mailroom_db.py
@@ -46,6 +46,8 @@ RESET_SEQUENCES = (
     "triggers_trigger_id_seq",
 )
 
+PG_DUMP_VERSION = "13"
+
 
 class Command(BaseCommand):
     help = "Generates a database suitable for mailroom testing"
@@ -58,11 +60,13 @@ class Command(BaseCommand):
 
         result = subprocess.run(["pg_dump", "--version"], stdout=subprocess.PIPE)
         version = result.stdout.decode("utf8")
-        if version.split(" ")[-1].find("12.") == 0:
+        if version.split(" ")[-1].find(f"{PG_DUMP_VERSION}.") == 0:
             self._log(self.style.SUCCESS("OK") + "\n")
         else:
             self._log(
-                "\n" + self.style.ERROR("Incorrect pg_dump version, needs version 12.*, found: " + version) + "\n"
+                "\n"
+                + self.style.ERROR(f"Incorrect pg_dump version, needs version {PG_DUMP_VERSION}.*, found: " + version)
+                + "\n"
             )
             sys.exit(1)
 
