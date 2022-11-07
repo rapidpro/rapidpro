9cab1935f5770c175d411f5118a0b74a7be18a7c
Rafael Soares <75167930+rasoro@users.noreply.github.com>
2022-11-07 17:41:42 -0300

    twilioflex changes to add inputfield for ticketer name (#380)

--- temba/tickets/types/twilioflex/tests.py
+++ temba/tickets/types/twilioflex/tests.py
@@ -1,9 +1,8 @@
 from unittest.mock import patch
 
-from requests.exceptions import Timeout
-
 from django.urls import reverse
 
+from temba.tests import MockResponse
 from temba.tests.base import TembaTest
 from temba.tickets.models import Ticketer
 
@@ -22,42 +21,23 @@ class TwilioflexMixin(TembaTest):
 
 
 class TwilioflexViewTest(TwilioflexMixin):
-    def check_exceptions(self, mock_choices, mock_request, timeout_msg, exception_msg):
-        self.client.force_login(self.admin)
-        check = [(Timeout(), timeout_msg), (Exception(), exception_msg)]
-        for err, msg in check:
-
-            def side_effect(*arg, **kwargs):
-                raise err
-
-            mock_request.side_effect = side_effect
-            data = {
-                "account_sid": "AC123456789",
-                "auth_token": "123456789",
-                "chat_service_sid": "SI123456789",
-                "flex_flow_sid": "FO123456789",
-                "workspace_sid": "WS123456789",
-            }
-            response = self.client.post(self.connect_url, data=data)
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(len(response.context["messages"]), 1)
-            self.assertEqual([f"{m}" for m in response.context["messages"]][0], msg)
-
-    @patch("random.choice")
-    def test_form_valid(self, mock_choices):
+    def test_connect(self):
         self.client.force_login(self.admin)
         data = {
+            "ticketer_name": "org support",
             "account_sid": "AC123456789",
             "auth_token": "123456789",
             "chat_service_sid": "SI123456789",
             "flex_flow_sid": "FO123456789",
             "workspace_sid": "WS123456789",
         }
-        response = self.client.post(self.connect_url, data=data)
 
-        self.assertEqual(response.status_code, 302)
+        with patch("requests.get") as mock_get:
+            mock_get.return_value = MockResponse(200, "{}")
+            response = self.client.post(self.connect_url, data=data)
+            self.assertEqual(response.status_code, 302)
 
-        ticketer = Ticketer.objects.order_by("id").last()
-        self.assertEqual("Twilio Flex", ticketer.name)
+            ticketer = Ticketer.objects.order_by("id").last()
+            self.assertEqual(data["ticketer_name"], ticketer.name)
 
-        self.assertRedirect(response, reverse("tickets.ticket_list"))
+            self.assertRedirect(response, reverse("tickets.ticket_list"))
--- temba/tickets/types/twilioflex/views.py
+++ temba/tickets/types/twilioflex/views.py
@@ -1,4 +1,7 @@
+import requests
 from django import forms
+
+from django.forms import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from temba.tickets.models import Ticketer
@@ -8,35 +11,76 @@ from temba.utils.uuid import uuid4
 
 class ConnectView(BaseConnectView):
     class Form(BaseConnectView.Form):
+        ticketer_name = forms.CharField(
+            label=_("Ticketer Name"),
+            help_text=_("A name to help identify your ticketer"),
+        )
         account_sid = forms.CharField(
-            label=_("Authentication User"), help_text=_("Account SID or API Key SID of a twilio account.")
+            label=_("Authentication User"),
+            help_text=_("Account SID or API Key SID of a twilio account."),
         )
         auth_token = forms.CharField(
-            label=_("Authentication Password"), help_text=_("Auth token or API Key Secret of a twilio account.")
+            label=_("Authentication Password"),
+            help_text=_("Auth token or API Key Secret of a twilio account."),
         )
-        chat_service_sid = forms.CharField(label=_("Chat Service SID"), help_text=_("SID of a Chat service instance."))
+        chat_service_sid = forms.CharField(
+            label=_("Chat Service SID"), help_text=_("SID of a Chat service instance.")
+        )
         flex_flow_sid = forms.CharField(
             label=_("Flex Flow"),
-            help_text=_("a Flex Flow (also called Flex Address) that is of the task type."),
+            help_text=_(
+                "a Flex Flow (also called Flex Address) that is of the task type."
+            ),
         )
-        workspace_sid = forms.CharField(label=_("Workspace SID"), help_text=_("SID of a Workspace."))
+        workspace_sid = forms.CharField(
+            label=_("Workspace SID"), help_text=_("SID of a Workspace.")
+        )
+
+        def clean(self):
+            account_sid = self.cleaned_data["account_sid"]
+            auth_token = self.cleaned_data["auth_token"]
+            chat_service_sid = self.cleaned_data["chat_service_sid"]
+
+            try:
+                response = requests.get(
+                    f"https://chat.twilio.com/v2/Services/{chat_service_sid}",
+                    auth=(account_sid, auth_token),
+                )
+                print(response.status_code)
+                response.raise_for_status()
+
+            except Exception:
+                raise ValidationError(
+                    _(
+                        "Unable to connect with twilio chat service, please check input fields and try again."
+                    )
+                )
+            return self.cleaned_data
 
     def form_valid(self, form):
         from .type import TwilioFlexType
 
+        ticketer_name = form.cleaned_data["ticketer_name"]
+        account_sid = form.cleaned_data["account_sid"]
+        auth_token = form.cleaned_data["auth_token"]
+        chat_service_sid = form.cleaned_data["chat_service_sid"]
+        flex_flow_sid = form.cleaned_data["flex_flow_sid"]
+        workspace_sid = form.cleaned_data["workspace_sid"]
+
         config = {
-            TwilioFlexType.CONFIG_ACCOUNT_SID: form.cleaned_data["account_sid"],
-            TwilioFlexType.CONFIG_AUTH_TOKEN: form.cleaned_data["auth_token"],
-            TwilioFlexType.CONFIG_CHAT_SERVICE_SID: form.cleaned_data["chat_service_sid"],
-            TwilioFlexType.CONFIG_FLEX_FLOW_SID: form.cleaned_data["flex_flow_sid"],
-            TwilioFlexType.CONFIG_WORKSPACE_SID: form.cleaned_data["workspace_sid"],
+            TwilioFlexType.CONFIG_ACCOUNT_SID: account_sid,
+            TwilioFlexType.CONFIG_AUTH_TOKEN: auth_token,
+            TwilioFlexType.CONFIG_CHAT_SERVICE_SID: chat_service_sid,
+            TwilioFlexType.CONFIG_FLEX_FLOW_SID: flex_flow_sid,
+            TwilioFlexType.CONFIG_WORKSPACE_SID: workspace_sid,
         }
+
         self.object = Ticketer(
             uuid=uuid4(),
             org=self.org,
             ticketer_type=TwilioFlexType.slug,
             config=config,
-            name=TwilioFlexType.name,
+            name=ticketer_name,
             created_by=self.request.user,
             modified_by=self.request.user,
         )
