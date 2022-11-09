a46f722245994e0fd0112375a69ce1f0d415c1ef
Rowan Seymour <rowanseymour@gmail.com>
2022-11-09 15:54:52 -0500

    Add apply_manual to simplify_org_hierarchies data migration

--- temba/orgs/migrations/0108_simplify_org_hierarchies.py
+++ temba/orgs/migrations/0108_simplify_org_hierarchies.py
@@ -27,6 +27,12 @@ def reverse(apps, schema_editor):
     pass
 
 
+def apply_manual():  # pragma: no cover
+    from django.apps import apps
+
+    simplify_org_hierarchies(apps, None)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
