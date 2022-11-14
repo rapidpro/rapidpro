80ba347c8cc1333978a666591a8c2e4aa9645b52
Rowan Seymour <rowanseymour@gmail.com>
2022-11-14 09:54:58 -0500

    Display channel logs with earliest at top

--- temba/channels/views.py
+++ temba/channels/views.py
@@ -1481,7 +1481,7 @@ class ChannelLogCRUDL(SmartCRUDL):
             return self.msg.org
 
         def derive_queryset(self, **kwargs):
-            return super().derive_queryset(**kwargs).filter(msg=self.msg).order_by("-created_on")
+            return super().derive_queryset(**kwargs).filter(msg=self.msg).order_by("created_on")
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
@@ -1514,7 +1514,7 @@ class ChannelLogCRUDL(SmartCRUDL):
             return self.call.org
 
         def derive_queryset(self, **kwargs):
-            return super().derive_queryset(**kwargs).filter(call=self.call).order_by("-created_on")
+            return super().derive_queryset(**kwargs).filter(call=self.call).order_by("created_on")
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
