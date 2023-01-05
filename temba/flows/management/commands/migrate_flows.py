5954a966bd6e915d243c2f88b1e2cf63ee218450
Rowan Seymour <rowanseymour@gmail.com>
2023-01-05 16:50:12 -0500

    Tweak migrate_flows to have smaller batch size and order by org to increase org assets cache hits

--- temba/flows/management/commands/migrate_flows.py
+++ temba/flows/management/commands/migrate_flows.py
@@ -13,8 +13,11 @@ class Command(BaseCommand):
         self.migrate_flows()
 
     def migrate_flows(self):
-        flows_to_migrate = Flow.objects.filter(is_active=True).exclude(version_number=Flow.CURRENT_SPEC_VERSION)
-
+        flows_to_migrate = (
+            Flow.objects.filter(is_active=True)
+            .exclude(version_number=Flow.CURRENT_SPEC_VERSION)
+            .order_by("org_id", "id")
+        )
         flow_ids = list(flows_to_migrate.values_list("id", flat=True))
         total = len(flow_ids)
 
@@ -27,7 +30,7 @@ class Command(BaseCommand):
         num_updated = 0
         num_errored = 0
 
-        for id_batch in chunk_list(flow_ids, 5000):
+        for id_batch in chunk_list(flow_ids, 1000):
             for flow in Flow.objects.filter(id__in=id_batch):
                 try:
                     flow.ensure_current_version()
