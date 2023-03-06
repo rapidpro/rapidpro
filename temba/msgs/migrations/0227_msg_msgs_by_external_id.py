a7a7e4f7ef142f0ccc1de48283c464547408c75e
Rowan Seymour <rowanseymour@gmail.com>
2023-03-06 13:27:14 -0500

    Fix migration conflict

--- temba/msgs/migrations/0227_msg_msgs_by_external_id.py
+++ temba/msgs/migrations/0227_msg_msgs_by_external_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("msgs", "0225_new_inbound_filtering"),
+        ("msgs", "0226_msg_created_by"),
     ]
 
     operations = [
