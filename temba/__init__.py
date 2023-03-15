3e378489ab3792dc6984300c10ee12705763ce64
Rowan Seymour <rowanseymour@gmail.com>
2023-03-15 13:32:55 -0500

    Add poetry plugin to maintain version number in temba/__init__.py

--- temba/__init__.py
+++ temba/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "8.1.73"  # set by poetry bumpversion plugin
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .temba_celery import app as celery_app  # noqa
