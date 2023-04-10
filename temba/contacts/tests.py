d2b70b2be12364d130cc5a2720a3631519748044
susanm74 <susan.m.lister@gmail.com>
2023-04-10 23:20:28 +0700

    - fixed unit test

--- temba/contacts/tests.py
+++ temba/contacts/tests.py
@@ -1406,6 +1406,15 @@ class ContactGroupCRUDLTest(TembaTest, CRUDLTestMixin):
         # create a group which isn't used by anything
         group1 = self.create_group("Group 1", contacts=[])
 
+        # a group with no dependents can be deleted
+        delete_group1_url = reverse("contacts.contactgroup_delete", args=[group1.uuid])
+        response = self.assertDeleteFetch(delete_group1_url, allow_editors=True)
+        self.assertEqual({}, response.context["soft_dependents"])
+        self.assertEqual({}, response.context["hard_dependents"])
+        self.assertContains(response, "You are about to delete")
+        self.assertContains(response, "There is no way to undo this. Are you sure?")
+        self.assertDeleteSubmit(delete_group1_url, object_deactivated=group1, success_status=200)
+
         # create a group which is used by a flow (soft) and a trigger (soft)
         group2 = self.create_group("Group 2", contacts=[])
         flow1 = self.create_flow("Flow 1")
@@ -1428,22 +1437,6 @@ class ContactGroupCRUDLTest(TembaTest, CRUDLTestMixin):
         )
         self.assertEqual(1, Trigger.objects.count())
 
-        # create a group which is used by a flow (soft), a trigger (soft), and a campaign (hard)
-        group3 = self.create_group("Group 3", contacts=[])
-        flow2 = self.create_flow("Flow 2")
-        flow2.group_dependencies.add(group3)
-        Trigger.create(self.org, self.admin, Trigger.TYPE_KEYWORD, flow2, keyword="Trigger2", groups=[group3])
-        campaign1 = Campaign.create(self.org, self.admin, "Planting Reminders", group3)
-
-        # a group with no dependents can be deleted
-        delete_group1_url = reverse("contacts.contactgroup_delete", args=[group1.uuid])
-        response = self.assertDeleteFetch(delete_group1_url, allow_editors=True)
-        self.assertEqual({}, response.context["soft_dependents"])
-        self.assertEqual({}, response.context["hard_dependents"])
-        self.assertContains(response, "You are about to delete")
-        self.assertContains(response, "There is no way to undo this. Are you sure?")
-        self.assertDeleteSubmit(delete_group1_url, object_deactivated=group1, success_status=200)
-
         # a group with only soft dependents can also be deleted but we give warnings
         delete_group2_url = reverse("contacts.contactgroup_delete", args=[group2.uuid])
         response = self.assertDeleteFetch(delete_group2_url, allow_editors=True)
@@ -1463,6 +1456,14 @@ class ContactGroupCRUDLTest(TembaTest, CRUDLTestMixin):
         self.assertTrue(flow1.has_issues)
         self.assertNotIn(group2, flow1.field_dependencies.all())
 
+        # create a group which is used by a flow (soft), a trigger (soft), and a campaign (hard)
+        group3 = self.create_group("Group 3", contacts=[])
+        flow2 = self.create_flow("Flow 2")
+        flow2.group_dependencies.add(group3)
+        Trigger.create(self.org, self.admin, Trigger.TYPE_KEYWORD, flow2, keyword="Trigger2", groups=[group3])
+        campaign1 = Campaign.create(self.org, self.admin, "Planting Reminders", group3)
+        self.assertEqual(1, Trigger.objects.count())
+
         # a group with hard dependents can't be deleted
         delete_group3_url = reverse("contacts.contactgroup_delete", args=[group3.uuid])
         response = self.assertDeleteFetch(delete_group3_url, allow_editors=True)
@@ -1471,6 +1472,9 @@ class ContactGroupCRUDLTest(TembaTest, CRUDLTestMixin):
         self.assertContains(response, "can't be deleted as it is still used by the following items:")
         self.assertContains(response, campaign1.name)
         self.assertNotContains(response, "Delete")
+
+        # check that the trigger is not deleted
+        self.assertEqual(1, Trigger.objects.count())
 
 
 class ContactTest(TembaTest, CRUDLTestMixin):
