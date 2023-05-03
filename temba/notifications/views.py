e2aa13cf911dab1677767915963a10b465308f65
Rowan Seymour <rowanseymour@gmail.com>
2023-05-03 16:45:47 -0500

    Remove JSON view to list notifications now that has moved to the internal API

--- temba/notifications/views.py
+++ temba/notifications/views.py
@@ -1,6 +1,5 @@
 from smartmin.views import SmartCRUDL, SmartListView
 
-from django.http import JsonResponse
 from django.utils.translation import gettext_lazy as _
 
 from temba.orgs.views import OrgPermsMixin
@@ -27,35 +26,6 @@ class NotificationTargetMixin:
             Notification.mark_seen(request.org, notification_type, scope=scope, user=request.user)
 
         return response
-
-
-class NotificationCRUDL(SmartCRUDL):
-    model = Notification
-    actions = ("list",)
-
-    class List(OrgPermsMixin, SmartListView):
-        default_order = "-id"
-        select_related = ("org",)
-        prefetch_related = (
-            "contact_import",
-            "contact_export",
-            "message_export",
-            "results_export",
-            "incident",
-        )
-
-        def get_queryset(self, **kwargs):
-            return (
-                super()
-                .get_queryset(**kwargs)
-                .filter(org=self.org, user=self.request.user)
-                .prefetch_related(*self.prefetch_related)
-            )
-
-        def render_to_response(self, context, **response_kwargs):
-            return JsonResponse(
-                {"results": [n.as_json() for n in context["object_list"]]}, json_dumps_params={"indent": 2}
-            )
 
 
 class IncidentCRUDL(SmartCRUDL):
