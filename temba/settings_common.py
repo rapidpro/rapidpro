c7f4c69a00c274149b8439c834016d64f7a7d1d6
Eric Newcomer <ericnewcomer@gmail.com>
2023-05-03 22:08:55 -0700

    Remove old nav from landing page

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -1110,3 +1110,5 @@ MAILROOM_AUTH_TOKEN = None
 # ElasticSearch
 # -----------------------------------------------------------------------------------
 ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL", "http://localhost:9200")
+
+CSRF_TRUSTED_ORIGINS = ("https://localhost",)
