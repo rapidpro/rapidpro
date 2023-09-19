9e47fe00daaa32e832789251c673b142aa5fdaa9
Norbert Kwizera <norkans7@gmail.com>
2023-09-19 14:13:45 +0200

    Add tests

--- temba/dashboard/tests.py
+++ temba/dashboard/tests.py
@@ -55,6 +55,71 @@ class DashboardTest(TembaTest):
         # outgoing messages
         self.assertEqual(3, response[1]["data"][0][1])
 
+    def test_workspace_stats(self):
+        url = reverse("dashboard.dashboard_workspace_stats")
+
+        # visit this page without authenticating
+        response = self.client.get(url, follow=True)
+
+        # nope!
+        self.assertRedirects(response, "/users/login/?next=%s" % url)
+
+        self.login(self.admin)
+        self.create_activity()
+        response = self.client.get(url).json()
+
+        self.assertEqual(5, len(response))
+        self.assertEqual(2, response[0]["data"][0][1])  # incoming messages
+        self.assertEqual(3, response[2]["data"][0][1])  # outgoing messages
+        self.assertEqual(5, response[4]["data"][0][1])  # total messages
+
+    def test_channel_types_stats(self):
+        url = reverse("dashboard.dashboard_channel_types_stats")
+
+        # visit this page without authenticating
+        response = self.client.get(url, follow=True)
+
+        # nope!
+        self.assertRedirects(response, "/users/login/?next=%s" % url)
+
+        self.login(self.admin)
+        self.create_activity()
+        types = ["T", "TWT", "FB", "NX", "AT", "KN"]
+        michael = self.create_contact("Michael", urns=["twitter:mjackson"])
+        for t in types:
+            channel = self.create_channel(t, f"Test Channel {t}", f"{t}:1234")
+            self.create_outgoing_msg(michael, f"Message on {t}", channel=channel)
+        response = self.client.get(url).json()
+
+        self.assertEqual([1, 1], response[0]["data"])
+        self.assertEqual("Android Incoming", response[0]["name"])
+        self.assertEqual([2, 1], response[1]["data"])
+        self.assertEqual("Android Outgoing", response[1]["name"])
+        self.assertEqual([], response[2]["data"])
+        self.assertEqual("Africa's Talking Incoming", response[2]["name"])
+        self.assertEqual([1], response[3]["data"])
+        self.assertEqual("Africa's Talking Outgoing", response[3]["name"])
+        self.assertEqual([], response[4]["data"])
+        self.assertEqual("Facebook Incoming", response[4]["name"])
+        self.assertEqual([1], response[5]["data"])
+        self.assertEqual("Facebook Outgoing", response[5]["name"])
+        self.assertEqual([], response[6]["data"])
+        self.assertEqual("Kannel Incoming", response[6]["name"])
+        self.assertEqual([1], response[7]["data"])
+        self.assertEqual("Kannel Outgoing", response[7]["name"])
+        self.assertEqual([], response[8]["data"])
+        self.assertEqual("Vonage Incoming", response[8]["name"])
+        self.assertEqual([1], response[9]["data"])
+        self.assertEqual("Vonage Outgoing", response[9]["name"])
+        self.assertEqual([], response[10]["data"])
+        self.assertEqual("Twilio Incoming", response[10]["name"])
+        self.assertEqual([1], response[11]["data"])
+        self.assertEqual("Twilio Outgoing", response[11]["name"])
+        self.assertEqual([], response[12]["data"])
+        self.assertEqual("Twitter Incoming", response[12]["name"])
+        self.assertEqual([1], response[13]["data"])
+        self.assertEqual("Twitter Outgoing", response[13]["name"])
+
     def test_range_details(self):
         url = reverse("dashboard.dashboard_range_details")
 
