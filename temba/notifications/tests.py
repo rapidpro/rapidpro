31790032152bbc699df1504c8e69d23d9b1ebb46
Rowan Seymour <rowanseymour@gmail.com>
2023-09-26 14:05:20 +0000

    Coverage

--- temba/notifications/tests.py
+++ temba/notifications/tests.py
@@ -354,6 +354,14 @@ class NotificationTest(TembaTest):
             email=True,
         )
 
+        send_notification_emails()
+
+        self.assertEqual(2, len(mail.outbox))
+        self.assertEqual("[Nyaruka] Incident: Workspace Flagged", mail.outbox[0].subject)
+        self.assertEqual(["admin@nyaruka.com"], mail.outbox[0].recipients())
+        self.assertEqual("[Nyaruka] Incident: Workspace Flagged", mail.outbox[1].subject)
+        self.assertEqual(["editor@nyaruka.com"], mail.outbox[1].recipients())
+
         # if a user visits the incident page, all incident notifications are now read
         self.login(self.editor)
         self.client.get("/incident/")
