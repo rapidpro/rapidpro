955bef926bbdd88e6f3ad5e0c8566ba5d3910b48
Rowan Seymour <rowanseymour@gmail.com>
2024-10-23 20:35:46 +0000

    Fix paging on archive list pages and make styling consistent with other list views

--- temba/archives/views.py
+++ temba/archives/views.py
@@ -13,48 +13,39 @@ from .models import Archive
 class ArchiveCRUDL(SmartCRUDL):
     model = Archive
     actions = ("read", "run", "message")
-    permissions = True
 
     class BaseList(BaseListView):
-        title = _("Archive")
         fields = ("url", "start_date", "period", "record_count", "size")
         default_order = ("-start_date", "-period", "archive_type")
-        paginate_by = 250
+        default_template = "archives/archive_list.html"
 
         def derive_queryset(self, **kwargs):
-            queryset = super().derive_queryset(**kwargs)
-
-            # filter by our archive type
-            return queryset.filter(archive_type=self.get_archive_type()).exclude(rollup_id__isnull=False)
-
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            context["archive_types"] = Archive.TYPE_CHOICES
-            context["selected"] = self.get_archive_type()
-            return context
+            # filter by our archive type and exclude archives included in rollups
+            return (
+                super()
+                .derive_queryset(**kwargs)
+                .filter(archive_type=self.get_archive_type())
+                .exclude(rollup_id__isnull=False)
+            )
 
     class Run(BaseList):
+        title = _("Run Archives")
         menu_path = "/settings/archives/run"
 
         @classmethod
         def derive_url_pattern(cls, path, action):
             return r"^%s/%s/$" % (path, Archive.TYPE_FLOWRUN)
 
-        def derive_title(self):
-            return _("Run Archives")
-
         def get_archive_type(self):
             return Archive.TYPE_FLOWRUN
 
     class Message(BaseList):
+        title = _("Message Archives")
         menu_path = "/settings/archives/message"
 
         @classmethod
         def derive_url_pattern(cls, path, action):
             return r"^%s/%s/$" % (path, Archive.TYPE_MSG)
-
-        def derive_title(self):
-            return _("Message Archives")
 
         def get_archive_type(self):
             return Archive.TYPE_MSG
