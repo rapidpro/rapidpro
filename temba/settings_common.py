9b5a7a5e5685c84fc07aa437dc9a6cb97c230881
Rowan Seymour <rowanseymour@gmail.com>
2024-11-18 18:24:25 +0000

    Add support for TST type testing channels

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -896,6 +896,7 @@ CHANNEL_TYPES = [
     "temba.channels.types.zenvia_sms.ZenviaSMSType",
     "temba.channels.types.zenvia_whatsapp.ZenviaWhatsAppType",
     "temba.channels.types.android.AndroidType",
+    "temba.channels.types.test.TestType",
 ]
 
 ANALYTICS_TYPES = [
